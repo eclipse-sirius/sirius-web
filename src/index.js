aa65d7a2c7f90c4be7c513c742216d5bb6c4272f
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2018-04-16 10:57:27 +0200

    [3] Add the first version of the code
    This commit will add a very basic entry point for the application along
    with the basic CSS resources. The code formatter has been setup using
    prettier and the static analysis with ESLint just like in Create React
    App.
    
    Bug: https://github.com/eclipse/sirius-components/issues/3
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Obeo and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2
+ * which accompanies this distribution and is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *******************************************************************************/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './reset.css';
+import './app.css';
+
+/**
+ * The entry point of the application.
+ */
+ReactDOM.render(<p>Hello World</p>, document.getElementById('root'));
