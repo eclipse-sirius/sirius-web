2b698b876f56311c01e291193f3258231145e8f0
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2018-05-15 16:22:01 +0200

    [29] Add the ability to hide the sidebar
    Both the navbar and the sidebar can be hidden using the query parameter
    fullscreen with the value true, for example with the following URL:
    
    https://domain.tld/projects/test?fullscreen=true
    
    Bug: https://github.com/eclipse/sirius-components/issues/29
    Signed-off-by: Stéphane Bégaudeau stephane.begaudeau@obeo.fr>

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,6 +8,7 @@
  *******************************************************************************/
 
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import { classNames } from '../../common/classnames';
 
@@ -22,6 +23,9 @@ const APP_NAVBAR__CLASS_NAMES = 'app-navbar';
 const APP_CONTAINER__CLASS_NAMES = 'app-container';
 const APP_ASIDE__CLASS_NAMES = 'app-aside';
 const APP_MAIN__CLASS_NAMES = 'app-main';
+const FULLSCREEN__CLASS_NAMES = 'fullscreen';
+
+const FULLSCREEN_PARAMETER_NAME = 'fullscreen';
 
 /**
  * The App component is the entry point of the user interface of the application.
@@ -30,15 +34,33 @@ const APP_MAIN__CLASS_NAMES = 'app-main';
  * application along with some additional actions. Under the Navbar, two columns
  * are used to display side by side the Aside component and the Main component.
  */
-export const App = ({ className, ...props }) => {
-  const appClassNames = classNames(APP__CLASS_NAMES, className);
-  return (
-    <div className={appClassNames} {...props}>
-      <Navbar className={APP_NAVBAR__CLASS_NAMES} />
+const AppWithoutRouter = ({ className, ...props }) => {
+  const { location } = props;
+  const urlParams = new URLSearchParams(location.search);
+  const isFullScreen = urlParams.get(FULLSCREEN_PARAMETER_NAME);
+
+  let appClassNames = classNames(APP__CLASS_NAMES, className);
+  let container = (
+    <div className={APP_CONTAINER__CLASS_NAMES}>
+      <Aside className={APP_ASIDE__CLASS_NAMES} />
+      <Main className={APP_MAIN__CLASS_NAMES} />
+    </div>
+  );
+
+  if (isFullScreen) {
+    appClassNames = classNames(APP__CLASS_NAMES, FULLSCREEN__CLASS_NAMES, className);
+    container = (
       <div className={APP_CONTAINER__CLASS_NAMES}>
-        <Aside className={APP_ASIDE__CLASS_NAMES} />
         <Main className={APP_MAIN__CLASS_NAMES} />
       </div>
+    );
+  }
+
+  return (
+    <div className={appClassNames}>
+      <Navbar className={APP_NAVBAR__CLASS_NAMES} />
+      {container}
     </div>
   );
 };
+export const App = withRouter(AppWithoutRouter);
