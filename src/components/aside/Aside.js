0901e67bd1fc0c18321117b7d3e4c12f305c9de6
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2018-08-10 14:50:31 +0200

    [51] Add the ability to toggle the Aside component
    Add a first version of the support for toggling the Aside component. The
    behavior should be improved at a later date in order to specify how it
    should work for various CSS breakpoints.
    
    Bug: https://github.com/eclipse/sirius-components/issues/51
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -11,6 +11,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { classNames } from '../../common/classnames';
+import { Spacing } from '../spacing/Spacing';
+import { M, L } from '../spacing/SpacingConstants';
 
 import './Aside.css';
 
@@ -35,42 +37,44 @@ export const Aside = ({ className, ...props }) => {
   const asideClassNames = classNames(ASIDE__CLASS_NAMES, className);
   return (
     <aside className={asideClassNames} {...props}>
-      <div className={ASIDE_MAIN_LINKS__CLASS_NAMES}>
-        <ul>
-          <li>
-            <Link className={MAIN_LINKS__CLASS_NAMES} to="/">
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link className={MAIN_LINKS__CLASS_NAMES} to="/projects">
-              Projects
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className={ASIDE_ADDITIONAL_LINKS__CLASS_NAMES}>
-        <ul>
-          <li>
-            <a
-              className={ADDITIONAL_LINKS__CLASS_NAMES}
-              href="https://github.com/eclipse/sirius-components/issues"
-              target="_blank"
-              rel="noopener noreferrer">
-              Report an issue
-            </a>
-          </li>
-          <li>
-            <a
-              className={ADDITIONAL_LINKS__CLASS_NAMES}
-              href="https://stackoverflow.com/questions/tagged/sirius"
-              target="_blank"
-              rel="noopener noreferrer">
-              Help
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Spacing top={L} right={M} left={M}>
+        <div className={ASIDE_MAIN_LINKS__CLASS_NAMES}>
+          <ul>
+            <li>
+              <Link className={MAIN_LINKS__CLASS_NAMES} to="/">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link className={MAIN_LINKS__CLASS_NAMES} to="/projects">
+                Projects
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={ASIDE_ADDITIONAL_LINKS__CLASS_NAMES}>
+          <ul>
+            <li>
+              <a
+                className={ADDITIONAL_LINKS__CLASS_NAMES}
+                href="https://github.com/eclipse/sirius-components/issues"
+                target="_blank"
+                rel="noopener noreferrer">
+                Report an issue
+              </a>
+            </li>
+            <li>
+              <a
+                className={ADDITIONAL_LINKS__CLASS_NAMES}
+                href="https://stackoverflow.com/questions/tagged/sirius"
+                target="_blank"
+                rel="noopener noreferrer">
+                Help
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Spacing>
     </aside>
   );
 };
