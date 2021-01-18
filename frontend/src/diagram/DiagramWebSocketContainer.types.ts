6e2c21e8fe088c6c91160f1bb9d12636737d80eb
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2021-01-18 15:39:38 +0100

    [252] Improve the typing of the DiagramWebSocketContainer
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/252
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ frontend/src/diagram/DiagramWebSocketContainer.types.ts
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+import { Selection, Subscriber } from 'views/edit-project/EditProjectView.types';
+
+export type DiagramWebSocketContainerProps = {
+  projectId: string;
+  representationId: string;
+  readOnly: boolean;
+  selection: Selection;
+  setSelection: (newSelection: Selection) => void;
+  setSubscribers: (newSubscribers: Subscriber[]) => void;
+};
