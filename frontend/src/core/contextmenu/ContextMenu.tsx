a893487ba5fcff85db36166d557d784c37cb2fb1
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2021-02-16 22:02:42 +0100

    [152] Remove two useless dependencies
    - airbnb-prop-types (we never really used it)
    - universal-cookie (we don't even use cookies)
    
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/152
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- frontend/src/core/contextmenu/ContextMenu.tsx
+++ frontend/src/core/contextmenu/ContextMenu.tsx
@@ -10,19 +10,18 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-import { forbidExtraProps } from 'airbnb-prop-types';
 import { Text } from 'core/text/Text';
 import PropTypes from 'prop-types';
 import React, { useCallback, useEffect, useRef } from 'react';
 import styles from './ContextMenu.module.css';
 
-const entryPropTypes = forbidExtraProps({
+const entryPropTypes = {
   icon: PropTypes.node,
   label: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
   'data-testid': PropTypes.string.isRequired,
-});
+};
 export const Entry = ({ icon, label, disabled, onClick, 'data-testid': dataTestId }) => {
   let textClassName = styles.label;
   if (disabled) {
