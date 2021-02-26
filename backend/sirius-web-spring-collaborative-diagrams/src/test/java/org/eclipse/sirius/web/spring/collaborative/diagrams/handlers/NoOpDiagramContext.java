2895c662eb2d04f1904f1dde1d14a2f075d29e1a
Pierre-Charles David <pierre-charles.david@obeo.fr>
2021-02-26 14:57:33 +0100

    [339] Let RenameDiagramEventHandler handle diagram renaming
    Bug: eclipse-sirius/sirius-components#339
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ backend/sirius-web-spring-collaborative-diagrams/src/test/java/org/eclipse/sirius/web/spring/collaborative/diagrams/handlers/NoOpDiagramContext.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.spring.collaborative.diagrams.handlers;
+
+import java.util.List;
+
+import org.eclipse.sirius.web.collaborative.diagrams.api.IDiagramContext;
+import org.eclipse.sirius.web.diagrams.Diagram;
+import org.eclipse.sirius.web.diagrams.MoveEvent;
+import org.eclipse.sirius.web.diagrams.Position;
+import org.eclipse.sirius.web.diagrams.ViewCreationRequest;
+
+/**
+ * Implementation of the diagram context which does nothing.
+ *
+ * @author sbegaudeau
+ */
+public class NoOpDiagramContext implements IDiagramContext {
+
+    @Override
+    public Diagram getDiagram() {
+        return null;
+    }
+
+    @Override
+    public void update(Diagram updatedDiagram) {
+    }
+
+    @Override
+    public List<ViewCreationRequest> getViewCreationRequests() {
+        return List.of();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public MoveEvent getMoveEvent() {
+        return null;
+    }
+
+    @Override
+    public void setMoveEvent(MoveEvent moveEvent) {
+    }
+
+    @Override
+    public Position getStartingPosition() {
+        return null;
+    }
+
+    @Override
+    public void setStartingPosition(Position startingPosition) {
+    }
+
+}
