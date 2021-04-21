rbelafia/sirius-components

[289] Handle element resizing in backend
* Create a UpdateNodeBounds GraphQL API

Bug: https://github.com/eclipse-sirius/sirius-components/issues/289
Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

backend/sirius-web-collaborative-diagrams-api/src/main/java/org/eclipse/sirius/web/collaborative/diagrams/api/dto/UpdateNodeBoundsInput.java
@@ -1,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.collaborative.diagrams.api.dto;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+
+import org.eclipse.sirius.web.annotations.graphql.GraphQLField;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLID;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLInputObjectType;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLNonNull;
+import org.eclipse.sirius.web.collaborative.diagrams.api.IDiagramInput;
+
+/**
+ * The input for the "Update Node Bounds" mutation.
+ *
+ * @author fbarbin
+ */
+@GraphQLInputObjectType
+public final class UpdateNodeBoundsInput implements IDiagramInput {
+    private UUID projectId;
+
+    private UUID representationId;
+
+    private UUID diagramElementId;
+
+    private double newPositionX;
+
+    private double newPositionY;
+
+    private double newWidth;
+
+    private double newHeight;
+
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getProjectId() {
+        return this.projectId;
+    }
+
+    @Override
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getRepresentationId() {
+        return this.representationId;
+    }
+
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getDiagramElementId() {
+        return this.diagramElementId;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewPositionX() {
+        return this.newPositionX;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewPositionY() {
+        return this.newPositionY;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewWidth() {
+        return this.newWidth;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewHeight() {
+        return this.newHeight;
+    }
+
+    @Override
+    public String toString() {
+        String pattern = "{0} '{'projectId: {1}, representationId: {2}, diagramElementId: {3}, newPositionX: {4}, newPositionY: {5}, newWidth: {6}, newHeight: {7}'}'"; //$NON-NLS-1$
+        return MessageFormat.format(pattern, this.getClass().getSimpleName(), this.projectId, this.representationId, this.diagramElementId, this.newPositionX, this.newPositionY, this.newWidth,
+                this.newHeight);
+    }
+}

eclipse-sirius/sirius-web

backend/sirius-web-collaborative-diagrams-api/src/main/java/org/eclipse/sirius/web/collaborative/diagrams/api/dto/UpdateNodeBoundsInput.java
@@ -1,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.collaborative.diagrams.api.dto;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+
+import org.eclipse.sirius.web.annotations.graphql.GraphQLField;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLID;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLInputObjectType;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLNonNull;
+import org.eclipse.sirius.web.collaborative.diagrams.api.IDiagramInput;
+
+/**
+ * The input for the "Update Node Bounds" mutation.
+ *
+ * @author fbarbin
+ */
+@GraphQLInputObjectType
+public final class UpdateNodeBoundsInput implements IDiagramInput {
+
+    private UUID id;
+
+    private UUID projectId;
+
+    private UUID representationId;
+
+    private UUID diagramElementId;
+
+    private double newPositionX;
+
+    private double newPositionY;
+
+    private double newWidth;
+
+    private double newHeight;
+
+    @Override
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getId() {
+        return this.id;
+    }
+
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getProjectId() {
+        return this.projectId;
+    }
+
+    @Override
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getRepresentationId() {
+        return this.representationId;
+    }
+
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getDiagramElementId() {
+        return this.diagramElementId;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewPositionX() {
+        return this.newPositionX;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewPositionY() {
+        return this.newPositionY;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewWidth() {
+        return this.newWidth;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public double getNewHeight() {
+        return this.newHeight;
+    }
+
+    @Override
+    public String toString() {
+        String pattern = "{0} '{'Id: {1}, projectId: {2}, representationId: {3}, diagramElementId: {4}, newPositionX: {5}, newPositionY: {6}, newWidth: {7}, newHeight: {8}'}'"; //$NON-NLS-1$
+        return MessageFormat.format(pattern, this.getClass().getSimpleName(), this.id, this.projectId, this.representationId, this.diagramElementId, this.newPositionX, this.newPositionY,
+                this.newWidth, this.newHeight);
+    }
+}