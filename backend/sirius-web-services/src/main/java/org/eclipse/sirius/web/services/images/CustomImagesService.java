6ed416298d6676e9aa3bdf50a33036210cb66f80
Pierre-Charles David <pierre-charles.david@obeo.fr>
2021-04-29 16:42:04 +0200

    [441] Add service layer for custom images
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/441
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ backend/sirius-web-services/src/main/java/org/eclipse/sirius/web/services/images/CustomImagesService.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.services.images;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.eclipse.sirius.web.persistence.entities.CustomImageEntity;
+import org.eclipse.sirius.web.persistence.repositories.ICustomImageRepository;
+import org.eclipse.sirius.web.services.api.images.CustomImage;
+import org.eclipse.sirius.web.services.api.images.ICustomImagesService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service used to manipulate custom images.
+ *
+ * @author pcdavid
+ */
+@Service
+public class CustomImagesService implements ICustomImagesService {
+    private final ICustomImageRepository customImageRepository;
+
+    public CustomImagesService(ICustomImageRepository customImageRepository) {
+        this.customImageRepository = Objects.requireNonNull(customImageRepository);
+    }
+
+    @Override
+    public Optional<byte[]> getImageContentsByFileName(String fileName) {
+        return this.customImageRepository.findByFileName(fileName).map(CustomImageEntity::getContent);
+    }
+
+    @Override
+    public List<CustomImage> getAvailableImages() {
+        return this.customImageRepository.findAll().stream().map(this::toDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<CustomImage> findById(UUID id) {
+        return this.customImageRepository.findById(id).map(this::toDTO);
+    }
+
+    private CustomImage toDTO(CustomImageEntity customImageEntity) {
+        return new CustomImage(customImageEntity.getId(), customImageEntity.getLabel(), customImageEntity.getFileName());
+    }
+}
