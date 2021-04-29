350d5e7bc11ddfd548d524eeb608e0959a1fe435
Pierre-Charles David <pierre-charles.david@obeo.fr>
2021-04-29 16:42:04 +0200

    [441] Add service layer for custom images
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/441
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ backend/sirius-web-services-api/src/main/java/org/eclipse/sirius/web/services/api/images/CustomImage.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.services.api.images;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import org.eclipse.sirius.web.annotations.Immutable;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLField;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLID;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLNonNull;
+import org.eclipse.sirius.web.annotations.graphql.GraphQLObjectType;
+
+/**
+ * Lightweight DTO representing a custom image (without the content).
+ *
+ * @author pcdavid
+ */
+@Immutable
+@GraphQLObjectType
+public class CustomImage {
+    private UUID id;
+
+    private String label;
+
+    private String fileName;
+
+    public CustomImage(UUID id, String label, String fileName) {
+        this.id = Objects.requireNonNull(id);
+        this.label = Objects.requireNonNull(label);
+        this.fileName = Objects.requireNonNull(fileName);
+    }
+
+    @GraphQLID
+    @GraphQLField
+    @GraphQLNonNull
+    public UUID getId() {
+        return this.id;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public String getLabel() {
+        return this.label;
+    }
+
+    @GraphQLField
+    @GraphQLNonNull
+    public String getFileName() {
+        return this.fileName;
+    }
+
+}
---
+++ backend/sirius-web-services-api/src/main/java/org/eclipse/sirius/web/services/api/images/ICustomImagesService.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.services.api.images;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Service layer for custom images.
+ *
+ * @author pcdavid
+ */
+public interface ICustomImagesService {
+    Optional<CustomImage> findById(UUID id);
+
+    Optional<byte[]> getImageContentsByFileName(String fileName);
+
+    List<CustomImage> getAvailableImages();
+
+    /**
+     * Null implementation.
+     *
+     * @author pcdavid
+     */
+    class NoOp implements ICustomImagesService {
+        @Override
+        public Optional<byte[]> getImageContentsByFileName(String fileName) {
+            return Optional.empty();
+        }
+
+        @Override
+        public List<CustomImage> getAvailableImages() {
+            return List.of();
+        }
+
+        @Override
+        public Optional<CustomImage> findById(UUID id) {
+            return Optional.empty();
+        }
+    }
+}
