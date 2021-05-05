2d0e1afb8a78d9e71de9121f7bfae88e67ce14b8
Pierre-Charles David <pierre-charles.david@obeo.fr>
2021-05-05 17:35:03 +0200

    [243] Remove CreationMode in View DSL
    Until it's actually supported in a meaningfull way it is more
    confusing than it's worth  to expose it in the UI.
    
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/243
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- backend/sirius-web-view/src/main/java/org/eclipse/sirius/web/view/impl/ViewFactoryImpl.java
+++ backend/sirius-web-view/src/main/java/org/eclipse/sirius/web/view/impl/ViewFactoryImpl.java
@@ -13,14 +13,12 @@
 package org.eclipse.sirius.web.view.impl;
 
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EDataType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.sirius.web.view.DiagramDescription;
 import org.eclipse.sirius.web.view.EdgeDescription;
-import org.eclipse.sirius.web.view.Mode;
 import org.eclipse.sirius.web.view.NodeDescription;
 import org.eclipse.sirius.web.view.NodeStyle;
 import org.eclipse.sirius.web.view.Style;
@@ -91,36 +89,6 @@ public class ViewFactoryImpl extends EFactoryImpl implements ViewFactory {
      * @generated
      */
     @Override
-    public Object createFromString(EDataType eDataType, String initialValue) {
-        switch (eDataType.getClassifierID()) {
-        case ViewPackage.MODE:
-            return this.createModeFromString(eDataType, initialValue);
-        default:
-            throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier"); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-    }
-
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    @Override
-    public String convertToString(EDataType eDataType, Object instanceValue) {
-        switch (eDataType.getClassifierID()) {
-        case ViewPackage.MODE:
-            return this.convertModeToString(eDataType, instanceValue);
-        default:
-            throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier"); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-    }
-
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    @Override
     public View createView() {
         ViewImpl view = new ViewImpl();
         return view;
@@ -179,27 +147,6 @@ public class ViewFactoryImpl extends EFactoryImpl implements ViewFactory {
     public NodeStyle createNodeStyle() {
         NodeStyleImpl nodeStyle = new NodeStyleImpl();
         return nodeStyle;
-    }
-
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    public Mode createModeFromString(EDataType eDataType, String initialValue) {
-        Mode result = Mode.get(initialValue);
-        if (result == null)
-            throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        return result;
-    }
-
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    public String convertModeToString(EDataType eDataType, Object instanceValue) {
-        return instanceValue == null ? null : instanceValue.toString();
     }
 
     /**
