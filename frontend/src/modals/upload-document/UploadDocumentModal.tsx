5c8cb19020a5bf576a4b46356d4e95f270ca5b1c
Guillaume Coutable <guillaume.coutable@obeo.fr>
2021-06-18 16:13:24 +0200

    [enh] Improve UploadModelModal
    Migrates the UploadModelModal to MUI & Xstate
    
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- frontend/src/modals/upload-document/UploadDocumentModal.tsx
+++ frontend/src/modals/upload-document/UploadDocumentModal.tsx
@@ -10,22 +10,31 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import { useMachine } from '@xstate/react';
 import { sendFile } from 'common/sendFile';
-import { ActionButton, Buttons } from 'core/button/Button';
 import { FileUpload } from 'core/file-upload/FileUpload';
 import { Form } from 'core/form/Form';
 import gql from 'graphql-tag';
-import { Modal } from 'modals/Modal';
-import {
-  HANDLE_SELECTED_ACTION,
-  HANDLE_SUBMIT_ACTION,
-  SELECTED_STATE,
-  SUBMIT_SUCCESS_STATE,
-} from 'modals/upload-document/machine';
-import { initialState, reducer } from 'modals/upload-document/reducer';
-import PropTypes from 'prop-types';
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
+import { GQLErrorPayload, GQLUploadDocumentPayload, UploadDocumentModalProps } from './UploadDocumentModal.types';
+import {
+  HandleResponseEvent,
+  HideToastEvent,
+  RequestDocumentUploadingEvent,
+  SchemaValue,
+  SelectDocumentEvent,
+  ShowToastEvent,
+  UploadDocumentModalContext,
+  UploadDocumentModalEvent,
+  uploadDocumentModalMachine,
+} from './UploadDocumentModalMachine';
 
 const uploadDocumentMutationFile = gql`
   mutation uploadDocument($input: UploadDocumentInput!) {
@@ -41,21 +50,24 @@ const uploadDocumentMutationFile = gql`
       }
     }
   }
-`;
+`.loc.source.body;
 
-const propTypes = {
-  projectId: PropTypes.string.isRequired,
-  onDocumentUploaded: PropTypes.func.isRequired,
-  onClose: PropTypes.func.isRequired,
-};
-export const UploadDocumentModal = ({ projectId, onDocumentUploaded, onClose }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { viewState, file } = state;
+const isErrorPayload = (payload: GQLUploadDocumentPayload): payload is GQLErrorPayload =>
+  payload.__typename === 'ErrorPayload';
+export const UploadDocumentModal = ({ projectId, onDocumentUploaded, onClose }: UploadDocumentModalProps) => {
+  const [{ value, context }, dispatch] = useMachine<UploadDocumentModalContext, UploadDocumentModalEvent>(
+    uploadDocumentModalMachine
+  );
+
+  const { toast, uploadDocumentModal } = value as SchemaValue;
+  const { file, message } = context;
 
   // Execute the upload of a document and redirect to the newly created document
-  const uploadDocument = async (event) => {
+  const uploadDocument = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const requestDocumentUploadingEvent: RequestDocumentUploadingEvent = { type: 'REQUEST_DOCUMENT_UPLOADING' };
+    dispatch(requestDocumentUploadingEvent);
+
     event.preventDefault();
-    const uploadDocumentMutation = uploadDocumentMutationFile.loc.source.body;
     const variables = {
       input: {
         id: uuid(),
@@ -64,38 +76,87 @@ export const UploadDocumentModal = ({ projectId, onDocumentUploaded, onClose }) 
       },
     };
     try {
-      const response = await sendFile(uploadDocumentMutation, variables, file);
-      const action = { type: HANDLE_SUBMIT_ACTION, response };
-      dispatch(action);
+      const { data, error } = await sendFile(uploadDocumentMutationFile, variables, file);
+      if (error) {
+        const showToastEvent: ShowToastEvent = {
+          type: 'SHOW_TOAST',
+          message: error.message,
+        };
+        dispatch(showToastEvent);
+      }
+      if (data) {
+        const event: HandleResponseEvent = { type: 'HANDLE_RESPONSE', data };
+        dispatch(event);
+
+        const { uploadDocument } = data;
+        if (isErrorPayload(uploadDocument)) {
+          const { message } = uploadDocument;
+          const showToastEvent: ShowToastEvent = { type: 'SHOW_TOAST', message };
+          dispatch(showToastEvent);
+        }
+      }
     } catch (exception) {
       // Handle other errors like max file size error send by the backend...
-      const action = { type: HANDLE_SUBMIT_ACTION, response: { error: exception.toString() } };
-      dispatch(action);
+      const showToastEvent: ShowToastEvent = { type: 'SHOW_TOAST', message: exception.toString() };
+      dispatch(showToastEvent);
     }
   };
 
   const onFileSelected = (file) => {
-    const action = { type: HANDLE_SELECTED_ACTION, file };
-    dispatch(action);
+    const selectDocumentEvent: SelectDocumentEvent = { type: 'SELECT_DOCUMENT', file };
+    dispatch(selectDocumentEvent);
   };
 
   useEffect(() => {
-    if (viewState === SUBMIT_SUCCESS_STATE) {
+    if (uploadDocumentModal === 'success') {
       onDocumentUploaded();
     }
-  }, [viewState, onDocumentUploaded]);
+  }, [uploadDocumentModal, onDocumentUploaded]);
 
-  const canSubmit = viewState === SELECTED_STATE;
+  const canSubmit = uploadDocumentModal === 'documentSelected';
   return (
-    <Modal title="Upload new model" onClose={onClose}>
-      <Form onSubmit={uploadDocument} encType="multipart/form-data">
-        <FileUpload onFileSelected={onFileSelected} data-testid="file" />
-
-        <Buttons>
-          <ActionButton type="submit" disabled={!canSubmit} label="Upload" data-testid="upload-document" />
-        </Buttons>
-      </Form>
-    </Modal>
+    <>
+      <Dialog open={true} onClose={onClose} aria-labelledby="dialog-title" fullWidth>
+        <DialogTitle id="dialog-title">Upload new model</DialogTitle>
+        <DialogContent>
+          <Form id="upload-form-id" onSubmit={uploadDocument} encType="multipart/form-data">
+            <FormGroup>
+              <FileUpload onFileSelected={onFileSelected} data-testid="file" />
+            </FormGroup>
+          </Form>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            disabled={!canSubmit}
+            color="primary"
+            type="submit"
+            form="upload-form-id"
+            data-testid="upload-document">
+            Upload
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        open={toast === 'visible'}
+        autoHideDuration={3000}
+        onClose={() => dispatch({ type: 'HIDE_TOAST' } as HideToastEvent)}
+        message={message}
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={() => dispatch({ type: 'HIDE_TOAST' } as HideToastEvent)}>
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+        data-testid="error"
+      />
+    </>
   );
 };
-UploadDocumentModal.propTypes = propTypes;
