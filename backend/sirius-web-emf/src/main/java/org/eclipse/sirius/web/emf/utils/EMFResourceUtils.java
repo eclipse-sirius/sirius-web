83d4f274dbb7af0c516b2abb1762f4c352e17184
Pierre-Charles David <pierre-charles.david@obeo.fr>
2021-07-16 15:00:01 +0200

    [cleanup] Disable potentially insecure XML features
    Configure the XML parser used to parse uploaded documents (user
    controlled) to disallow some potentially insecure features.
    
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- backend/sirius-web-emf/src/main/java/org/eclipse/sirius/web/emf/utils/EMFResourceUtils.java
+++ backend/sirius-web-emf/src/main/java/org/eclipse/sirius/web/emf/utils/EMFResourceUtils.java
@@ -27,17 +27,33 @@ import org.eclipse.sirius.emfjson.resource.JsonResource;
  */
 public class EMFResourceUtils {
 
-    public Map<String, Object> getFastXMILoadOptions() {
-        return this.getFastXMILoadOptions(new XMLParserPoolImpl());
+    /**
+     * Disable potentially insecure XML features.
+     *
+     * @see https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html
+     * @see https://xerces.apache.org/xerces2-j/features.html
+     */
+    // @formatter:off
+    private static final Map<String, Boolean> SAFE_SAX_PARSER_FEATURES = Map.of(
+            "http://xml.org/sax/features/external-general-entities", false, //$NON-NLS-1$
+            "http://xml.org/sax/features/external-parameter-entities", false, //$NON-NLS-1$
+            "http://apache.org/xml/features/nonvalidating/load-external-dtd", false, //$NON-NLS-1$
+            "http://apache.org/xml/features/disallow-doctype-decl", true); //$NON-NLS-1$
+    // @formatter:on
+
+    public Map<String, Object> getXMILoadOptions() {
+        return this.getXMILoadOptions(new XMLParserPoolImpl());
     }
 
-    public Map<String, Object> getFastXMILoadOptions(XMLParserPool parserPool) {
+    public Map<String, Object> getXMILoadOptions(XMLParserPool parserPool) {
         Map<String, Object> options = new HashMap<>();
         options.put(XMLResource.OPTION_DEFER_ATTACHMENT, Boolean.TRUE);
         options.put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, Boolean.TRUE);
         options.put(XMLResource.OPTION_USE_DEPRECATED_METHODS, Boolean.TRUE);
         options.put(XMLResource.OPTION_USE_PARSER_POOL, parserPool);
         options.put(XMLResource.OPTION_USE_XML_NAME_TO_FEATURE_MAP, new HashMap<>());
+        options.put(XMLResource.OPTION_USE_PACKAGE_NS_URI_AS_LOCATION, Boolean.FALSE);
+        options.put(XMLResource.OPTION_PARSER_FEATURES, SAFE_SAX_PARSER_FEATURES);
         return options;
     }
 
