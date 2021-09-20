kchobantonov/sirius-components

[132] Add a drop handler
Bug: https://github.com/eclipse-sirius/sirius-components/issues/132
Signed-off-by: Hugo Marchadour <hugo.marchadour@obeo.fr>

frontend/src/index.ts
@@ -12,12 +12,14 @@
  *******************************************************************************/
 // Required because Sprotty uses Inversify and both frameworks are written in TypeScript with experimental features.
 import 'reflect-metadata';
+export * from './common/drop';
 export * from './common/sendFile';
 export * from './common/ServerContext';
 export * from './common/URL';
 export * from './core/contextmenu/ContextMenu';
 export * from './core/file-upload/FileUpload';
 export * from './core/form/Form';
+export * from './core/panels/Panels';
 export * from './diagram/DiagramWebSocketContainer';
 export * from './diagram/DiagramWebSocketContainer.types';
 export * from './errors/ErrorBoundary';

eclipse-sirius/sirius-web

frontend/src/index.ts
@@ -12,12 +12,14 @@
  *******************************************************************************/
 // Required because Sprotty uses Inversify and both frameworks are written in TypeScript with experimental features.
 import 'reflect-metadata';
+export * from './common/dataTransferTypes';
 export * from './common/sendFile';
 export * from './common/ServerContext';
 export * from './common/URL';
 export * from './core/contextmenu/ContextMenu';
 export * from './core/file-upload/FileUpload';
 export * from './core/form/Form';
+export * from './core/panels/Panels';
 export * from './diagram/DiagramWebSocketContainer';
 export * from './diagram/DiagramWebSocketContainer.types';
 export * from './errors/ErrorBoundary';