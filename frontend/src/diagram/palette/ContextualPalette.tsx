4affce60fc55bd69495ebb9aa1a24f854257567c
Guillaume Coutable <guillaume.coutable@obeo.fr>
2022-01-12 14:16:02 +0100

    [952] The palette can use many lines
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/952
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- frontend/src/diagram/palette/ContextualPalette.tsx
+++ frontend/src/diagram/palette/ContextualPalette.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Obeo.
+ * Copyright (c) 2019, 2022 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,22 +10,22 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+import { makeStyles } from '@material-ui/core/styles';
 import {
   GQLDeletionPolicy,
   GQLDiagramDescription,
   GQLEdgeDescription,
   GQLNodeDescription,
+  ToolSection as ToolSectionType,
 } from 'diagram/DiagramWebSocketContainer.types';
 import { ContextualPaletteProps } from 'diagram/palette/ContextualPalette.types';
 import { ToolSection } from 'diagram/palette/tool-section/ToolSection';
-import { ToolSeparator } from 'diagram/palette/tool-separator/ToolSeparator';
 import { Tool } from 'diagram/palette/tool/Tool';
 import { Node } from 'diagram/sprotty/Diagram.types';
 import { isContextualTool } from 'diagram/toolServices';
 import { GQLSynchronizationPolicy } from 'index';
 import React from 'react';
 import { SEdge, SLabel, SModelElement } from 'sprotty';
-import styles from './ContextualPalette.module.css';
 import closeImagePath from './icons/close.svg';
 import connectorImagePath from './icons/connector.svg';
 import editImagePath from './icons/edit.svg';
@@ -62,6 +62,46 @@ const closeTool = {
   imageURL: closeImagePath,
   label: 'Close',
 };
+
+const useContextualPaletteStyle = makeStyles((theme) => ({
+  toolbar: {
+    background: theme.palette.background.paper,
+    border: '1px solid #d1dadb',
+    boxShadow: '0px 2px 5px #002b3c40',
+    borderRadius: '2px',
+    display: 'grid',
+    gridTemplateRows: '8px 1fr 8px',
+    gridTemplateColumns: '8px 1fr 8px',
+    position: 'fixed',
+    zIndex: 2,
+  },
+  toolEntries: {
+    gridColumnStart: 2,
+    gridRowStart: 2,
+    display: 'grid',
+    gridTemplateColumns: (props: { itemsPerLine: number }) => `repeat(${props.itemsPerLine}, 1fr)`,
+    gridTemplateRows: '1fr',
+  },
+  toolLine: {
+    display: 'flex',
+  },
+  toolEntry: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '0 0px 0 8px',
+  },
+  toolSectionEntry: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  toolbarClose: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 
 const isSynchronized = (diagramDescription: GQLDiagramDescription, element: SModelElement): boolean => {
   let elementWithTarget = element;
@@ -133,6 +173,8 @@ const findEdgeDescription = (
   return diagramDescription.edgeDescriptions.filter((edgeDescription) => edgeDescription.id === edgeDescriptionId)[0];
 };
 
+const TOOL_PER_LINE = 15;
+
 /**
  * The component used to display a Contextual Palette.
  *
@@ -148,13 +190,8 @@ export const ContextualPalette = ({
   invokeDelete,
   invokeClose,
 }: ContextualPaletteProps) => {
-  let connectorToolContent;
-  let connectorToolSeparator;
-  let toolSectionsContent;
-
   const isTargetElementSynchronized = isSynchronized(diagramDescription, targetElement);
-
-  const contextualToolSections = [];
+  const contextualToolSections: ToolSectionType[] = [];
   // Keep only create node/edge tools
   toolSections.forEach((toolSection) => {
     const filteredTools = toolSection.tools.filter((tool) => isContextualTool(tool, targetElement));
@@ -169,36 +206,42 @@ export const ContextualPalette = ({
     }
   });
 
-  if (contextualToolSections.length > 0) {
-    const atLeastOneEdgeTool = contextualToolSections.some((toolSection) =>
-      toolSection.tools.some((tool) => tool.__typename === 'CreateEdgeTool')
+  const atLeastOneEdgeTool = contextualToolSections.some((toolSection) =>
+    toolSection.tools.some((tool) => tool.__typename === 'CreateEdgeTool')
+  );
+
+  // Find how many items should be display per line.
+  let itemsPerLine = getNumberOfColumns(contextualToolSections, !!invokeLabelEdit, !!invokeDelete, atLeastOneEdgeTool);
+  const props = { itemsPerLine };
+  const classes = useContextualPaletteStyle(props);
+
+  let connectorToolContent: JSX.Element;
+  if (atLeastOneEdgeTool) {
+    connectorToolContent = (
+      <div className={classes.toolEntry}>
+        <Tool
+          tool={connectorTool}
+          thumbnail={true}
+          onClick={() => {
+            invokeConnectorTool();
+          }}
+        />
+      </div>
     );
-    if (atLeastOneEdgeTool) {
-      connectorToolContent = (
-        <div className={styles.toolEntry}>
-          <Tool
-            tool={connectorTool}
-            thumbnail={true}
-            onClick={() => {
-              invokeConnectorTool();
-            }}
-          />
-        </div>
-      );
-      connectorToolSeparator = <ToolSeparator />;
-    }
-    toolSectionsContent = contextualToolSections.map((toolSection) => {
-      return (
-        <div className={styles.toolSectionEntry} key={targetElement.id + toolSection.id}>
-          <ToolSection toolSection={toolSection} onToolClick={invokeTool} />
-        </div>
-      );
-    });
   }
+
+  const toolSectionsContent: JSX.Element[] = contextualToolSections.map((toolSection) => {
+    return (
+      <div className={classes.toolSectionEntry} key={targetElement.id + toolSection.id}>
+        <ToolSection toolSection={toolSection} onToolClick={invokeTool} />
+      </div>
+    );
+  });
+
   let renameEntry;
   if (invokeLabelEdit) {
     renameEntry = (
-      <div className={styles.toolEntry}>
+      <div className={classes.toolEntry}>
         <Tool tool={editTool} thumbnail={true} onClick={() => invokeLabelEdit()} />
       </div>
     );
@@ -207,54 +250,113 @@ export const ContextualPalette = ({
   if (invokeDelete) {
     if (isTargetElementSynchronized) {
       deleteEntry = (
-        <div className={styles.toolEntry}>
+        <div className={classes.toolEntry}>
           <Tool tool={semanticDeleteTool} thumbnail={true} onClick={() => invokeDelete(GQLDeletionPolicy.SEMANTIC)} />
         </div>
       );
     } else {
       deleteEntry = (
         <>
-          <div className={styles.toolEntry}>
+          <div className={classes.toolEntry}>
             <Tool
               tool={graphicalDeleteTool}
               thumbnail={true}
               onClick={() => invokeDelete(GQLDeletionPolicy.GRAPHICAL)}
             />
           </div>
-          <div className={styles.toolEntry}>
+          <div className={classes.toolEntry}>
             <Tool tool={semanticDeleteTool} thumbnail={true} onClick={() => invokeDelete(GQLDeletionPolicy.SEMANTIC)} />
           </div>
         </>
       );
     }
   }
-  let separator;
-  if (toolSectionsContent && (invokeLabelEdit || invokeDelete)) {
-    separator = <ToolSeparator />;
-  }
   const closeEntry = (
-    <div className={styles.toolEntry}>
+    <div className={classes.toolEntry}>
       <Tool tool={closeTool} thumbnail={true} onClick={() => invokeClose()} />
     </div>
   );
+
+  /*
+   * The palette first line is composed of 15 elements:
+   *   - the connector tool first, if at leat one edge tool is defined.
+   *   - in the last position of the line are in order, the direct edit tool if it exists,
+   * the delete tool if it exists and the close button.
+   *   - The remaing elements are tool sections
+   *
+   * All other lines are composed of remaining tool sections.
+   */
+  const toolPaletteContent: JSX.Element[] = [];
+  if ((!!toolSectionsContent && toolSectionsContent.length > 0) || invokeLabelEdit || invokeDelete) {
+    // Fill the first line
+    const firstLine: JSX.Element[] = [];
+    let firstlineToolElementToTake = TOOL_PER_LINE - 1;
+    if (!!invokeLabelEdit) {
+      firstlineToolElementToTake--;
+    }
+    if (!!invokeDelete) {
+      firstlineToolElementToTake--;
+    }
+    if (!!connectorToolContent) {
+      firstlineToolElementToTake--;
+      firstLine.push(connectorToolContent);
+    }
+
+    if (!!toolSectionsContent && toolSectionsContent.length > 0) {
+      const firstElements = toolSectionsContent.splice(0, firstlineToolElementToTake);
+      firstLine.push(...firstElements);
+    }
+
+    if (!!invokeLabelEdit) {
+      firstLine.push(renameEntry);
+    }
+    if (!!invokeDelete) {
+      firstLine.push(deleteEntry);
+    }
+    firstLine.push(closeEntry);
+    toolPaletteContent.push(<>{firstLine}</>);
+
+    // Fill other lines
+    while (!!toolSectionsContent && toolSectionsContent.length > 0) {
+      const line = toolSectionsContent.splice(0, TOOL_PER_LINE);
+      toolPaletteContent.push(<>{line}</>);
+    }
+  }
+
   let result = <></>;
-  if (toolSectionsContent || invokeLabelEdit || invokeDelete) {
+  if (toolPaletteContent.length > 0) {
     result = (
       <>
-        <div className={styles.toolbar} data-testid={`PopupToolbar`}>
-          <div className={styles.toolEntries}>
-            {connectorToolContent}
-            {connectorToolSeparator}
-            {toolSectionsContent}
-            {separator}
-            {renameEntry}
-            {deleteEntry}
-            <ToolSeparator />
-            {closeEntry}
-          </div>
+        <div className={classes.toolbar} data-testid={`PopupToolbar`}>
+          <div className={classes.toolEntries}>{toolPaletteContent}</div>
         </div>
       </>
     );
   }
   return result;
+};
+
+const getNumberOfColumns = (
+  toolSections: ToolSectionType[],
+  hasDirectEdit: boolean,
+  hasDeleteTool: boolean,
+  hasAtLeastOneConnector: boolean
+): number => {
+  let numberOfColumns = toolSections.length + 1;
+
+  if (hasDirectEdit) {
+    numberOfColumns++;
+  }
+  if (hasDeleteTool) {
+    numberOfColumns++;
+  }
+  if (hasAtLeastOneConnector) {
+    numberOfColumns++;
+  }
+
+  if (numberOfColumns > TOOL_PER_LINE) {
+    numberOfColumns = TOOL_PER_LINE;
+  }
+
+  return numberOfColumns;
 };
