f44396b68a248978147fa1b14296eea258cc97db
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2022-03-16 17:46:13 +0100

    [1112] Add support for ctrl+click to select multiple elements in the explorer
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/1112
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- frontend/src/tree/Tree.tsx
+++ frontend/src/tree/Tree.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Obeo.
+ * Copyright (c) 2019, 2022 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -51,12 +51,12 @@ export const Tree = ({ editingContextId, tree, onExpand, selection, setSelection
               break;
             case 'ArrowUp':
               if (index > 0) {
-                treeItemDomElements[index - 1].focus();
+                treeItemDomElements[index - 1].click();
               }
               break;
             case 'ArrowDown':
               if (index < treeItemDomElements.length - 1) {
-                treeItemDomElements[index + 1].focus();
+                treeItemDomElements[index + 1].click();
               }
               break;
             default:
