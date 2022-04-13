878a8410021406832423c5f3820b1129169500d3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2022-04-13 11:15:41 +0200

    [cleanup] Remove support for configurable panel orientation
    We only use the horizontal oritentation, so no need to make the code
    more complex than required.
    
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- frontend/src/core/panels/Panels.tsx
+++ frontend/src/core/panels/Panels.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Obeo.
+ * Copyright (c) 2019, 2022 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -14,32 +14,24 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import styles from './Panels.module.css';
 
-export const HORIZONTAL = 'HORIZONTAL';
-export const VERTICAL = 'VERTICAL';
-
 export const FIRST_PANEL = 'FIRST_PANEL';
 export const SECOND_PANEL = 'SECOND_PANEL';
 
 const propTypes = {
-  orientation: PropTypes.oneOf([HORIZONTAL, VERTICAL]),
   resizablePanel: PropTypes.oneOf([FIRST_PANEL, SECOND_PANEL]),
   initialResizablePanelSize: PropTypes.number.isRequired,
 };
 const defaultProps = {
-  orientation: HORIZONTAL,
   resizablePanel: FIRST_PANEL,
 };
 
-export const Panels = ({ firstPanel, secondPanel, orientation, resizablePanel, initialResizablePanelSize }) => {
+export const Panels = ({ firstPanel, secondPanel, resizablePanel, initialResizablePanelSize }) => {
   const initialState = { isDragging: false, initialPosition: 0, resizablePanelSize: initialResizablePanelSize };
   const [state, setState] = useState(initialState);
   const { isDragging, resizablePanelSize } = state;
 
   const startResize = (event) => {
     let initialPosition = event.clientX;
-    if (orientation === VERTICAL) {
-      initialPosition = event.clientY;
-    }
     setState((prevState) => {
       return { isDragging: true, initialPosition, resizablePanelSize: prevState.resizablePanelSize };
     });
@@ -48,9 +40,6 @@ export const Panels = ({ firstPanel, secondPanel, orientation, resizablePanel, i
   const resizePanel = (event) => {
     if (isDragging) {
       let initialPosition = event.clientX;
-      if (orientation === VERTICAL) {
-        initialPosition = event.clientY;
-      }
       setState((prevState) => {
         const delta = initialPosition - prevState.initialPosition;
         let resizablePanelSize = prevState.resizablePanelSize + delta;
@@ -82,25 +71,8 @@ export const Panels = ({ firstPanel, secondPanel, orientation, resizablePanel, i
       gridTemplateColumns: `minmax(0, 1fr) min-content ${resizablePanelSize}px`,
     };
   }
-  if (orientation === VERTICAL) {
-    style = {
-      display: 'grid',
-      gridTemplateRows: `${resizablePanelSize}px min-content minmax(0, 1fr)`,
-      gridTemplateColumns: 'minmax(0, 1fr)',
-    };
-    if (resizablePanel === SECOND_PANEL) {
-      style = {
-        display: 'grid',
-        gridTemplateRows: `minmax(0, 1fr) min-content ${resizablePanelSize}px`,
-        gridTemplateColumns: 'minmax(0, 1fr)',
-      };
-    }
-  }
 
   let resizerClassName = styles.verticalResizer;
-  if (orientation === VERTICAL) {
-    resizerClassName = styles.horizontalResizer;
-  }
   return (
     <div style={style} onMouseMove={resizePanel} onMouseUp={stopResize} onMouseLeave={stopResize}>
       <div className={styles.panel}>{firstPanel}</div>
