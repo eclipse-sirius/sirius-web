a61ba89b6e547aaaae661f0e1387be10895eff1d
Axel RICHARD <axel.richard@obeo.fr>
2022-06-09 12:11:33 +0200

    [1260] Fix download fails when model contains a FormDescriptionEditor
    The download action on a Project fails when one of its model contains a
    Form Description Editor.
    
    Fix by implementing an
    o.e.s.c.collaborative.api.IRepresentationDeserializer for the
    FormDescriptionEditor.
    
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/1260
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ backend/sirius-components-collaborative-formdescriptioneditors/src/main/java/org/eclipse/sirius/components/collaborative/formdescriptioneditors/services/FormDescriptionEditorDeserializer.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.components.collaborative.formdescriptioneditors.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Optional;
+
+import org.eclipse.sirius.components.collaborative.api.IRepresentationDeserializer;
+import org.eclipse.sirius.components.formdescriptioneditors.FormDescriptionEditor;
+import org.eclipse.sirius.components.representations.IRepresentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Used to deserialize a form description editor.
+ *
+ * @author arichard
+ */
+@Service
+public class FormDescriptionEditorDeserializer implements IRepresentationDeserializer {
+
+    private final Logger logger = LoggerFactory.getLogger(FormDescriptionEditorDeserializer.class);
+
+    @Override
+    public boolean canHandle(ObjectNode root) {
+        // @formatter:off
+        return Optional.ofNullable(root.get("kind")) //$NON-NLS-1$
+                .map(JsonNode::asText)
+                .filter(FormDescriptionEditor.KIND::equals)
+                .isPresent();
+        // @formatter:on
+    }
+
+    @Override
+    public Optional<IRepresentation> handle(ObjectMapper mapper, ObjectNode root) {
+        try {
+            return Optional.of(mapper.readValue(root.toString(), FormDescriptionEditor.class));
+        } catch (JsonProcessingException exception) {
+            this.logger.warn(exception.getMessage(), exception);
+        }
+
+        return Optional.empty();
+    }
+
+}
