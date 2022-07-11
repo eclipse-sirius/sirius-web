6b956a02a82db318e52d5f0fd1843770d2f6a538
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2022-07-11 15:07:47 +0200

    [fix] Fix an issue with iconURL preventing the representations view from working
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- backend/sirius-components-forms/src/main/java/org/eclipse/sirius/components/forms/renderer/FormElementFactory.java
+++ backend/sirius-components-forms/src/main/java/org/eclipse/sirius/components/forms/renderer/FormElementFactory.java
@@ -212,7 +212,6 @@ public class FormElementFactory implements IElementFactory {
         // @formatter:off
         Builder listBuilder = org.eclipse.sirius.components.forms.List.newList(props.getId())
                 .label(props.getLabel())
-                .iconURL(props.getIconURL())
                 .items(props.getItems())
                 .diagnostics(diagnostics);
         // @formatter:on
