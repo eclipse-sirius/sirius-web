8cdbe115d269071cdaf7b5f34bb1a42784a44c69
Axel RICHARD <axel.richard@obeo.fr>
2023-02-21 09:54:08 +0100

    [1674] Fix fade/hide tools are available on the diagram background
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/1674
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ integration-tests/cypress/integration/project/edit/diagram-contextual-palette_spec.js
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+describe('/projects/:projectId/edit - Robot Diagram', () => {
+  beforeEach(() => {
+    cy.deleteAllProjects();
+    cy.createProject('Cypress Project').then((res) => {
+      const projectId = res.body.data.createProject.project.id;
+      const robot_flow_id = 'c26b6086-b444-3ee6-b8cd-9a4fde5956a7';
+      cy.createDocument(projectId, robot_flow_id, 'flow').then(() => {
+        cy.visit(`/projects/${projectId}/edit`);
+      });
+    });
+
+    cy.getByTestId('flow').dblclick();
+    cy.getByTestId('Robot').dblclick();
+    cy.getByTestId('Robot-more').click();
+    cy.getByTestId('treeitem-contextmenu').findByTestId('new-representation').click();
+
+    cy.getByTestId('name').clear();
+    cy.getByTestId('name').type('Topography');
+    cy.getByTestId('representationDescription').click();
+    cy.getByTestId('Topography with auto layout').click();
+    cy.getByTestId('create-representation').click();
+    cy.getByTestId('Topography').click();
+  });
+
+  it('test Hide/Fade action is not available on diagram background', () => {
+    cy.getByTestId('Rectangle - CaptureSubSystem').click();
+    cy.getByTestId('visibilitySection').should('exist');
+    cy.getByTestId('Diagram').click();
+    cy.getByTestId('visibilitySection').should('not.exist');
+  });
+});
