d04836c3b8a5bfc12795714cd7987a8498bdd1b7
Axel RICHARD <axel.richard@obeo.fr>
2023-03-01 17:30:37 +0100

    [619] Fix order of the models in the explorer
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/619
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/trees/Tree.tsx
+++ packages/trees/frontend/sirius-components-trees/src/trees/Tree.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 Obeo.
+ * Copyright (c) 2019, 2023 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -77,7 +77,7 @@ export const Tree = ({ editingContextId, tree, onExpand, selection, setSelection
 
   return (
     <div ref={treeElement}>
-      <ul>
+      <ul data-testid="tree-root-elements">
         {tree.children.map((item) => (
           <li key={item.id}>
             <TreeItem
