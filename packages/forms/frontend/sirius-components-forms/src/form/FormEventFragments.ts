f04a0b28b47ef6fd342a1cedf9112b79ab8968f5
Axel RICHARD <axel.richard@obeo.fr>
2023-03-09 15:46:36 +0100

    [1785] Fix name collision on labelFields GraphQL fragment
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/1785
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- packages/forms/frontend/sirius-components-forms/src/form/FormEventFragments.ts
+++ packages/forms/frontend/sirius-components-forms/src/form/FormEventFragments.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2022 Obeo.
+ * Copyright (c) 2021, 2023 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -256,7 +256,7 @@ export const widgetFields = `
     }
   } 
 
-  fragment labelFields on LabelWidget {
+  fragment labelWidgetFields on LabelWidget {
     label
     stringValue: value
     style {
@@ -329,7 +329,7 @@ export const widgetFields = `
       ...toolbarActionFields
     }
     ... on LabelWidget {
-      ...labelFields
+      ...labelWidgetFields
     }
     ... on ChartWidget {
       ...chartWidgetFields
