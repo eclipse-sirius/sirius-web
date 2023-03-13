67057669c48104a7c0990db615c5ef675323b7d3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2023-03-13 19:23:03 +0100

    [cleanup] Remove useless requireNonNull for primitives
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/diagrams/backend/sirius-components-diagrams/src/main/java/org/eclipse/sirius/components/diagrams/ImageNodeStyle.java
+++ packages/diagrams/backend/sirius-components-diagrams/src/main/java/org/eclipse/sirius/components/diagrams/ImageNodeStyle.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 Obeo.
+ * Copyright (c) 2019, 2023 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -104,7 +104,7 @@ public final class ImageNodeStyle implements INodeStyle {
         }
 
         public Builder scalingFactor(int scalingFactor) {
-            this.scalingFactor = Objects.requireNonNull(scalingFactor);
+            this.scalingFactor = scalingFactor;
             return this;
         }
 
