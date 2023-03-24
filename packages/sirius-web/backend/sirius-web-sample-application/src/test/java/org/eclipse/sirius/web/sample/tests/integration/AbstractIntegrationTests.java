b0311b02d28bc430e0b331fabcfb7640e4eb52f8
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2023-03-24 15:53:13 +0100

    [test] Add some integrations tests in sirius-web-sample-application
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ packages/sirius-web/backend/sirius-web-sample-application/src/test/java/org/eclipse/sirius/web/sample/tests/integration/AbstractIntegrationTests.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.sample.tests.integration;
+
+import org.eclipse.sirius.web.sample.configuration.JPAConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+/**
+ * Superclass of all the integration tests used to setup the PostgreSQL docker container.
+ *
+ * @author sbegaudeau
+ */
+@SpringJUnitConfig(classes = { IntegrationTestConfiguration.class, JPAConfiguration.class })
+public abstract class AbstractIntegrationTests {
+    public static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+
+    static {
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:latest").withReuse(true);
+        POSTGRESQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    public static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+    }
+}
