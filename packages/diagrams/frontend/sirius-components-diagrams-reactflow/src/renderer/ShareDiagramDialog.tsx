4d379097c4cb1064639a0df8d2a8cf94012239bb
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2023-06-22 15:51:30 +0200

    [2086] Add a panel with the default diagram actions
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/2086
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ packages/diagrams/frontend/sirius-components-diagrams-reactflow/src/renderer/ShareDiagramDialog.tsx
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Obeo and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { ShareDiagramDialogProps } from './ShareDiagramDialog.types';
+
+export const ShareDiagramDialog = ({ onClose }: ShareDiagramDialogProps) => {
+  const refCallback = (node: HTMLElement) => {
+    if (node !== null) {
+      var range = document.createRange();
+      range.selectNodeContents(node);
+      var selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
+
+  let title = 'Shareable link';
+  if (navigator.clipboard && document.hasFocus()) {
+    navigator.clipboard.writeText(window.location.href);
+    title += ' (copied into the clipboard)';
+  }
+
+  return (
+    <Dialog open onClose={onClose} aria-labelledby="dialog-title" fullWidth>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent ref={refCallback}>
+        <DialogContentText>{window.location.href}</DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
+};
