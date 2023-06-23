f747c0a44665237cb75e5da262ec498c4b2e5512
Guillaume Coutable <guillaume.coutable@obeo.fr>
2023-06-23 17:38:31 +0200

    [cleanup] Remove auto-generated EMF icons
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- packages/forms/backend/sirius-components-widget-reference-view-edit/src/main/java/org/eclipse/sirius/components/widgets/reference/provider/ReferenceWidgetDescriptionItemProvider.java
+++ packages/forms/backend/sirius-components-widget-reference-view-edit/src/main/java/org/eclipse/sirius/components/widgets/reference/provider/ReferenceWidgetDescriptionItemProvider.java
@@ -121,6 +121,16 @@ public class ReferenceWidgetDescriptionItemProvider extends WidgetDescriptionIte
     }
 
     /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected boolean shouldComposeCreationImage() {
+        return true;
+    }
+
+    /**
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
