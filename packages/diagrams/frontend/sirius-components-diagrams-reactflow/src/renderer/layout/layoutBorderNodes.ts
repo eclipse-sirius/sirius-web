ef92c0341ad32d95ea843d067b732d28accd4580
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2023-11-06 17:54:24 +0100

    [2532] Allows border to move on all parent node sizes
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/2532
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams-reactflow/src/renderer/layout/layoutBorderNodes.ts
+++ packages/diagrams/frontend/sirius-components-diagrams-reactflow/src/renderer/layout/layoutBorderNodes.ts
@@ -32,34 +32,10 @@ export const getBorderNodeExtent = (
 ): CoordinateExtent | 'parent' => {
   let coordinateExtent: CoordinateExtent | 'parent' = 'parent';
   if (nodeParent.width && nodeParent.height && borderNorde.height && borderNorde.width) {
-    switch (borderNorde.data.borderNodePosition) {
-      case BorderNodePositon.EAST:
-        coordinateExtent = [
-          [nodeParent.width, 0],
-          [nodeParent.width, nodeParent.height - borderNorde.height],
-        ];
-        break;
-      case BorderNodePositon.NORTH:
-        coordinateExtent = [
-          [0 - borderNorde.width * 0.8, 0 - borderNorde.height],
-          [nodeParent.width - borderNorde.width * 0.3, 0 - borderNorde.height],
-        ];
-        break;
-      case BorderNodePositon.SOUTH:
-        coordinateExtent = [
-          [0 - borderNorde.width * 0.8, nodeParent.height],
-          [nodeParent.width - borderNorde.width * 0.3, nodeParent.height],
-        ];
-        break;
-      case BorderNodePositon.WEST:
-        coordinateExtent = [
-          [0 - borderNorde.width, 0],
-          [0 - borderNorde.width, nodeParent.height - borderNorde.height],
-        ];
-        break;
-      default:
-        coordinateExtent = 'parent';
-    }
+    coordinateExtent = [
+      [0 - borderNorde.width, 0 - borderNorde.height],
+      [nodeParent.width, nodeParent.height],
+    ];
   }
   return coordinateExtent;
 };
