5faffdcb9467c7338f1eaf2e439fad744194487c
Axel RICHARD <axel.richard@obeo.fr>
2023-11-21 13:46:43 +0100

    [test] Add test to prevent the deletion of references to moved objects
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- packages/emf/backend/sirius-components-emf/src/test/java/org/eclipse/sirius/components/emf/services/EditingContextCrossReferenceAdapterTest.java
+++ packages/emf/backend/sirius-components-emf/src/test/java/org/eclipse/sirius/components/emf/services/EditingContextCrossReferenceAdapterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 CEA.
+ * Copyright (c) 2022, 2023 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -13,10 +13,14 @@
 package org.eclipse.sirius.components.emf.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.UUID;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcoreFactory;
@@ -40,7 +44,7 @@ public class EditingContextCrossReferenceAdapterTest {
     @Test
     public void testRemoveResource() {
         ResourceSet resourceSet = new ResourceSetImpl();
-        this.createModel(resourceSet);
+        this.createModelsForRemove(resourceSet);
 
         EditingContextCrossReferenceAdapter editingContextCrossReferenceAdapter = new EditingContextCrossReferenceAdapter();
         resourceSet.eAdapters().add(editingContextCrossReferenceAdapter);
@@ -54,13 +58,12 @@ public class EditingContextCrossReferenceAdapterTest {
         resourceSet.getResources().remove(this.referencedResource);
 
         this.checkClean(editingContextCrossReferenceAdapter);
-
     }
 
     @Test
     public void testRemoveReferencedEObject() {
         ResourceSet resourceSet = new ResourceSetImpl();
-        this.createModel(resourceSet);
+        this.createModelsForRemove(resourceSet);
 
         EditingContextCrossReferenceAdapter editingContextCrossReferenceAdapter = new EditingContextCrossReferenceAdapter();
         resourceSet.eAdapters().add(editingContextCrossReferenceAdapter);
@@ -74,7 +77,61 @@ public class EditingContextCrossReferenceAdapterTest {
         this.referencedClass.getEPackage().getEClassifiers().remove(this.referencedClass);
 
         this.checkClean(editingContextCrossReferenceAdapter);
+    }
 
+    @Test
+    public void testMoveEObject() {
+        ResourceSet resourceSet = new ResourceSetImpl();
+        Resource resource = this.createModelForMove(resourceSet);
+
+        EditingContextCrossReferenceAdapter editingContextCrossReferenceAdapter = new EditingContextCrossReferenceAdapter();
+        resourceSet.eAdapters().add(editingContextCrossReferenceAdapter);
+
+        EPackage ePackageRoot = resource.getContents().stream()
+            .filter(EPackage.class::isInstance)
+            .map(EPackage.class::cast)
+            .filter(ePkg -> "ePackageRoot".equals(ePkg.getName()))
+            .findFirst()
+            .orElse(null);
+        assertNotNull(ePackageRoot);
+
+        EPackage ePackageA = ePackageRoot.getESubpackages().stream()
+                .filter(ePkg -> "ePackageA".equals(ePkg.getName()))
+                .findFirst()
+                .orElse(null);
+        assertNotNull(ePackageA);
+
+        EClass eClassA1 = ePackageA.getEClassifiers().stream()
+                .filter(eClass -> "eClassA1".equals(eClass.getName()))
+                .filter(EClass.class::isInstance)
+                .map(EClass.class::cast)
+                .findFirst()
+                .orElse(null);
+        assertNotNull(eClassA1);
+        assertFalse(eClassA1.getESuperTypes().isEmpty());
+
+        EPackage ePackageB = ePackageRoot.getESubpackages().stream()
+                .filter(ePkg -> "ePackageB".equals(ePkg.getName()))
+                .findFirst()
+                .orElse(null);
+        assertNotNull(ePackageB);
+
+        EPackage ePackageC = ePackageA.getESubpackages().stream()
+                .filter(ePkg -> "ePackageC".equals(ePkg.getName()))
+                .findFirst()
+                .orElse(null);
+        assertNotNull(ePackageC);
+
+        EClassifier eClassC1 = ePackageC.getEClassifiers().stream()
+                .filter(eClass -> "eClassC1".equals(eClass.getName()))
+                .findFirst()
+                .orElse(null);
+        assertNotNull(eClassC1);
+
+        ePackageB.getESubpackages().add(ePackageC);
+
+        assertFalse(eClassA1.getESuperTypes().isEmpty());
+        assertEquals(ePackageC, eClassC1.getEPackage());
     }
 
     /**
@@ -88,8 +145,7 @@ public class EditingContextCrossReferenceAdapterTest {
         }).isEmpty();
     }
 
-    private void createModel(ResourceSet resourceSet) {
-
+    private void createModelsForRemove(ResourceSet resourceSet) {
         this.referencedResource = new JSONResourceFactory().createResourceFromPath(UUID.randomUUID().toString());
         resourceSet.getResources().add(this.referencedResource);
 
@@ -107,8 +163,39 @@ public class EditingContextCrossReferenceAdapterTest {
         EClass referencingClass = EcoreFactory.eINSTANCE.createEClass();
         ePackage.getEClassifiers().add(referencingClass);
         this.eReference = EcoreFactory.eINSTANCE.createEReference();
-        referencingClass.getEReferences().add(this.eReference);
+        referencingClass.getEStructuralFeatures().add(this.eReference);
         this.eReference.setEType(this.referencedClass);
     }
 
+    private Resource createModelForMove(ResourceSet resourceSet) {
+        Resource  resource = new JSONResourceFactory().createResourceFromPath(UUID.randomUUID().toString());
+        resourceSet.getResources().add(resource);
+
+        EPackage ePackageRoot = EcoreFactory.eINSTANCE.createEPackage();
+        ePackageRoot.setName("ePackageRoot");
+        resource.getContents().add(ePackageRoot);
+
+        EPackage ePackageA = EcoreFactory.eINSTANCE.createEPackage();
+        ePackageA.setName("ePackageA");
+        ePackageRoot.getESubpackages().add(ePackageA);
+
+        EClass eClassA1 = EcoreFactory.eINSTANCE.createEClass();
+        eClassA1.setName("eClassA1");
+        ePackageA.getEClassifiers().add(eClassA1);
+
+        EPackage ePackageB = EcoreFactory.eINSTANCE.createEPackage();
+        ePackageB.setName("ePackageB");
+        ePackageRoot.getESubpackages().add(ePackageB);
+
+        EPackage ePackageC = EcoreFactory.eINSTANCE.createEPackage();
+        ePackageC.setName("ePackageC");
+        ePackageA.getESubpackages().add(ePackageC);
+
+        EClass eClassC1 = EcoreFactory.eINSTANCE.createEClass();
+        eClassC1.setName("eClassC1");
+        ePackageC.getEClassifiers().add(eClassC1);
+        eClassA1.getESuperTypes().add(eClassC1);
+
+        return resource;
+    }
 }
