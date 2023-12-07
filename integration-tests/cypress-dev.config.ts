ade48d425092c3d0eb3e948ef49853673a2af198
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2023-12-07 14:53:51 +0100

    [test] Add support for typescript-eslint for integration tests
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- integration-tests/cypress-dev.config.ts
+++ integration-tests/cypress-dev.config.ts
@@ -26,9 +26,6 @@ export default defineConfig({
     baseAPIUrl: 'http://localhost:8080',
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:5173',
     experimentalRunAllSpecs: true,
   },
