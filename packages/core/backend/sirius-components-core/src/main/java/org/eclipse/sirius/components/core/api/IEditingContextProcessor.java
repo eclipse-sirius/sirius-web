724a013bd578420327e1ca94bb4c593ddb5ab479
Axel RICHARD <axel.richard@obeo.fr>
2024-01-15 13:53:35 +0100

    [2926] Add the ability to run code before and after the loading of the editing context
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/2926
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ packages/core/backend/sirius-components-core/src/main/java/org/eclipse/sirius/components/core/api/IEditingContextProcessor.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.components.core.api;
+
+/**
+ * Processor for {@link IEditingContext}.
+ *
+ * @author arichard
+ */
+public interface IEditingContextProcessor {
+
+    void preProcess(IEditingContext editingContext);
+
+    void postProcess(IEditingContext editingContext);
+
+    /**
+     * Implementation which does nothing, used for mocks in unit tests.
+     *
+     * @author arichard
+     */
+    class NoOp implements IEditingContextProcessor {
+
+        @Override
+        public void preProcess(IEditingContext editingContext) {
+        }
+
+        @Override
+        public void postProcess(IEditingContext editingContext) {
+        }
+    }
+}
