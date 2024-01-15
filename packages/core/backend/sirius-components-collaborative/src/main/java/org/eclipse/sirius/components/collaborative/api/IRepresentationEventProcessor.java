c65ba2eaf280b64abbce19a493d8fc4591051773
Guillaume Coutable <guillaume.coutable@obeo.fr>
2024-01-15 12:18:34 +0100

    [fix] Mock IRepresentation
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- packages/core/backend/sirius-components-collaborative/src/main/java/org/eclipse/sirius/components/collaborative/api/IRepresentationEventProcessor.java
+++ packages/core/backend/sirius-components-collaborative/src/main/java/org/eclipse/sirius/components/collaborative/api/IRepresentationEventProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 Obeo.
+ * Copyright (c) 2019, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,7 @@ public interface IRepresentationEventProcessor extends IDisposablePublisher {
 
         @Override
         public IRepresentation getRepresentation() {
-            return null;
+            return new IRepresentation.NoOp();
         }
 
         @Override
@@ -73,7 +73,7 @@ public interface IRepresentationEventProcessor extends IDisposablePublisher {
 
         @Override
         public ISubscriptionManager getSubscriptionManager() {
-            return null;
+            return new ISubscriptionManager.NoOp();
         }
 
         @Override
