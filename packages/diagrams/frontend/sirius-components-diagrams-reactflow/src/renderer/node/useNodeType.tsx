b56c4def9403689e2ad0cbb85b015e3e7fa0456d
Michael Charfadi <michael.charfadi@obeosoft.com>
2024-01-23 11:29:12 +0100

    [2988] Add useCallback to ReactFlow onNodesChange event
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/2988
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams-reactflow/src/renderer/node/useNodeType.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams-reactflow/src/renderer/node/useNodeType.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Obeo.
+ * Copyright (c) 2023, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -32,5 +32,5 @@ export const useNodeType = (): UseNodeTypeValue => {
     return { ...initialNodeType, ...contributedNodeTypes };
   }, [contributedNodeTypes]);
 
-  return { getNodeTypes: () => nodeTypes };
+  return { nodeTypes };
 };
