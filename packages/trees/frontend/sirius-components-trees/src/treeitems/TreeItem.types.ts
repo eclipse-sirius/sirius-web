0c5946c60851eb8d450970fb49df9b57d5d8186d
Axel RICHARD <axel.richard@obeo.fr>
2024-02-28 10:32:15 +0100

    [1707] Fix click while editing tree item label ends the edition
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/1707
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/treeitems/TreeItem.types.ts
+++ packages/trees/frontend/sirius-components-trees/src/treeitems/TreeItem.types.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2023 Obeo.
+ * Copyright (c) 2021, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -24,4 +24,14 @@ export interface TreeItemProps {
   textToFilter: string | null;
   enableMultiSelection: boolean;
   markedItemIds: string[];
+}
+
+export interface TreeItemState {
+  showContextMenu: boolean;
+  menuAnchor: Element | null;
+  editingMode: boolean;
+  label: string;
+  prevSelectionId: string | null;
+  editingKey: string | null;
+  isHovered: boolean;
 }
