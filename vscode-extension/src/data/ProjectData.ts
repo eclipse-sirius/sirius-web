1bbcd3e5129ffc9af0d5481b2d153c834ed74c03
Pierre-Charles David <pierre-charles.david@obeo.fr>
2024-03-12 09:56:20 +0100

    [fix] Adapt VS Code extension to recent GraphQL Schema changes
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- vscode-extension/src/data/ProjectData.ts
+++ vscode-extension/src/data/ProjectData.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2023 Obeo.
+ * Copyright (c) 2022, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -62,7 +62,15 @@ export class ProjectData {
       }
       const payload = {
         query: graphQLSubscription,
-        variables: { input: { id: uuid(), treeId: 'explorer://', editingContextId: this.id, expanded: expandedItems } },
+        variables: {
+          input: {
+            id: uuid(),
+            treeId: 'explorer://',
+            editingContextId: this.id,
+            expanded: expandedItems,
+            activeFilterIds: [],
+          },
+        },
       };
       const startMessage = { id: this.genSubscriptionTreeEventId(), type: 'start', payload };
       const startMessageJSON = JSON.stringify(startMessage);
@@ -126,7 +134,6 @@ export class ProjectData {
     const graphQLQuery = `
       query getRepresentations($editingContextId: ID!) {
         viewer {
-          id
           editingContext(editingContextId: $editingContextId) {
             id
             representations {
