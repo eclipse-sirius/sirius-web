5efe4c59f68d122f43bbc135379ff5a3b10cb2fd
Pierre-Charles David <pierre-charles.david@obeo.fr>
2024-03-21 18:38:52 +0100

    [doc] Update Sirius website URL
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/footer/Footer.tsx
+++ packages/sirius-web/frontend/sirius-web-application/src/footer/Footer.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2023 Obeo.
+ * Copyright (c) 2021, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ export const Footer = () => {
   return (
     <footer className={classes.footer}>
       <Typography variant="caption">&copy; {new Date().getFullYear()} Obeo. Powered by </Typography>
-      <Link variant="caption" href="https://www.eclipse.org/sirius" rel="noopener noreferrer" target="_blank">
+      <Link variant="caption" href="https://www.eclipse.dev/sirius" rel="noopener noreferrer" target="_blank">
         Sirius Web
       </Link>
     </footer>
