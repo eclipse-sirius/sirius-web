d507317fd15ae8335045d71abaf10fe03141c6d0
Pierre-Charles David <pierre-charles.david@obeo.fr>
2024-04-17 16:46:23 +0200

    [test] Add test for label edit on diagrams
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/application/controllers/diagrams/EditLabelDiagramContollerTests.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.application.controllers.diagrams;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import com.jayway.jsonpath.JsonPath;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramRefreshedEventPayload;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.EditLabelInput;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.EditLabelSuccessPayload;
+import org.eclipse.sirius.components.collaborative.dto.CreateRepresentationInput;
+import org.eclipse.sirius.components.diagrams.tests.graphql.InvokeEditLabelOnDiagramElementToolMutationRunner;
+import org.eclipse.sirius.components.diagrams.tests.navigation.DiagramNavigator;
+import org.eclipse.sirius.web.AbstractIntegrationTests;
+import org.eclipse.sirius.web.data.PapayaIdentifiers;
+import org.eclipse.sirius.web.services.api.IGivenCreatedDiagramSubscription;
+import org.eclipse.sirius.web.services.api.IGivenInitialServerState;
+import org.eclipse.sirius.web.services.diagrams.EditableLabelDiagramDescriptionProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+/**
+ * Integration tests of the expand / collapse diagrams.
+ *
+ * @author pcdavid
+ */
+@Transactional
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = { "sirius.web.test.enabled=studio" })
+public class EditLabelDiagramContollerTests extends AbstractIntegrationTests {
+    @Autowired
+    private IGivenInitialServerState givenInitialServerState;
+
+    @Autowired
+    private IGivenCreatedDiagramSubscription givenCreatedDiagramSubscription;
+
+    @Autowired
+    private InvokeEditLabelOnDiagramElementToolMutationRunner invokeDiagramElementToolMutationRunner;
+
+    @Autowired
+    private EditableLabelDiagramDescriptionProvider editableLabelDiagramDescriptionProvider;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.givenInitialServerState.initialize();
+    }
+
+    private Flux<DiagramRefreshedEventPayload> givenSubscriptionToLabelEditableDiagramDiagram() {
+        var input = new CreateRepresentationInput(
+                UUID.randomUUID(),
+                PapayaIdentifiers.PAPAYA_PROJECT.toString(),
+                this.editableLabelDiagramDescriptionProvider.getRepresentationDescriptionId(),
+                PapayaIdentifiers.ROOT_OBJECT.toString(),
+                "EditableLabelDiagram"
+        );
+        return this.givenCreatedDiagramSubscription.createAndSubscribe(input);
+    }
+
+    @Test
+    @Sql(scripts = {"/scripts/papaya.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {"/scripts/cleanup.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    public void givenDiagramWithLabelEditableNodeWhenLabelEditedThenLabelUpdated() {
+        var flux = this.givenSubscriptionToLabelEditableDiagramDiagram();
+
+        var diagramId = new AtomicReference<String>();
+        var nodeId = new AtomicReference<String>();
+        var labelId = new AtomicReference<String>();
+
+        Consumer<DiagramRefreshedEventPayload> initialDiagramContentConsumer = payload -> Optional.of(payload)
+                .map(DiagramRefreshedEventPayload::diagram)
+                .ifPresentOrElse(diagram -> {
+                    diagramId.set(diagram.getId());
+                    var node = new DiagramNavigator(diagram).nodeWithLabel("sirius-web-domain").getNode();
+                    nodeId.set(node.getId());
+                    labelId.set(node.getInsideLabel().getId());
+                }, () -> fail("Missing diagram"));
+
+        Runnable editLabel = () -> {
+            var input = new EditLabelInput(UUID.randomUUID(), PapayaIdentifiers.PAPAYA_PROJECT.toString(), diagramId.get(), labelId.get(), "new label");
+            var invokeSingleClickOnDiagramElementToolResult = this.invokeDiagramElementToolMutationRunner.run(input);
+
+            String invokeSingleClickOnDiagramElementToolResultTypename = JsonPath.read(invokeSingleClickOnDiagramElementToolResult, "$.data.editLabel.__typename");
+            assertThat(invokeSingleClickOnDiagramElementToolResultTypename).isEqualTo(EditLabelSuccessPayload.class.getSimpleName());
+        };
+
+        Predicate<DiagramRefreshedEventPayload> updatedDiagramContentMatcher = payload -> Optional.of(payload)
+                .map(DiagramRefreshedEventPayload::diagram)
+                .filter(diagram -> {
+                    return diagram.getNodes().stream()
+                        .filter(node -> node.getId().equals(nodeId.get()))
+                        .map(node -> node.getInsideLabel().getText().equals("new label"))
+                        .findFirst()
+                        .orElse(false);
+                })
+                .isPresent();
+
+        StepVerifier.create(flux)
+                .consumeNextWith(initialDiagramContentConsumer)
+                .then(editLabel)
+                .expectNextMatches(updatedDiagramContentMatcher)
+                .thenCancel()
+                .verify(Duration.ofSeconds(10));
+    }
+
+
+}
