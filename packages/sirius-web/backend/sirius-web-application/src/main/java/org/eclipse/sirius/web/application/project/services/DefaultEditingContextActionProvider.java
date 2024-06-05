389819acdefba455a9ae6f2753bd55a02890e12b
Guillaume Coutable <guillaume.coutable@obeo.fr>
2024-06-05 15:29:09 +0200

    [3562] Update displayed default model creation action
    - The action to create an empty model is displayed only when the project does not have a nature.
    - Add the possibility for a project with the studio nature to create an empty model.
    
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3562
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/project/services/DefaultEditingContextActionProvider.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/project/services/DefaultEditingContextActionProvider.java
@@ -13,10 +13,15 @@
 package org.eclipse.sirius.web.application.project.services;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import org.eclipse.sirius.components.collaborative.api.IEditingContextActionProvider;
 import org.eclipse.sirius.components.collaborative.dto.EditingContextAction;
 import org.eclipse.sirius.components.core.api.IEditingContext;
+import org.eclipse.sirius.web.application.UUIDParser;
+import org.eclipse.sirius.web.domain.boundedcontexts.project.Project;
+import org.eclipse.sirius.web.domain.boundedcontexts.project.services.api.IProjectSearchService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -29,8 +34,22 @@ public class DefaultEditingContextActionProvider implements IEditingContextActio
 
     public static final String EMPTY_ACTION_ID = "empty";
 
+    private final IProjectSearchService projectSearchService;
+
+    public DefaultEditingContextActionProvider(IProjectSearchService projectSearchService) {
+        this.projectSearchService = Objects.requireNonNull(projectSearchService);
+    }
+
     @Override
     public List<EditingContextAction> getEditingContextAction(IEditingContext editingContext) {
-        return List.of(new EditingContextAction(EMPTY_ACTION_ID, "Others..."));
+        var isWithoutNature = new UUIDParser().parse(editingContext.getId())
+                .flatMap(this.projectSearchService::findById)
+                .map(Project::getNatures)
+                .orElseGet(Set::of)
+                .isEmpty();
+        if (isWithoutNature) {
+            return List.of(new EditingContextAction(EMPTY_ACTION_ID, "Others..."));
+        }
+        return List.of();
     }
 }
