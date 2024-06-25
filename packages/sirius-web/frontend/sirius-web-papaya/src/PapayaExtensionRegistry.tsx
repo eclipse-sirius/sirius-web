bdcd2b6e16ef97b8681bcc49a3e27ea3168d2869
Michael Charfadi <michael.charfadi@obeosoft.com>
2024-06-25 16:24:35 +0200

    [3662] Add an extension point to contribute props to ReactFlow renderer
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3662
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/sirius-web/frontend/sirius-web-papaya/src/PapayaExtensionRegistry.tsx
+++ packages/sirius-web/frontend/sirius-web-papaya/src/PapayaExtensionRegistry.tsx
@@ -11,13 +11,22 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 
-import { ComponentExtension, ExtensionRegistry } from '@eclipse-sirius/sirius-components-core';
+import { ComponentExtension, DataExtension, ExtensionRegistry } from '@eclipse-sirius/sirius-components-core';
+import {
+  ReactFlowPropsCustomizer,
+  diagramRendererReactFlowPropsCustomizerExtensionPoint,
+} from '@eclipse-sirius/sirius-components-diagrams';
 import {
   EditProjectNavbarSubtitleProps,
   editProjectNavbarSubtitleExtensionPoint,
   useCurrentProject,
 } from '@eclipse-sirius/sirius-web-application';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { memo } from 'react';
+import { Panel, ReactFlowProps } from 'reactflow';
 
 const papayaExtensionRegistry = new ExtensionRegistry();
 
@@ -39,5 +48,74 @@ const editProjectNavbarSubtitleExtension: ComponentExtension<EditProjectNavbarSu
   Component: PapayaProjectNavbarSubtitle,
 };
 papayaExtensionRegistry.addComponent(editProjectNavbarSubtitleExtensionPoint, editProjectNavbarSubtitleExtension);
+
+const usePapayaDiagramPanelStyles = makeStyles((theme) => ({
+  papayaDiagramPanel: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(1),
+    maxWidth: '250px',
+  },
+  links: {
+    display: 'flex',
+    flexDirection: 'column',
+    listStyle: 'disc',
+    listStylePosition: 'inside',
+    paddingTop: theme.spacing(1),
+  },
+}));
+const PapayaDiagramPanel = memo(() => {
+  const classes = usePapayaDiagramPanelStyles();
+
+  const { project } = useCurrentProject();
+
+  if (project.natures.filter((nature) => nature.name === 'siriusComponents://nature?kind=papaya').length === 0) {
+    return null;
+  }
+  return (
+    <Panel position="bottom-right">
+      <Paper className={classes.papayaDiagramPanel}>
+        <Typography variant="subtitle2">Learn more</Typography>
+        <Typography variant="body2">
+          Follow these links to view the code of this Sirius Web internal test project on Github.
+        </Typography>
+        <ul className={classes.links}>
+          <li>
+            <Link
+              href="https://github.com/eclipse-sirius/sirius-web/tree/master/packages/sirius-web/backend/sirius-web-papaya"
+              target="_blank"
+              rel="noopener noreferrer">
+              Backend code
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://github.com/eclipse-sirius/sirius-web/tree/master/packages/sirius-web/frontend/sirius-web-papaya"
+              target="_blank"
+              rel="noopener noreferrer">
+              Frontend code
+            </Link>
+          </li>
+        </ul>
+      </Paper>
+    </Panel>
+  );
+});
+
+const reactFlowPropsCustomizer: ReactFlowPropsCustomizer = ({ children, ...props }: ReactFlowProps) => {
+  const newChildren = (
+    <>
+      {children}
+      <PapayaDiagramPanel />
+    </>
+  );
+  return { children: newChildren, ...props };
+};
+
+const papayaDiagramPanelExtension: DataExtension<Array<ReactFlowPropsCustomizer>> = {
+  identifier: `papaya_${diagramRendererReactFlowPropsCustomizerExtensionPoint.identifier}`,
+  data: [reactFlowPropsCustomizer],
+};
+papayaExtensionRegistry.putData(diagramRendererReactFlowPropsCustomizerExtensionPoint, papayaDiagramPanelExtension);
 
 export { papayaExtensionRegistry };
