wpiers/sirius-components

[3684] Replace form & tree converters configurations with extensions
Bug: eclipse-sirius#3684
Signed-off-by: William Piers <william.piers@obeo.fr>

packages/forms/frontend/sirius-components-forms/src/index.ts
@@ -33,8 +33,7 @@ export * from './propertysections/getTextDecorationLineValue';
 export * from './propertysections/useClickHandler';
 export * from './representations/FormRepresentation';
 export * from './views/DetailsView';
-export * from './views/DetailsViewConfiguration';
-export * from './views/DetailsViewConfiguration.types';
+export * from './views/DetailsViewExtensionPoints';
 export * from './views/FormBasedView';
 export * from './views/FormBasedView.types';
 export * from './views/FormConverter.types';

eclipse-sirius/sirius-web

Bug: https://github.com/eclipse-sirius/sirius-web/issues/3684

packages/forms/frontend/sirius-components-forms/src/index.ts
@@ -33,10 +33,8 @@ export * from './propertysections/getTextDecorationLineValue';
 export * from './propertysections/useClickHandler';
 export * from './representations/FormRepresentation';
 export * from './views/DetailsView';
-export * from './views/DetailsViewConfiguration';
-export * from './views/DetailsViewConfiguration.types';
 export * from './views/FormBasedView';
 export * from './views/FormBasedView.types';
-export * from './views/FormConverter.types';
+export * from './views/FormBasedViewExtensionPoints';
 export * from './views/RelatedElementsView';
 export * from './views/RepresentationsView';