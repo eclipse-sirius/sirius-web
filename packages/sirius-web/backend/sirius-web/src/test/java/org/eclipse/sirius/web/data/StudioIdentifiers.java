df5101627bb170d926b6591c3fb77159fc30b2db
Laurent Fasani <laurent.fasani@obeo.fr>
2024-07-19 15:37:29 +0200

    [3740] Add support for object duplication from Explorer
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3740
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/data/StudioIdentifiers.java
+++ packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/data/StudioIdentifiers.java
@@ -35,6 +35,8 @@ public final class StudioIdentifiers {
 
     public static final UUID NAMED_ELEMENT_ENTITY_OBJECT = UUID.fromString("c6fdba07-dea5-4a53-99c7-7eefc1bfdfcc");
 
+    public static final UUID LABEL_ATTRIBUTE_OBJECT = UUID.fromString("7ac92c9d-3cb6-4374-9774-11bb62962fe2");
+
     public static final UUID DESCRIPTION_ATTRIBUTE_OBJECT = UUID.fromString("d51d676c-0cb7-414b-8358-bacbc5d33942");
 
     public static final UUID NAME_ATTRIBUTE_OBJECT = UUID.fromString("520bb7c9-5f28-40f7-bda0-b35dd593876d");
@@ -67,6 +69,8 @@ public final class StudioIdentifiers {
 
     public static final String TABLE_DESCRIPTION_ID = "siriusComponents://representationDescription?kind=tableDescription&sourceKind=view&sourceId=" +
             StudioIdentifiers.VIEW_DOCUMENT + "&sourceElementId=" + StudioIdentifiers.TABLE_ID;
+
+    public static final String RECTANGULAR_NODE_STYLE_OBJECT = "d8f8f5f4-5044-45ec-860a-aa1122e192e7";
 
     private StudioIdentifiers() {
         // Prevent instantiation
