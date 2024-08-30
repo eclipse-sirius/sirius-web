623431205edd8934c7c164e98e0a968c1012946a
Michael Charfadi <michael.charfadi@obeosoft.com>
2024-08-30 15:11:10 +0200

    [3933] Fix explorer in vscode extension
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3933
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- vscode-extension/src/data/getTreeEventSubscription.ts
+++ vscode-extension/src/data/getTreeEventSubscription.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2023 Obeo.
+ * Copyright (c) 2022, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -37,9 +37,25 @@ fragment treeItemFields on TreeItem {
   id
   hasChildren
   expanded
-  label
+  label {
+    styledStringFragments {
+      text
+      styledStringFragmentStyle {
+        isStruckOut
+        underlineStyle
+        borderStyle
+        font
+        backgroundColor
+        foregroundColor
+        strikeoutColor
+        underlineColor
+        borderColor
+      }
+    }
+  }
   editable
   deletable
+  selectable
   kind
   iconURL
 }
