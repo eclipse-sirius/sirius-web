a6bbd304b74ca3a592cb1cb9a268bad4ecac78f4
Pierre-Charles David <pierre-charles.david@obeo.fr>
2024-09-04 16:22:24 +0200

    [3963] Add support for optional GraphQLCodeRegistry post-processing
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3963
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/application/controllers/projects/ProjectControllerFailureIntegrationTests.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.application.controllers.projects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.eclipse.sirius.components.core.api.IInput;
+import org.eclipse.sirius.components.graphql.tests.api.IGraphQLRequestor;
+import org.eclipse.sirius.web.AbstractIntegrationTests;
+import org.eclipse.sirius.web.application.project.dto.RenameProjectInput;
+import org.eclipse.sirius.web.application.services.ExceptionInjectionTransformer;
+import org.eclipse.sirius.web.data.TestIdentifiers;
+import org.eclipse.sirius.web.domain.boundedcontexts.project.services.api.IProjectSearchService;
+import org.eclipse.sirius.web.tests.graphql.RenameProjectMutationRunner;
+import org.eclipse.sirius.web.tests.services.api.IGivenInitialServerState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import graphql.ExceptionWhileDataFetching;
+import graphql.ExecutionInput;
+import graphql.GraphQL;
+import graphql.GraphQLError;
+import reactor.core.publisher.Flux;
+
+/**
+ * Test the behavior when a data fetcher throws an exception.
+ *
+ * @author pcdavid
+ */
+@Transactional
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+@Import(ExceptionInjectionTransformer.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProjectControllerFailureIntegrationTests extends AbstractIntegrationTests {
+
+    @Autowired
+    private IGivenInitialServerState givenInitialServerState;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private GraphQL graphQL;
+
+    @Autowired
+    private IProjectSearchService projectSearchService;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.givenInitialServerState.initialize();
+    }
+
+    @Test
+    @DisplayName("Given a data fetcher which throws an exception, when it is inboked, then a proper error is returned")
+    @Sql(scripts = {"/scripts/initialize.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {"/scripts/cleanup.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    public void givenDataFetcherThrowingExceptionWhenInvokedThenProperErrorReturned() {
+        assertThat(this.projectSearchService.existsById(TestIdentifiers.UML_SAMPLE_PROJECT)).isTrue();
+
+        List<GraphQLError> errors = new ArrayList<>();
+        var input = new RenameProjectInput(UUID.randomUUID(), TestIdentifiers.UML_SAMPLE_PROJECT, "New Name");
+        RenameProjectMutationRunner renameProjectMutationRunner = new RenameProjectMutationRunner(new IGraphQLRequestor() {
+
+            @Override
+            public Flux<String> subscribeToSpecification(String query, IInput input) {
+                return Flux.empty();
+            }
+
+            @Override
+            public Flux<Object> subscribe(String query, IInput input) {
+                return Flux.empty();
+            }
+
+            @Override
+            public String execute(String query, IInput input) {
+                Map<String, Object> variables = Map.of("input", ProjectControllerFailureIntegrationTests.this.objectMapper.convertValue(input, new TypeReference<Map<String, Object>>() { }));
+                return this.execute(query, variables);
+            }
+
+            @Override
+            public String execute(String query, Map<String, Object> variables) {
+                var executionInput = ExecutionInput.newExecutionInput()
+                        .query(query)
+                        .variables(variables)
+                        .build();
+
+                var executionResult = ProjectControllerFailureIntegrationTests.this.graphQL.execute(executionInput);
+                errors.addAll(executionResult.getErrors());
+                try {
+                    return ProjectControllerFailureIntegrationTests.this.objectMapper.writeValueAsString(executionResult.toSpecification());
+                } catch (JsonProcessingException exception) {
+                    fail(exception.getMessage());
+                }
+                return null;
+            }
+        });
+        var jsonResult = renameProjectMutationRunner.run(input);
+        var data = JsonPath.read(jsonResult, "$.data");
+        assertThat(data).isNull();
+        List<String> errorMessages = JsonPath.read(jsonResult, "$.errors[*].message");
+        assertThat(errorMessages).isEqualTo(List.of("Exception while fetching data (/renameProject) : injected fault"));
+        assertThat(errors).hasSize(1);
+        assertThat(errors.get(0)).isInstanceOf(ExceptionWhileDataFetching.class);
+        assertThat(((ExceptionWhileDataFetching) errors.get(0)).getMessage()).isEqualTo("Exception while fetching data (/renameProject) : injected fault");
+    }
+}
---
+++ packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/application/services/ExceptionInjectionTransformer.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.application.services;
+
+import java.util.Optional;
+
+import org.eclipse.sirius.web.infrastructure.graphql.IGraphQLCodeRegistryTransformer;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.FieldCoordinates;
+import graphql.schema.GraphQLCodeRegistry.Builder;
+
+/**
+ * Used to modify some data-fetcher to deliberately throw exceptions for testing purposes.
+ *
+ * @author pcdavid
+ */
+public class ExceptionInjectionTransformer implements IGraphQLCodeRegistryTransformer {
+    @Override
+    public void transform(Builder builder) {
+        var coordinates = FieldCoordinates.coordinates("Mutation", "renameProject");
+        builder.dataFetcher(coordinates, new DataFetcher<>() {
+            @Override
+            public Object get(DataFetchingEnvironment environment) throws Exception {
+                Optional.empty().orElseThrow(() -> new RuntimeException("injected fault"));
+                return null;
+            }
+        });
+    }
+}
