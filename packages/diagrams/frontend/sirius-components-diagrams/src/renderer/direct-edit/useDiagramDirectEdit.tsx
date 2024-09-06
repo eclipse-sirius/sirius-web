62e522ece1789dc6caabc393db52d2a3d8db1ae5
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2024-09-06 14:58:50 +0200

    [3974] Add support for `<` and `>` to trigger direct edit
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3974
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/direct-edit/useDiagramDirectEdit.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/direct-edit/useDiagramDirectEdit.tsx
@@ -19,7 +19,7 @@ import { DiagramDirectEditContext } from './DiagramDirectEditContext';
 import { DiagramDirectEditContextValue } from './DiagramDirectEditContext.types';
 import { UseDiagramDirectEditValue } from './useDiagramDirectEdit.types';
 
-const directEditActivationValidCharacters = /[\w&é§èàùçÔØÁÛÊË"«»’”„´$¥€£\\¿?!=+-,;:%/{}[\]–#@*.]/;
+const directEditActivationValidCharacters = /[\w&é§èàùçÔØÁÛÊË"«»<>’”„´$¥€£\\¿?!=+-,;:%/{}[\]–#@*.]/;
 
 export const useDiagramDirectEdit = (): UseDiagramDirectEditValue => {
   const { currentlyEditedLabelId, editingKey, setCurrentlyEditedLabelId, resetDirectEdit } =
