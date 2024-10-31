cf82640cd22c05bb87710f916844d77d64134ad9
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2024-10-31 16:03:08 +0100

    [4148] Fix dragged node absolute position evaluation
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4148
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/dropNode/useDropNode.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/dropNode/useDropNode.ts
@@ -12,7 +12,8 @@
  *******************************************************************************/
 import { gql, useMutation } from '@apollo/client';
 import { useMultiToast } from '@eclipse-sirius/sirius-components-core';
-import { Edge, Node, OnNodeDrag, XYPosition, useReactFlow, useStoreApi } from '@xyflow/react';
+import { Edge, InternalNode, Node, OnNodeDrag, XYPosition, useReactFlow, useStoreApi } from '@xyflow/react';
+import { NodeLookup, Rect } from '@xyflow/system';
 import { useCallback, useContext, useEffect } from 'react';
 import { DiagramContext } from '../../contexts/DiagramContext';
 import { DiagramContextValue } from '../../contexts/DiagramContext.types';
@@ -70,6 +71,20 @@ const getNodeDepth = (node: Node<NodeData>, intersections: Node<NodeData>[]): nu
     nodeHierarchy = intersections.find((node) => node.id === nodeHierarchy?.parentId);
   }
   return nodeDepth;
+};
+
+const evaluateAbsolutePosition = (node: Node, nodeLookup: NodeLookup<InternalNode<Node<NodeData>>>): XYPosition => {
+  let nextParentId: string | undefined = node.parentId;
+  const positionAbsolute: XYPosition = { ...node.position };
+  while (nextParentId) {
+    const parent = nodeLookup.get(nextParentId);
+    nextParentId = parent?.parentId;
+    if (parent) {
+      positionAbsolute.x += parent.position.x;
+      positionAbsolute.y += parent.position.y;
+    }
+  }
+  return positionAbsolute;
 };
 
 const useDropNodeMutation = () => {
@@ -201,7 +216,13 @@ export const useDropNode = (): UseDropNodeValue => {
       const draggedNode = getNodes().find((node) => node.id === draggedNodeId) || null;
 
       if (draggedNode && !draggedNode.data.isBorderNode) {
-        const intersections = getIntersectingNodes(draggedNode).filter((intersectingNode) => !intersectingNode.hidden);
+        const rectNode: Rect = {
+          ...evaluateAbsolutePosition(draggedNode, storeApi.getState().nodeLookup),
+          width: draggedNode.width ?? 0,
+          height: draggedNode.height ?? 0,
+        };
+        const intersections = getIntersectingNodes(rectNode).filter((intersectingNode) => !intersectingNode.hidden);
+
         const newParentId =
           [...intersections]
             .filter(
