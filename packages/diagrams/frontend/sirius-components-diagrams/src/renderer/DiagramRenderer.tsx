7f19700604dd89e29b1b6640e8c4b9eb50764515
Pierre-Charles David <pierre-charles.david@obeo.fr>
2024-11-13 09:58:42 +0100

    [3980] Re-enable selection after the execution of a tool on diagrams
    This reverts commits 5fc00f3c2c322d8d6c5160271ef82dd24087f1b8 and
    14f4133753af01571ef091d39641bc5029c11827 which themselves reverted the
    initial frontend side of the feature, and provides an updated version
    of the DiagramRenderer.tsx hook which should fix the issue.
    
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3980
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/DiagramRenderer.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/DiagramRenderer.tsx
@@ -27,6 +27,7 @@ import {
   ReactFlow,
   ReactFlowProps,
   applyNodeChanges,
+  useStoreApi,
 } from '@xyflow/react';
 import React, { MouseEvent as ReactMouseEvent, memo, useCallback, useContext, useEffect, useMemo, useRef } from 'react';
 import { DiagramContext } from '../contexts/DiagramContext';
@@ -121,24 +122,76 @@ export const DiagramRenderer = memo(({ diagramRefreshedEventPayload }: DiagramRe
 
   const { nodeConverters } = useContext<NodeTypeContextValue>(NodeTypeContext);
 
-  const { setSelection } = useSelection();
+  const { selection, setSelection } = useSelection();
   const { edgeType, setEdgeType } = useEdgeType();
 
   useInitialFitToScreen();
 
+  const store = useStoreApi<Node<NodeData>, Edge<EdgeData>>();
   useEffect(() => {
     const { diagram, cause } = diagramRefreshedEventPayload;
     const convertedDiagram: Diagram = convertDiagram(diagram, nodeConverters, diagramDescription, edgeType);
 
-    const selectedNodeIds = nodes.filter((node) => node.selected).map((node) => node.id);
-    const selectedEdgeIds = edges.filter((edge) => edge.selected).map((edge) => edge.id);
     if (cause === 'layout') {
-      convertedDiagram.nodes
-        .filter((node) => selectedNodeIds.includes(node.id))
-        .forEach((node) => (node.selected = true));
-      convertedDiagram.edges
-        .filter((edge) => selectedEdgeIds.includes(edge.id))
-        .forEach((edge) => (edge.selected = true));
+      const diagramElementIds: string[] = [
+        ...getNodes().map((node) => node.data.targetObjectId),
+        ...getEdges().map((edge) => edge.data?.targetObjectId ?? ''),
+      ];
+
+      const selectionDiagramEntryIds = selection.entries
+        .map((entry) => entry.id)
+        .filter((id) => diagramElementIds.includes(id))
+        .sort((id1: string, id2: string) => id1.localeCompare(id2));
+      const selectedDiagramElementIds = [
+        ...new Set(
+          [...getNodes(), ...getEdges()]
+            .filter((element) => element.selected)
+            .map((element) => element.data?.targetObjectId ?? '')
+        ),
+      ];
+      selectedDiagramElementIds.sort((id1: string, id2: string) => id1.localeCompare(id2));
+
+      const semanticElementsViews: Map<string, string[]> = new Map();
+      [...getNodes(), ...getEdges()].forEach((element) => {
+        const viewId = element.id;
+        const semanticElementId = element.data?.targetObjectId ?? '';
+        if (!semanticElementsViews.has(semanticElementId)) {
+          semanticElementsViews.set(semanticElementId, [viewId]);
+        } else {
+          semanticElementsViews.get(semanticElementId)?.push(viewId);
+        }
+      });
+
+      // For each selected semantic element which appears on the diagram,
+      // determine which of its views should be selected.
+      const viewsToSelect: Map<string, string[]> = new Map();
+      const previouslySelectedViews = [...getNodes(), ...getEdges()].filter((element) => element.selected);
+      for (var semanticElementId of selectionDiagramEntryIds) {
+        const allRelatedViews = semanticElementsViews.get(semanticElementId) || [];
+        const alreadySelectedViews = allRelatedViews.filter(
+          (viewId) => !!previouslySelectedViews.find((view: Node<NodeData> | Edge<EdgeData>) => view.id === viewId)
+        );
+        if (alreadySelectedViews.length > 0) {
+          // Keep the previous graphical selection if there was one that is still valid
+          viewsToSelect.set(semanticElementId, alreadySelectedViews);
+        } else if (allRelatedViews.length > 0 && allRelatedViews[0]) {
+          // Otherwise select a single view among the candidates.
+          // Given the order we receive the views from the backend, if there
+          // are multiple candidates in the same view hierarchy, the parent
+          // will appear first, and it's the "main" view we want to select.
+          viewsToSelect.set(semanticElementId, [allRelatedViews[0]]);
+        }
+      }
+
+      // Apply the new graphical selection
+      convertedDiagram.nodes = convertedDiagram.nodes.map((node) => ({
+        ...node,
+        selected: viewsToSelect.get(node.data?.targetObjectId)?.includes(node.id),
+      }));
+      convertedDiagram.edges = convertedDiagram.edges.map((edge) => ({
+        ...edge,
+        selected: !!(edge.data?.targetObjectId && viewsToSelect.get(edge.data?.targetObjectId)?.includes(edge.id)),
+      }));
 
       setEdges(convertedDiagram.edges);
       setNodes(convertedDiagram.nodes);
@@ -148,12 +201,27 @@ export const DiagramRenderer = memo(({ diagramRefreshedEventPayload }: DiagramRe
         edges,
       };
       layout(previousDiagram, convertedDiagram, diagramRefreshedEventPayload.referencePosition, (laidOutDiagram) => {
-        laidOutDiagram.nodes
-          .filter((node) => selectedNodeIds.includes(node.id))
-          .forEach((node) => (node.selected = true));
-        laidOutDiagram.edges
-          .filter((edge) => selectedEdgeIds.includes(edge.id))
-          .forEach((edge) => (edge.selected = true));
+        const { nodeLookup, edgeLookup } = store.getState();
+
+        laidOutDiagram.nodes = laidOutDiagram.nodes.map((node) => {
+          if (nodeLookup.get(node.id)) {
+            return {
+              ...node,
+              selected: !!nodeLookup.get(node.id)?.selected,
+            };
+          }
+          return node;
+        });
+
+        laidOutDiagram.edges = laidOutDiagram.edges.map((edge) => {
+          if (edgeLookup.get(edge.id)) {
+            return {
+              ...edge,
+              selected: !!edgeLookup.get(edge.id)?.selected,
+            };
+          }
+          return edge;
+        });
 
         setEdges(laidOutDiagram.edges);
         setNodes(laidOutDiagram.nodes);
