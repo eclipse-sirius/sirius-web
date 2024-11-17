48daebda55bf0a75660d546127f0617778a7ebd6
Denis Nikiforov <denis.nikif@gmail.com>
2024-11-18 00:19:36 +0300

    [4208] Improve performance of details view rendering on selection change
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4208
    Signed-off-by: Denis Nikiforov <denis.nikif@gmail.com>

--- packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/details/DetailsView.types.ts
+++ packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/details/DetailsView.types.ts
@@ -11,10 +11,8 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 
-import { Selection } from '@eclipse-sirius/sirius-components-core';
 import { GQLForm } from '@eclipse-sirius/sirius-components-forms';
 
 export interface DetailsViewState {
-  currentSelection: Selection;
   form: GQLForm | null;
 }
