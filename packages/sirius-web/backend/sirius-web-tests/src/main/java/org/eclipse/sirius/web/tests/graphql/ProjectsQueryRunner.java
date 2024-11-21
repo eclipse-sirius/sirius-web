f66dac9f741ab0cff8b5f983d3073919d131116f
Axel RICHARD <axel.richard@obeo.fr>
2024-11-21 17:58:13 +0100

    [4233] Add Cursor-based pagination in Project related GET REST APIs
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4233
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- packages/sirius-web/backend/sirius-web-tests/src/main/java/org/eclipse/sirius/web/tests/graphql/ProjectsQueryRunner.java
+++ packages/sirius-web/backend/sirius-web-tests/src/main/java/org/eclipse/sirius/web/tests/graphql/ProjectsQueryRunner.java
@@ -28,13 +28,14 @@ import org.springframework.stereotype.Service;
 public class ProjectsQueryRunner implements IQueryRunner {
 
     private static final String PROJECTS_QUERY = """
-            query getProjects($page: Int!, $limit: Int!) {
+            query getProjects($after: String, $before: String, $first: Int, $last: Int) {
               viewer {
-                projects(page: $page, limit: $limit) {
+                projects(after: $after, before: $before, first: $first, last: $last) {
                   edges {
                     node {
                       id
                     }
+                    cursor
                   }
                   pageInfo {
                     hasPreviousPage
