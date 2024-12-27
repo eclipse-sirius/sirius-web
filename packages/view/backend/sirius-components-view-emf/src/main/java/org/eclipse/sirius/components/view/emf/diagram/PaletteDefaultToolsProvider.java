2fb27242c74d2f53f34d99486f1bdc43f42e6a51
Michael Charfadi <michael.charfadi@obeosoft.com>
2024-12-27 10:01:42 +0100

    [4348] Move quick tools declaration on the backend
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4348
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/view/backend/sirius-components-view-emf/src/main/java/org/eclipse/sirius/components/view/emf/diagram/PaletteDefaultToolsProvider.java
+++ packages/view/backend/sirius-components-view-emf/src/main/java/org/eclipse/sirius/components/view/emf/diagram/PaletteDefaultToolsProvider.java
@@ -14,13 +14,19 @@ package org.eclipse.sirius.components.view.emf.diagram;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import org.eclipse.sirius.components.collaborative.diagrams.DiagramContext;
 import org.eclipse.sirius.components.collaborative.diagrams.api.DiagramImageConstants;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.ITool;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.SingleClickOnDiagramElementTool;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.ToolSection;
+import org.eclipse.sirius.components.diagrams.Edge;
+import org.eclipse.sirius.components.diagrams.IDiagramElement;
 import org.eclipse.sirius.components.diagrams.Node;
+import org.eclipse.sirius.components.diagrams.ViewModifier;
 import org.eclipse.sirius.components.diagrams.description.EdgeDescription;
 import org.eclipse.sirius.components.diagrams.description.EdgeLabelKind;
 import org.eclipse.sirius.components.diagrams.description.IDiagramElementDescription;
@@ -37,12 +43,12 @@ import org.springframework.stereotype.Service;
 public class PaletteDefaultToolsProvider implements IPaletteToolsProvider {
 
     @Override
-    public List<ToolSection> createExtraToolSections(Object diagramElementDescription, Object diagramElement) {
+    public List<ToolSection> createExtraToolSections(DiagramContext diagramContext, Object diagramElementDescription, Object diagramElement) {
         List<ToolSection> extraToolSections = new ArrayList<>();
 
         if (diagramElementDescription instanceof NodeDescription || diagramElementDescription instanceof EdgeDescription) {
 
-            List<ITool> extraTools = this.createExtraTools(diagramElementDescription, diagramElement);
+            List<ITool> extraTools = this.createExtraTools(diagramContext, diagramElementDescription, diagramElement);
             var editToolSection = ToolSection.newToolSection("edit-section")
                     .label("Edit")
                     .iconURL(List.of())
@@ -55,11 +61,11 @@ public class PaletteDefaultToolsProvider implements IPaletteToolsProvider {
     }
 
     @Override
-    public List<ITool> createQuickAccessTools(Object diagramElementDescription, Object diagramElement) {
-        return this.createExtraTools(diagramElementDescription, diagramElement);
+    public List<ITool> createQuickAccessTools(DiagramContext diagramContext, Object diagramElementDescription, Object diagramElement) {
+        return this.createExtraTools(diagramContext, diagramElementDescription, diagramElement);
     }
 
-    private List<ITool> createExtraTools(Object diagramElementDescription, Object diagramElement) {
+    private List<ITool> createExtraTools(DiagramContext diagramContext, Object diagramElementDescription, Object diagramElement) {
         List<IDiagramElementDescription> targetDescriptions = new ArrayList<>();
         if (diagramElementDescription instanceof NodeDescription nodeDescription) {
             targetDescriptions.add(nodeDescription);
@@ -83,35 +89,125 @@ public class PaletteDefaultToolsProvider implements IPaletteToolsProvider {
             var expandCollapseTool = this.createExtraExpandCollapseTool(targetDescriptions, diagramElement);
             expandCollapseTool.ifPresent(extraTools::add);
         }
+        var fadeTool = this.createFadeTool(targetDescriptions, diagramElement);
+        extraTools.add(fadeTool);
+
+        if (diagramElement instanceof Node node) {
+            var pinTool = this.createPinTool(targetDescriptions, node);
+            extraTools.add(pinTool);
+        }
+
+        if (diagramElement instanceof IDiagramElement element && isOutSideLabelManuallyPositioned(diagramContext, element)) {
+            extraTools.add(getResetOutSideLabelPositionedTool(targetDescriptions));
+        }
+
+        if (diagramElement instanceof Edge edge && isEdgeBendingPointsManuallyPositioned(diagramContext, edge)) {
+            extraTools.add(getResetBendingPointsPositionedTool(targetDescriptions));
+        }
+
+        if (diagramElement instanceof Edge edge && isEdgeHandlesManuallyPositioned(diagramContext, edge)) {
+            extraTools.add(getResetHandlesPositionTool(targetDescriptions));
+        }
+
+        if (diagramElement instanceof Node) {
+            extraTools.add(getAdjustSizeTool(targetDescriptions));
+        }
+
         return extraTools;
     }
 
-    private Optional<ITool> createExtraExpandCollapseTool(List<IDiagramElementDescription> targetDescriptions, Object diagramElement) {
+    private SingleClickOnDiagramElementTool getAdjustSizeTool(List<IDiagramElementDescription> targetDescriptions) {
+        return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("adjust-size")
+                .label("Adjust size")
+                .iconURL(List.of(DiagramImageConstants.ADJUST_SVG))
+                .targetDescriptions(targetDescriptions)
+                .appliesToDiagramRoot(false)
+                .build();
+    }
 
+    private boolean isOutSideLabelManuallyPositioned(DiagramContext diagramContext, IDiagramElement diagramElement) {
+        boolean isManualyPositionned = false;
         if (diagramElement instanceof Node node) {
-            ITool collapsingTool = null;
-            SingleClickOnDiagramElementTool collapseTool = SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("collapse")
-                    .label("Collapse")
-                    .iconURL(List.of(DiagramImageConstants.COLLAPSE_SVG))
-                    .targetDescriptions(targetDescriptions)
-                    .appliesToDiagramRoot(false)
-                    .withImpactAnalysis(false)
-                    .build();
+            var outSideLabels = node.getOutsideLabels();
+            var labelLayoutData = diagramContext.diagram().getLayoutData().labelLayoutData();
+            isManualyPositionned = outSideLabels.stream()
+                    .anyMatch(label -> labelLayoutData.containsKey(label.id()) &&
+                            labelLayoutData.get(label.id()).position().x() != 0 &&
+                            labelLayoutData.get(label.id()).position().y() != 0);
+        } else if (diagramElement instanceof Edge edge) {
+            var edgeLabels = Stream.of(edge.getBeginLabel(), edge.getCenterLabel(), edge.getEndLabel())
+                    .filter(Objects::nonNull)
+                    .toList();
+            var labelLayoutData = diagramContext.diagram().getLayoutData().labelLayoutData();
+            isManualyPositionned = edgeLabels.stream()
+                    .anyMatch(label -> labelLayoutData.containsKey(label.id()) &&
+                            labelLayoutData.get(label.id()).position().x() != 0 &&
+                            labelLayoutData.get(label.id()).position().y() != 0);
+        }
+        return isManualyPositionned;
+    }
 
-            SingleClickOnDiagramElementTool expandTool = SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("expand")
-                    .label("Expand")
-                    .iconURL(List.of(DiagramImageConstants.EXPAND_SVG))
-                    .targetDescriptions(targetDescriptions)
-                    .appliesToDiagramRoot(false)
-                    .withImpactAnalysis(false)
-                    .build();
+    private SingleClickOnDiagramElementTool getResetOutSideLabelPositionedTool(List<IDiagramElementDescription> targetDescriptions) {
+        return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("reset-outside-label-position")
+                .label("Reset-outside-label-position")
+                .iconURL(List.of(DiagramImageConstants.LABEL_OFF))
+                .targetDescriptions(targetDescriptions)
+                .appliesToDiagramRoot(false)
+                .build();
+    }
 
-            collapsingTool = switch (node.getCollapsingState()) {
-                case EXPANDED -> collapseTool;
-                case COLLAPSED -> expandTool;
-                default -> null;
+    private boolean isEdgeBendingPointsManuallyPositioned(DiagramContext diagramContext, Edge edge) {
+        var edgeLayoutData = diagramContext.diagram().getLayoutData().edgeLayoutData().get(edge.getId());
+        return edgeLayoutData != null && !edgeLayoutData.bendingPoints().isEmpty();
+    }
+
+    private SingleClickOnDiagramElementTool getResetBendingPointsPositionedTool(List<IDiagramElementDescription> targetDescriptions) {
+        return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("reset-bending-points")
+                .label("Reset bending points")
+                .iconURL(List.of(DiagramImageConstants.DIRECTIONS_OFF))
+                .targetDescriptions(targetDescriptions)
+                .appliesToDiagramRoot(false)
+                .build();
+    }
+
+    private boolean isEdgeHandlesManuallyPositioned(DiagramContext diagramContext, Edge edge) {
+        var nodeLayoutDatas = Stream.of(diagramContext.diagram().getLayoutData().nodeLayoutData().get(edge.getSourceId()),
+                diagramContext.diagram().getLayoutData().nodeLayoutData().get(edge.getTargetId()))
+                .filter(Objects::nonNull)
+                .toList();
+        return nodeLayoutDatas.stream()
+                .flatMap(nodeLayoutData -> nodeLayoutData.handleLayoutData().stream())
+                .anyMatch(handleLayoutData -> handleLayoutData.position() != null && (handleLayoutData.position().x() != 0 || handleLayoutData.position().y() != 0));
+    }
+
+    private SingleClickOnDiagramElementTool getResetHandlesPositionTool(List<IDiagramElementDescription> targetDescriptions) {
+        return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("reset-handles-position")
+                .label("Reset Handles Position")
+                .iconURL(List.of(DiagramImageConstants.SWIPE_RIGHT_ALT))
+                .targetDescriptions(targetDescriptions)
+                .appliesToDiagramRoot(false)
+                .build();
+    }
+
+    private Optional<ITool> createExtraExpandCollapseTool(List<IDiagramElementDescription> targetDescriptions, Object diagramElement) {
+        if (diagramElement instanceof Node node) {
+            var collapsingTool = switch (node.getCollapsingState()) {
+                case EXPANDED -> SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("collapse")
+                        .label("Collapse")
+                        .iconURL(List.of(DiagramImageConstants.COLLAPSE_SVG))
+                        .targetDescriptions(targetDescriptions)
+                        .appliesToDiagramRoot(false)
+                        .withImpactAnalysis(false)
+                        .build();
+                case COLLAPSED -> SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("expand")
+                        .label("Expand")
+                        .iconURL(List.of(DiagramImageConstants.EXPAND_SVG))
+                        .targetDescriptions(targetDescriptions)
+                        .appliesToDiagramRoot(false)
+                        .withImpactAnalysis(false)
+                        .build();
             };
-            return Optional.ofNullable(collapsingTool);
+            return Optional.of(collapsingTool);
         }
         return Optional.empty();
     }
@@ -131,6 +227,40 @@ public class PaletteDefaultToolsProvider implements IPaletteToolsProvider {
             return nodeDescription.isCollapsible();
         }
         return false;
+    }
+
+    private ITool createFadeTool(List<IDiagramElementDescription> targetDescriptions, Object diagramElement) {
+        String label = "Fade element";
+        String id = "fade";
+        if (diagramElement instanceof Node node && node.getModifiers().contains(ViewModifier.Faded)
+                || diagramElement instanceof Edge edge && edge.getModifiers().contains(ViewModifier.Faded)) {
+            label = "Unfade element";
+            id = "unfade";
+        }
+        return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool(id)
+                .label(label)
+                .iconURL(List.of(DiagramImageConstants.FADE_SVG))
+                .targetDescriptions(targetDescriptions)
+                .appliesToDiagramRoot(false)
+                .build();
+    }
+
+    private ITool createPinTool(List<IDiagramElementDescription> targetDescriptions, Node node) {
+        if (node.isPinned()) {
+            return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("unpin")
+                    .label("Unpin")
+                    .iconURL(List.of(DiagramImageConstants.UNPIN_SVG))
+                    .targetDescriptions(targetDescriptions)
+                    .appliesToDiagramRoot(false)
+                    .build();
+        } else {
+            return SingleClickOnDiagramElementTool.newSingleClickOnDiagramElementTool("pin")
+                    .label("Pin")
+                    .iconURL(List.of(DiagramImageConstants.PIN_SVG))
+                    .targetDescriptions(targetDescriptions)
+                    .appliesToDiagramRoot(false)
+                    .build();
+        }
     }
 
     private boolean hasLabelEditTool(Object diagramElementDescription) {
