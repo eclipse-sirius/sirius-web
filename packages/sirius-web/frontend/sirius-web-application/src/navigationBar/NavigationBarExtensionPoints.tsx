58668fef754a8fb5377b88fd0a7c9e8d209ea24f
Florent Latombe <florent.latombe@obeo.fr>
2025-01-13 16:24:16 +0100

    [4404] Make the navigation bar icon button more customizable
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4404
    Signed-off-by: Florent Latombe <florent.latombe@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/navigationBar/NavigationBarExtensionPoints.tsx
+++ packages/sirius-web/frontend/sirius-web-application/src/navigationBar/NavigationBarExtensionPoints.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2024 Obeo.
+ * Copyright (c) 2021, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -12,14 +12,12 @@
  *******************************************************************************/
 
 import { ComponentExtensionPoint } from '@eclipse-sirius/sirius-components-core';
-import { SiriusIcon } from '../core/SiriusIcon';
 import {
   NavigationBarIconProps,
   NavigationBarLeftContributionProps,
   NavigationBarRightContributionProps,
 } from './NavigationBar.types';
-
-const NavigationBarIcon = ({}: NavigationBarIconProps) => <SiriusIcon fontSize="large" />;
+import { NavigationBarIcon } from './NavigationBarIcon';
 
 export const navigationBarIconExtensionPoint: ComponentExtensionPoint<NavigationBarIconProps> = {
   identifier: 'navigationBar#icon',
