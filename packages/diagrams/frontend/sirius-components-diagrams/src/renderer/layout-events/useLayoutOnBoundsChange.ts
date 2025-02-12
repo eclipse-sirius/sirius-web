9f004322c3a05293b7d9a42324a82946f704b24c
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-02-12 16:12:55 +0100

    [4618] Ensure layout-related mutations use a unique event id
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4618
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/layout-events/useLayoutOnBoundsChange.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/layout-events/useLayoutOnBoundsChange.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Obeo.
+ * Copyright (c) 2023, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -21,7 +21,7 @@ import { useSynchronizeLayoutData } from '../layout/useSynchronizeLayoutData';
 import { DiagramNodeType } from '../node/NodeTypes.types';
 import { UseLayoutOnBoundsChangeValue } from './useLayoutOnBoundsChange.types';
 
-export const useLayoutOnBoundsChange = (refreshEventPayloadId: string): UseLayoutOnBoundsChangeValue => {
+export const useLayoutOnBoundsChange = (): UseLayoutOnBoundsChangeValue => {
   const { layout } = useLayout();
   const { synchronizeLayoutData } = useSynchronizeLayoutData();
   const { getEdges, getNodes } = useStore();
@@ -116,11 +116,11 @@ export const useLayoutOnBoundsChange = (refreshEventPayloadId: string): UseLayou
             edges: laidOutDiagram.edges,
           };
 
-          synchronizeLayoutData(refreshEventPayloadId, finalDiagram);
+          synchronizeLayoutData(crypto.randomUUID(), finalDiagram);
         });
       }
     },
-    [refreshEventPayloadId, synchronizeLayoutData, getNodes]
+    [synchronizeLayoutData, getNodes]
   );
 
   return { layoutOnBoundsChange };
