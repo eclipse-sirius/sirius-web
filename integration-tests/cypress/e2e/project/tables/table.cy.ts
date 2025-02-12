9611610aa1fe62704f95ebf81e7c27ed372178b7
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2025-02-12 15:39:42 +0100

    [4535] Conserve page size on table filter changes
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4535
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- integration-tests/cypress/e2e/project/tables/table.cy.ts
+++ integration-tests/cypress/e2e/project/tables/table.cy.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 CEA LIST.
+ * Copyright (c) 2024, 2025 CEA LIST.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@
 import { Project } from '../../../pages/Project';
 import { Papaya } from '../../../usecases/Papaya';
 import { Explorer } from '../../../workbench/Explorer';
+import { Table } from '../../../workbench/Table';
 
 describe('Tables', () => {
   context('Given a papaya project', () => {
@@ -35,7 +36,22 @@ describe('Tables', () => {
 
     it('Check table representation can be created', () => {
       new Explorer().createRepresentation('Package Package', 'Papaya package table', 'Table');
-      cy.getByTestId('table-representation').should('exist');
+      const table = new Table();
+      table.getTableRepresentation().should('exist');
+      table.checkRowCount(7);
+    });
+
+    it('Check global filter reset keep the page size', () => {
+      new Explorer().createRepresentation('Package Package', 'Papaya package table', 'Table');
+      const table = new Table();
+      table.getTableRepresentation().should('exist');
+      table.checkRowCount(7);
+      table.changeRowPerPage(5);
+      table.checkRowCount(5);
+      table.typeGlobalSearch('Class');
+      table.checkRowCount(4);
+      table.resetGlobalSearchValue();
+      table.checkRowCount(5);
     });
   });
 });
--- packages/tables/frontend/sirius-components-tables/src/table/CursorBasedPagination.tsx
+++ packages/tables/frontend/sirius-components-tables/src/table/CursorBasedPagination.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 CEA LIST.
+ * Copyright (c) 2024, 2025 CEA LIST.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,8 @@ export const CursorBasedPagination = ({
         <Select
           value={pageSize}
           onChange={(e) => onPageSizeChange(Number(e.target.value))}
-          inputProps={{ 'aria-label': 'Rows per page' }}>
+          inputProps={{ 'aria-label': 'Rows per page' }}
+          data-testid="cursor-based-pagination-size">
           {[5, 10, 20, 50].map((option) => (
             <MenuItem key={option} value={option}>
               {option}
