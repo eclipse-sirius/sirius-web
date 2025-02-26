fe6b41ef0a9fa315cb072c2a42fc63f657ecad6c
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2025-02-26 15:49:44 +0100

    [4628] Allow the loading of a studio based library
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4628
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/studio/services/StudioCapableEditingContextPredicate.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/studio/services/StudioCapableEditingContextPredicate.java
@@ -14,6 +14,8 @@ package org.eclipse.sirius.web.application.studio.services;
 
 import java.util.Objects;
 
+import org.eclipse.sirius.components.domain.DomainPackage;
+import org.eclipse.sirius.components.view.ViewPackage;
 import org.eclipse.sirius.web.application.UUIDParser;
 import org.eclipse.sirius.web.application.studio.services.api.IStudioCapableEditingContextPredicate;
 import org.eclipse.sirius.web.domain.boundedcontexts.project.Nature;
@@ -21,6 +23,7 @@ import org.eclipse.sirius.web.domain.boundedcontexts.project.Project;
 import org.eclipse.sirius.web.domain.boundedcontexts.project.services.api.IProjectSearchService;
 import org.eclipse.sirius.web.domain.boundedcontexts.projectsemanticdata.ProjectSemanticData;
 import org.eclipse.sirius.web.domain.boundedcontexts.projectsemanticdata.services.api.IProjectSemanticDataSearchService;
+import org.eclipse.sirius.web.domain.boundedcontexts.semanticdata.services.api.ISemanticDataSearchService;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
@@ -36,20 +39,31 @@ public class StudioCapableEditingContextPredicate implements IStudioCapableEditi
 
     private final IProjectSemanticDataSearchService projectSemanticDataSearchService;
 
-    public StudioCapableEditingContextPredicate(IProjectSearchService projectSearchService, IProjectSemanticDataSearchService projectSemanticDataSearchService) {
+    private final ISemanticDataSearchService semanticDataSearchService;
+
+    public StudioCapableEditingContextPredicate(IProjectSearchService projectSearchService, IProjectSemanticDataSearchService projectSemanticDataSearchService, ISemanticDataSearchService semanticDataSearchService) {
         this.projectSearchService = Objects.requireNonNull(projectSearchService);
         this.projectSemanticDataSearchService = Objects.requireNonNull(projectSemanticDataSearchService);
+        this.semanticDataSearchService = Objects.requireNonNull(semanticDataSearchService);
     }
 
     @Override
     public boolean test(String editingContextId) {
-        return new UUIDParser().parse(editingContextId)
+        var isStudioProject = new UUIDParser().parse(editingContextId)
                 .flatMap(semanticDataId -> this.projectSemanticDataSearchService.findBySemanticDataId(AggregateReference.to(semanticDataId)))
                 .map(ProjectSemanticData::getProject)
                 .map(AggregateReference::getId)
                 .flatMap(this.projectSearchService::findById)
                 .filter(this::isStudio)
                 .isPresent();
+
+        var isStudioSemanticData = new UUIDParser().parse(editingContextId)
+                .flatMap(this.semanticDataSearchService::findById)
+                .filter(semanticData -> semanticData.getDomains().stream()
+                        .anyMatch(semanticDataDomain -> semanticDataDomain.uri().equals(ViewPackage.eNS_URI) || semanticDataDomain.uri().equals(DomainPackage.eNS_URI)))
+                .isPresent();
+
+        return isStudioProject || isStudioSemanticData;
     }
 
     private boolean isStudio(Project project) {
