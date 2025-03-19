258aca8b10b4677d3c46fbec8ee517fb7ee43c12
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-03-19 12:48:59 +0100

    [4098] Cache the AQL services for a given EPackage
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4098
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ packages/emf/backend/sirius-components-interpreter/src/main/java/org/eclipse/sirius/components/interpreter/AQLServicesAdapter.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.components.interpreter;
+
+import java.util.Set;
+
+import org.eclipse.acceleo.query.runtime.IService;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
+
+/**
+ * Adapter attached to a (static) EPackage to cache the result of AQL services discovery for this EPackage.
+ *
+ * @author pcdavid
+ */
+public class AQLServicesAdapter implements Adapter {
+    private Set<IService<?>> services = Set.of();
+
+    private Notifier notifier;
+
+    @Override
+    public void notifyChanged(Notification notification) {
+        // do nothing
+    }
+
+    public Set<IService<?>> getServices() {
+        return this.services;
+    }
+
+    public void setServices(Set<IService<?>> services) {
+        this.services = services;
+    }
+
+    @Override
+    public Notifier getTarget() {
+        return this.notifier;
+    }
+
+    @Override
+    public void setTarget(Notifier newTarget) {
+        this.notifier = newTarget;
+    }
+
+    @Override
+    public boolean isAdapterForType(Object type) {
+        return false;
+    }
+}
