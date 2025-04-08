7ffe9d7eac7acac42fb72ea76d99039c035e8908
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-04-08 10:32:59 +0200

    [4099] Consider only non-derived refrences when testing for proxies
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4099
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/document/services/ProxyValidator.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/document/services/ProxyValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo.
+ * Copyright (c) 2024, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ public class ProxyValidator implements IProxyValidator {
 
     private boolean hasProxies(EObject eObject) {
         return eObject.eClass().getEAllReferences().stream()
-                .filter(eReference -> !eReference.isContainment() && eObject.eIsSet(eReference))
+                .filter(eReference -> !eReference.isContainment() && !eReference.isDerived() && eObject.eIsSet(eReference))
                 .anyMatch(eReference -> this.hasProxies(eObject, eReference));
     }
 
