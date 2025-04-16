a28356921489d471fadcbb5dd9ae4024bb18fc62
Michael Charfadi <michael.charfadi@obeosoft.com>
2025-04-16 16:42:41 +0200

    [4841] Allow moving the handle of an edge targeting another edge
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4841
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/ConnectionLine.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/ConnectionLine.tsx
@@ -21,7 +21,7 @@ import {
 } from '@xyflow/react';
 import React, { memo, useEffect } from 'react';
 import { EdgeData, NodeData } from '../DiagramRenderer.types';
-import { getNearestPointInPerimeter, getNodesUpdatedWithHandles } from './EdgeLayout';
+import { getNearestPointInPath, getNearestPointInPerimeter, getNodesUpdatedWithHandles } from './EdgeLayout';
 
 const connectionLineStyle = (theme: Theme): React.CSSProperties => {
   return {
@@ -43,13 +43,26 @@ export const ConnectionLine = memo(
     fromHandle,
   }: ConnectionLineComponentProps<Node<NodeData>>) => {
     const theme = useTheme();
-    const { getNodes, setNodes, getEdges } = useReactFlow<Node<NodeData>, Edge<EdgeData>>();
+    const { getNodes, setNodes, getEdges, getEdge } = useReactFlow<Node<NodeData>, Edge<EdgeData>>();
     const updateNodeInternals = useUpdateNodeInternals();
 
     const edgeId = fromNode.data.connectionHandles.find((handle) => handle.id === fromHandle?.id)?.edgeId;
     const targetHandle = getEdges().find((e) => e.id === edgeId)?.targetHandle;
     const sourceHandle = getEdges().find((e) => e.id === edgeId)?.sourceHandle;
     const handleToUpdate = targetHandle === fromHandle.id ? sourceHandle : targetHandle;
+
+    if (edgeId) {
+      const edge = getEdge(edgeId);
+      if (edge) {
+        const edgeBase = getEdge(edge.target);
+        // Snap the ConnectionLine to the border of the targetted edge
+        if (edgeBase && edgeBase.data && edgeBase.data.edgePath && edgeBase.data.isHovered) {
+          const { position } = getNearestPointInPath(toX, toY, edgeBase.data.edgePath);
+          toX = position.x;
+          toY = position.y;
+        }
+      }
+    }
 
     if (handleToUpdate) {
       if (edgeId && toNode && toNode.width && toNode.height) {
@@ -82,6 +95,32 @@ export const ConnectionLine = memo(
         );
         setNodes(nodes);
         updateNodeInternals(toNode.id);
+      }
+
+      if (edgeId) {
+        const edge = getEdge(edgeId);
+        if (edge) {
+          const edgeBase = getEdge(edge.target);
+          if (edgeBase && edgeBase.data && edgeBase.data.edgePath && edgeBase.data.isHovered) {
+            const { position, positionRatio } = getNearestPointInPath(toX, toY, edgeBase.data.edgePath);
+            setNodes((prevNodes) =>
+              prevNodes.map((prevNode) => {
+                if (prevNode.id === edgeBase.id) {
+                  return {
+                    ...prevNode,
+                    position: position,
+                    data: {
+                      ...prevNode.data,
+                      isLayouted: true,
+                      positionRatio: positionRatio,
+                    },
+                  };
+                }
+                return prevNode;
+              })
+            );
+          }
+        }
       }
     }, [toX, toY]);
 
