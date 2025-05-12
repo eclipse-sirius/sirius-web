675d50f8d86ad8bbd8659063e59f7c3e8b93b705
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-05-12 16:33:43 +0200

    [4102] Add IRepresentationRenderVariableCustomizer mechanism
    It is only used by Trees for now.
    
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4102
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/trees/backend/sirius-components-collaborative-trees/src/main/java/org/eclipse/sirius/components/collaborative/trees/TreeService.java
+++ packages/trees/backend/sirius-components-collaborative-trees/src/main/java/org/eclipse/sirius/components/collaborative/trees/TreeService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2024 Obeo.
+ * Copyright (c) 2019, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -12,10 +12,14 @@
  *******************************************************************************/
 package org.eclipse.sirius.components.collaborative.trees;
 
+import java.util.List;
+import java.util.Objects;
+
 import org.eclipse.sirius.components.collaborative.trees.api.ITreeService;
 import org.eclipse.sirius.components.collaborative.trees.api.TreeCreationParameters;
 import org.eclipse.sirius.components.core.api.IEditingContext;
 import org.eclipse.sirius.components.representations.GetOrCreateRandomIdProvider;
+import org.eclipse.sirius.components.representations.IRepresentationRenderVariableCustomizer;
 import org.eclipse.sirius.components.representations.VariableManager;
 import org.eclipse.sirius.components.trees.Tree;
 import org.eclipse.sirius.components.trees.renderer.TreeRenderer;
@@ -29,17 +33,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class TreeService implements ITreeService {
 
+    private final List<IRepresentationRenderVariableCustomizer> renderVariableCustomizers;
+
+    public TreeService(List<IRepresentationRenderVariableCustomizer> renderVariableCustomizers) {
+        this.renderVariableCustomizers = Objects.requireNonNull(renderVariableCustomizers);
+    }
+
     @Override
     public Tree create(TreeCreationParameters treeCreationParameters) {
+        VariableManager variableManager = this.createDefaultVariables(treeCreationParameters);
+        for (var renderVariableCustomizer : this.renderVariableCustomizers) {
+            variableManager = renderVariableCustomizer.customize(treeCreationParameters.getTreeDescription(), variableManager);
+        }
+
+        TreeRenderer treeRenderer = new TreeRenderer(variableManager, treeCreationParameters.getTreeDescription());
+        return treeRenderer.render();
+    }
+
+    private VariableManager createDefaultVariables(TreeCreationParameters treeCreationParameters) {
         VariableManager variableManager = new VariableManager();
         variableManager.put(GetOrCreateRandomIdProvider.PREVIOUS_REPRESENTATION_ID, treeCreationParameters.getId());
         variableManager.put(IEditingContext.EDITING_CONTEXT, treeCreationParameters.getEditingContext());
         variableManager.put(VariableManager.SELF, treeCreationParameters.getTargetObject());
         variableManager.put(TreeRenderer.EXPANDED, treeCreationParameters.getExpanded());
         variableManager.put(TreeRenderer.ACTIVE_FILTER_IDS, treeCreationParameters.getActiveFilterIds());
-
-        TreeRenderer treeRenderer = new TreeRenderer(variableManager, treeCreationParameters.getTreeDescription());
-        return treeRenderer.render();
+        return variableManager;
     }
 
 }
