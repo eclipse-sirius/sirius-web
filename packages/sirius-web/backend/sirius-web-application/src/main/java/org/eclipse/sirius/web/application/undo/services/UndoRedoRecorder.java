c6f01d2ecd1fba6335e5be5a7f74e537df93f762
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-05-26 15:55:20 +0200

    [4963] Disable undo/redo recording for some well-known safe inputs
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4963
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/undo/services/UndoRedoRecorder.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/undo/services/UndoRedoRecorder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo.
+ * Copyright (c) 2024, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -12,16 +12,31 @@
  *******************************************************************************/
 package org.eclipse.sirius.web.application.undo.services;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.eclipse.sirius.components.collaborative.api.ChangeDescription;
 import org.eclipse.sirius.components.collaborative.api.IInputPostProcessor;
 import org.eclipse.sirius.components.collaborative.api.IInputPreProcessor;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.GetActionsInput;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.GetPaletteInput;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.InitialDirectEditElementLabelInput;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.LayoutDiagramInput;
+import org.eclipse.sirius.components.collaborative.dto.EditingContextRepresentationDescriptionsInput;
+import org.eclipse.sirius.components.collaborative.dto.GetEditingContextActionsInput;
+import org.eclipse.sirius.components.collaborative.dto.GetRepresentationDescriptionInput;
+import org.eclipse.sirius.components.collaborative.selection.dto.GetSelectionDescriptionMessageInput;
+import org.eclipse.sirius.components.collaborative.trees.dto.TreeItemContextMenuInput;
+import org.eclipse.sirius.components.collaborative.trees.dto.TreePathInput;
 import org.eclipse.sirius.components.core.api.IEditingContext;
 import org.eclipse.sirius.components.core.api.IInput;
 import org.eclipse.sirius.web.application.editingcontext.EditingContext;
 import org.eclipse.sirius.web.application.undo.dto.RedoInput;
 import org.eclipse.sirius.web.application.undo.dto.UndoInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 import reactor.core.publisher.Sinks;
 
 /**
@@ -31,14 +46,37 @@ import reactor.core.publisher.Sinks;
  */
 @Service
 public class UndoRedoRecorder implements IInputPreProcessor, IInputPostProcessor {
+    /**
+     * Whitelist of input types that should not trigger undo/redo recording, either because it does not make sense
+     * (undo/redo operations themselves), because we do not support under/redo operations on them (diagram layout), or
+     * because they correspond to known read-only operations where recording can have performance drawbacks for no
+     * benefits (as they do not actually change the backend state).
+     */
+    private static final List<Class<?>> INGORED_INPUT_TYPES = List.of(
+            UndoInput.class,
+            RedoInput.class,
+            LayoutDiagramInput.class,
+            TreePathInput.class,
+            TreeItemContextMenuInput.class,
+            EditingContextRepresentationDescriptionsInput.class,
+            GetEditingContextActionsInput.class,
+            GetRepresentationDescriptionInput.class,
+            GetPaletteInput.class,
+            GetSelectionDescriptionMessageInput.class,
+            GetActionsInput.class,
+            InitialDirectEditElementLabelInput.class,
+            org.eclipse.sirius.components.collaborative.trees.dto.InitialDirectEditElementLabelInput.class
+    );
+
+    private final Logger logger = LoggerFactory.getLogger(UndoRedoRecorder.class);
 
     private boolean canHandle(IInput input)  {
-        return !(input instanceof UndoInput || input instanceof RedoInput || input instanceof LayoutDiagramInput);
+        return INGORED_INPUT_TYPES.stream().noneMatch(type -> type.isInstance(input));
     }
 
     @Override
     public IInput preProcess(IEditingContext editingContext, IInput input, Sinks.Many<ChangeDescription> changeDescriptionSink) {
-        if (editingContext instanceof EditingContext siriusEditingContext && canHandle(input)) {
+        if (editingContext instanceof EditingContext siriusEditingContext && this.canHandle(input)) {
             siriusEditingContext.getChangeRecorder().beginRecording(siriusEditingContext.getDomain().getResourceSet().getResources());
         }
         return input;
@@ -46,11 +84,13 @@ public class UndoRedoRecorder implements IInputPreProcessor, IInputPostProcessor
 
     @Override
     public void postProcess(IEditingContext editingContext, IInput input, Sinks.Many<ChangeDescription> changeDescriptionSink) {
-        if (editingContext instanceof EditingContext siriusEditingContext && canHandle(input)) {
+        if (editingContext instanceof EditingContext siriusEditingContext && this.canHandle(input)) {
+            long start = System.nanoTime();
             var changeDescription = siriusEditingContext.getChangeRecorder().summarize();
             siriusEditingContext.getInputId2change().put(input.id().toString(), changeDescription);
             siriusEditingContext.getChangeRecorder().endRecording();
+            long stop = System.nanoTime();
+            this.logger.debug("UndoRedoRecorder post-processing of input {} in {}ms", input.getClass().getSimpleName(), Duration.ofNanos(stop - start).toMillis());
         }
-
     }
 }
