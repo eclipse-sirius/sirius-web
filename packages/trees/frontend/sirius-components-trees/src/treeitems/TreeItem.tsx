3eb7cce108f5253a5ec3a6e95eb4efc86e202a35
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-06-04 10:37:15 +0200

    [3895] Memoize TreeItem
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3895
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/treeitems/TreeItem.tsx
+++ packages/trees/frontend/sirius-components-trees/src/treeitems/TreeItem.tsx
@@ -11,7 +11,7 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 import { DRAG_SOURCES_TYPE, GQLStyledString, StyledLabel } from '@eclipse-sirius/sirius-components-core';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import { makeStyles } from 'tss-react/mui';
 import { GQLTreeItem } from '../views/TreeView.types';
 import { PartHovered, TreeItemProps, TreeItemState } from './TreeItem.types';
@@ -122,242 +122,249 @@ const directEditActivationValidCharacters = /[\w&é§èàùçÔØÁÛÊË"«»�
 /**
  * Renders a *single* tree item (excluding its sub-items).
  */
-export const TreeItem = ({
-  editingContextId,
-  treeId,
-  item,
-  itemIndex,
-  depth,
-  expanded,
-  maxDepth,
-  readOnly,
-  textToHighlight,
-  textToFilter,
-  marked,
-  selected,
-  onExpandedElementChange,
-  onTreeItemClick,
-  onDragStart,
-  treeItemActionRender,
-}: TreeItemProps) => {
-  const [state, setState] = useState<TreeItemState>({
-    editingMode: false,
-    editingKey: null,
-    partHovered: null,
-  });
-
-  const refDom = useRef() as any;
-
-  const { classes } = useTreeItemStyle({ depth });
-  const { onDropTreeItem } = useDropTreeItem(editingContextId, treeId);
-
-  const handleMouseEnter = (partHovered: PartHovered) => {
-    setState((prevState) => ({ ...prevState, partHovered }));
-  };
-
-  const handleMouseLeave = () => {
-    setState((prevState) => ({ ...prevState, partHovered: null }));
-  };
-
-  const onTreeItemAction = () => {
-    setState((prevState) => ({ ...prevState, partHovered: null }));
-  };
-
-  const enterEditingMode = () => {
-    setState((prevState) => ({
-      ...prevState,
-      editingMode: true,
+export const TreeItem = memo(
+  ({
+    editingContextId,
+    treeId,
+    item,
+    itemIndex,
+    depth,
+    expanded,
+    maxDepth,
+    readOnly,
+    textToHighlight,
+    textToFilter,
+    marked,
+    selected,
+    onExpandedElementChange,
+    onTreeItemClick,
+    onDragStart,
+    treeItemActionRender,
+  }: TreeItemProps) => {
+    const [state, setState] = useState<TreeItemState>({
+      editingMode: false,
       editingKey: null,
-    }));
-  };
-
-  let className = classes.treeItem;
-  let dataTestid: string | undefined = undefined;
-
-  if (selected) {
-    className = `${className} ${classes.selected}`;
-    dataTestid = 'selected';
-  }
-  if (state.partHovered === 'item' && item.selectable) {
-    className = `${className} ${classes.treeItemHover}`;
-  }
-  useEffect(() => {
-    if (selected) {
-      if (refDom.current?.scrollIntoViewIfNeeded) {
-        refDom.current.scrollIntoViewIfNeeded(true);
-      } else {
-        // Fallback for browsers not supporting the non-standard `scrollIntoViewIfNeeded`
-        refDom.current?.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [selected]);
-
-  const onCloseEditingMode = () => {
-    setState((prevState) => {
-      return { ...prevState, editingMode: false };
+      partHovered: null,
     });
-    refDom.current.focus();
-  };
 
-  const onClick: React.MouseEventHandler<HTMLDivElement> = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!state.editingMode && event.currentTarget.contains(event.target as HTMLElement)) {
-      refDom.current.focus();
-      if (!item.selectable) {
-        return;
+    const refDom = useRef() as any;
+
+    const { classes } = useTreeItemStyle({ depth });
+    const { onDropTreeItem } = useDropTreeItem(editingContextId, treeId);
+
+    const handleMouseEnter = (partHovered: PartHovered) => {
+      setState((prevState) => ({ ...prevState, partHovered }));
+    };
+
+    const handleMouseLeave = () => {
+      setState((prevState) => ({ ...prevState, partHovered: null }));
+    };
+
+    const onTreeItemAction = () => {
+      setState((prevState) => ({ ...prevState, partHovered: null }));
+    };
+
+    const enterEditingMode = () => {
+      setState((prevState) => ({
+        ...prevState,
+        editingMode: true,
+        editingKey: null,
+      }));
+    };
+
+    let className = classes.treeItem;
+    let dataTestid: string | undefined = undefined;
+
+    if (selected) {
+      className = `${className} ${classes.selected}`;
+      dataTestid = 'selected';
+    }
+    if (state.partHovered === 'item' && item.selectable) {
+      className = `${className} ${classes.treeItemHover}`;
+    }
+    useEffect(() => {
+      if (selected) {
+        if (refDom.current?.scrollIntoViewIfNeeded) {
+          refDom.current.scrollIntoViewIfNeeded(true);
+        } else {
+          // Fallback for browsers not supporting the non-standard `scrollIntoViewIfNeeded`
+          refDom.current?.scrollIntoView({ behavior: 'smooth' });
+        }
       }
+    }, [selected]);
 
-      // Don't change the selection if the user clicked on the TreeItemArrow
-      const treeItemArrowTestId = `${getString(item.label)}-toggle`;
-      if ((event.target as HTMLElement).getAttribute('data-testid') === treeItemArrowTestId) {
-        return;
-      }
-
-      onTreeItemClick(event, item);
-    }
-  };
-
-  const onBeginEditing = (event) => {
-    if (!item.editable || state.editingMode || readOnly || !event.currentTarget.contains(event.target as HTMLElement)) {
-      return;
-    }
-    const { key } = event;
-    /*If a modifier key is hit alone, do nothing*/
-    if ((event.altKey || event.shiftKey) && event.getModifierState(key)) {
-      return;
-    }
-    const validFirstInputChar =
-      !event.metaKey && !event.ctrlKey && key.length === 1 && directEditActivationValidCharacters.test(key);
-    if (validFirstInputChar) {
+    const onCloseEditingMode = () => {
       setState((prevState) => {
-        return { ...prevState, editingMode: true, editingKey: key };
+        return { ...prevState, editingMode: false };
       });
-    }
-  };
+      refDom.current.focus();
+    };
 
-  const dragOver: React.DragEventHandler<HTMLDivElement> = (event) => {
-    event.preventDefault();
-  };
+    const onClick: React.MouseEventHandler<HTMLDivElement> = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      if (!state.editingMode && event.currentTarget.contains(event.target as HTMLElement)) {
+        refDom.current.focus();
+        if (!item.selectable) {
+          return;
+        }
 
-  const onDropItem: React.DragEventHandler<HTMLDivElement> = (event) => {
-    const dragSourcesStringified = event.dataTransfer.getData(DRAG_SOURCES_TYPE);
-    const selectedIds = JSON.parse(dragSourcesStringified);
-    onDropTreeItem(selectedIds, item.id, -1);
-    event.preventDefault();
-  };
+        // Don't change the selection if the user clicked on the TreeItemArrow
+        const treeItemArrowTestId = `${getString(item.label)}-toggle`;
+        if ((event.target as HTMLElement).getAttribute('data-testid') === treeItemArrowTestId) {
+          return;
+        }
 
-  const onDropBefore: React.DragEventHandler<HTMLDivElement> = (event) => {
-    const dragSourcesStringified = event.dataTransfer.getData(DRAG_SOURCES_TYPE);
-    const selectedIds = JSON.parse(dragSourcesStringified);
-    onDropTreeItem(selectedIds, item.id, itemIndex);
-    event.preventDefault();
-  };
+        onTreeItemClick(event, item);
+      }
+    };
 
-  const text: JSX.Element = state.editingMode ? (
-    <TreeItemDirectEditInput
-      editingContextId={editingContextId}
-      treeId={treeId}
-      treeItemId={item.id}
-      editingKey={state.editingKey}
-      onClose={onCloseEditingMode}
-    />
-  ) : (
-    <StyledLabel styledString={item.label} selected={false} textToHighlight={textToHighlight ?? ''} marked={marked} />
-  );
+    const onBeginEditing = (event) => {
+      if (
+        !item.editable ||
+        state.editingMode ||
+        readOnly ||
+        !event.currentTarget.contains(event.target as HTMLElement)
+      ) {
+        return;
+      }
+      const { key } = event;
+      /*If a modifier key is hit alone, do nothing*/
+      if ((event.altKey || event.shiftKey) && event.getModifierState(key)) {
+        return;
+      }
+      const validFirstInputChar =
+        !event.metaKey && !event.ctrlKey && key.length === 1 && directEditActivationValidCharacters.test(key);
+      if (validFirstInputChar) {
+        setState((prevState) => {
+          return { ...prevState, editingMode: true, editingKey: key };
+        });
+      }
+    };
 
-  const onExpand = (id: string, depth: number) => {
-    if (expanded.includes(id)) {
-      const newExpanded = [...expanded];
-      newExpanded.splice(newExpanded.indexOf(id), 1);
-      onExpandedElementChange(newExpanded, Math.max(maxDepth, depth));
+    const dragOver: React.DragEventHandler<HTMLDivElement> = (event) => {
+      event.preventDefault();
+    };
+
+    const onDropItem: React.DragEventHandler<HTMLDivElement> = (event) => {
+      const dragSourcesStringified = event.dataTransfer.getData(DRAG_SOURCES_TYPE);
+      const selectedIds = JSON.parse(dragSourcesStringified);
+      onDropTreeItem(selectedIds, item.id, -1);
+      event.preventDefault();
+    };
+
+    const onDropBefore: React.DragEventHandler<HTMLDivElement> = (event) => {
+      const dragSourcesStringified = event.dataTransfer.getData(DRAG_SOURCES_TYPE);
+      const selectedIds = JSON.parse(dragSourcesStringified);
+      onDropTreeItem(selectedIds, item.id, itemIndex);
+      event.preventDefault();
+    };
+
+    const text: JSX.Element = state.editingMode ? (
+      <TreeItemDirectEditInput
+        editingContextId={editingContextId}
+        treeId={treeId}
+        treeItemId={item.id}
+        editingKey={state.editingKey}
+        onClose={onCloseEditingMode}
+      />
+    ) : (
+      <StyledLabel styledString={item.label} selected={false} textToHighlight={textToHighlight ?? ''} marked={marked} />
+    );
+
+    const onExpand = (id: string, depth: number) => {
+      if (expanded.includes(id)) {
+        const newExpanded = [...expanded];
+        newExpanded.splice(newExpanded.indexOf(id), 1);
+        onExpandedElementChange(newExpanded, Math.max(maxDepth, depth));
+      } else {
+        onExpandedElementChange([...expanded, id], Math.max(maxDepth, depth));
+      }
+    };
+
+    let currentTreeItem: JSX.Element | null;
+    if (textToFilter && isFilterCandidate(item, textToFilter)) {
+      currentTreeItem = null;
     } else {
-      onExpandedElementChange([...expanded, id], Math.max(maxDepth, depth));
-    }
-  };
-
-  let currentTreeItem: JSX.Element | null;
-  if (textToFilter && isFilterCandidate(item, textToFilter)) {
-    currentTreeItem = null;
-  } else {
-    const label = getString(item.label);
-    const tooltipText = getTooltipText(item);
-    /* ref, tabindex and onFocus are used to set the React component focusabled and to set the focus to the corresponding DOM part */
-    currentTreeItem = (
-      <>
-        <div
-          className={`${state.partHovered === 'before' ? classes.treeItemHover : ''} ${classes.treeItemBefore}`}
-          onDrop={onDropBefore}
-          onDragEnter={() => handleMouseEnter('before')}
-          onDragExit={handleMouseLeave}
-          onDragOver={dragOver}
-          data-testid={`${dataTestid}-drop-before`}
-        />
-        <div
-          className={className}
-          onClick={onClick}
-          onDragEnter={() => handleMouseEnter('item')}
-          onDragExit={handleMouseLeave}
-          onDrop={onDropItem}
-          onMouseEnter={() => handleMouseEnter('item')}
-          onMouseLeave={handleMouseLeave}
-          data-testid={`${label}-fullrow`}>
-          <TreeItemArrow item={item} depth={depth} onExpand={onExpand} data-testid={`${label}-toggle`} />
+      const label = getString(item.label);
+      const tooltipText = getTooltipText(item);
+      /* ref, tabindex and onFocus are used to set the React component focusabled and to set the focus to the corresponding DOM part */
+      currentTreeItem = (
+        <>
           <div
-            ref={refDom}
-            tabIndex={0}
-            onKeyDown={onBeginEditing}
-            draggable
-            onDragStart={onDragStart}
+            className={`${state.partHovered === 'before' ? classes.treeItemHover : ''} ${classes.treeItemBefore}`}
+            onDrop={onDropBefore}
+            onDragEnter={() => handleMouseEnter('before')}
+            onDragExit={handleMouseLeave}
             onDragOver={dragOver}
-            data-treeitemid={item.id}
-            data-treeitemlabel={label}
-            data-treeitemkind={item.kind}
-            data-haschildren={item.hasChildren.toString()}
-            data-depth={depth}
-            data-expanded={item.expanded.toString()}
-            data-testid={dataTestid}>
-            <div className={`${classes.content} ${item.selectable ? '' : classes.nonSelectable}`}>
-              <div
-                className={`${classes.imageAndLabel} ${item.selectable ? classes.imageAndLabelSelectable : ''}`}
-                onDoubleClick={() => item.hasChildren && onExpand(item.id, depth)}
-                title={tooltipText}
-                data-testid={label}>
-                <TreeItemIcon item={item} />
-                {text}
-              </div>
-              <div onClick={onTreeItemAction}>
-                {treeItemActionRender ? (
-                  treeItemActionRender({
-                    editingContextId: editingContextId,
-                    treeId: treeId,
-                    item: item,
-                    depth: depth,
-                    expanded: expanded,
-                    maxDepth: maxDepth,
-                    onExpandedElementChange: onExpandedElementChange,
-                    readOnly: readOnly,
-                    onEnterEditingMode: enterEditingMode,
-                    isHovered: state.partHovered === 'item',
-                  })
-                ) : (
-                  <TreeItemAction
-                    editingContextId={editingContextId}
-                    treeId={treeId}
-                    item={item}
-                    depth={depth}
-                    expanded={expanded}
-                    maxDepth={maxDepth}
-                    onExpandedElementChange={onExpandedElementChange}
-                    readOnly={readOnly}
-                    onEnterEditingMode={enterEditingMode}
-                    isHovered={state.partHovered === 'item'}
-                  />
-                )}
+            data-testid={`${dataTestid}-drop-before`}
+          />
+          <div
+            className={className}
+            onClick={onClick}
+            onDragEnter={() => handleMouseEnter('item')}
+            onDragExit={handleMouseLeave}
+            onDrop={onDropItem}
+            onMouseEnter={() => handleMouseEnter('item')}
+            onMouseLeave={handleMouseLeave}
+            data-testid={`${label}-fullrow`}>
+            <TreeItemArrow item={item} depth={depth} onExpand={onExpand} data-testid={`${label}-toggle`} />
+            <div
+              ref={refDom}
+              tabIndex={0}
+              onKeyDown={onBeginEditing}
+              draggable
+              onDragStart={onDragStart}
+              onDragOver={dragOver}
+              data-treeitemid={item.id}
+              data-treeitemlabel={label}
+              data-treeitemkind={item.kind}
+              data-haschildren={item.hasChildren.toString()}
+              data-depth={depth}
+              data-expanded={item.expanded.toString()}
+              data-testid={dataTestid}>
+              <div className={`${classes.content} ${item.selectable ? '' : classes.nonSelectable}`}>
+                <div
+                  className={`${classes.imageAndLabel} ${item.selectable ? classes.imageAndLabelSelectable : ''}`}
+                  onDoubleClick={() => item.hasChildren && onExpand(item.id, depth)}
+                  title={tooltipText}
+                  data-testid={label}>
+                  <TreeItemIcon item={item} />
+                  {text}
+                </div>
+                <div onClick={onTreeItemAction}>
+                  {treeItemActionRender ? (
+                    treeItemActionRender({
+                      editingContextId: editingContextId,
+                      treeId: treeId,
+                      item: item,
+                      depth: depth,
+                      expanded: expanded,
+                      maxDepth: maxDepth,
+                      onExpandedElementChange: onExpandedElementChange,
+                      readOnly: readOnly,
+                      onEnterEditingMode: enterEditingMode,
+                      isHovered: state.partHovered === 'item',
+                    })
+                  ) : (
+                    <TreeItemAction
+                      editingContextId={editingContextId}
+                      treeId={treeId}
+                      item={item}
+                      depth={depth}
+                      expanded={expanded}
+                      maxDepth={maxDepth}
+                      onExpandedElementChange={onExpandedElementChange}
+                      readOnly={readOnly}
+                      onEnterEditingMode={enterEditingMode}
+                      isHovered={state.partHovered === 'item'}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </>
-    );
+        </>
+      );
+    }
+    return currentTreeItem;
   }
-  return currentTreeItem;
-};
+);
