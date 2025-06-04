f4267b2df0e9e44d09df895c81cee72c2888574b
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-06-04 10:44:01 +0200

    [3895] Pull onDragStart callback up in TreeView
    Use the data-treeitemid from the drag event target to make the
    callback independent on the TreeItem instance.
    
    The callback is still dependent on selectedTreeItemIds, but at least
    should change only *once* per selection change.
    
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3895
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/trees/Tree.tsx
+++ packages/trees/frontend/sirius-components-trees/src/trees/Tree.tsx
@@ -12,7 +12,6 @@
  *******************************************************************************/
 import { makeStyles } from 'tss-react/mui';
 
-import { DRAG_SOURCES_TYPE } from '@eclipse-sirius/sirius-components-core';
 import { useEffect, useRef } from 'react';
 import { TreeItem } from '../treeitems/TreeItem';
 import { TreeItemWithChildren } from '../treeitems/TreeItemWithChildren';
@@ -44,14 +43,15 @@ export const Tree = ({
   tree,
   expanded,
   maxDepth,
-  onExpandedElementChange,
   readOnly,
   textToHighlight,
   textToFilter,
   markedItemIds,
-  treeItemActionRender,
-  onTreeItemClick,
   selectedTreeItemIds,
+  onExpandedElementChange,
+  onTreeItemClick,
+  onDragStart,
+  treeItemActionRender,
 }: TreeProps) => {
   const { classes } = useTreeStyle();
   const treeElement = useRef<HTMLDivElement | null>(null);
@@ -134,17 +134,6 @@ export const Tree = ({
         {tree.children.map((childItem, index) => {
           const itemSelected = selectedTreeItemIds.some((id) => id === childItem.id);
           const itemMarked = markedItemIds.some((id) => id === childItem.id);
-          const dragStart: React.DragEventHandler<HTMLDivElement> = (event) => {
-            const isDraggedItemSelected = selectedTreeItemIds.map((id) => id).includes(childItem.id);
-            if (!isDraggedItemSelected) {
-              // If we're dragging a non-selected item, drag it alone
-              event.dataTransfer.setData(DRAG_SOURCES_TYPE, JSON.stringify([childItem.id]));
-            } else if (selectedTreeItemIds.length > 0) {
-              // Otherwise drag the whole selection
-              event.dataTransfer.setData(DRAG_SOURCES_TYPE, JSON.stringify(selectedTreeItemIds));
-            }
-          };
-
           return (
             <li key={childItem.id}>
               <TreeItem
@@ -162,7 +151,7 @@ export const Tree = ({
                 selected={itemSelected}
                 onExpandedElementChange={onExpandedElementChange}
                 onTreeItemClick={onTreeItemClick}
-                onDragStart={dragStart}
+                onDragStart={onDragStart}
                 treeItemActionRender={treeItemActionRender}
               />
               <TreeItemWithChildren
@@ -173,14 +162,15 @@ export const Tree = ({
                 depth={2}
                 expanded={expanded}
                 maxDepth={maxDepth}
-                onExpandedElementChange={onExpandedElementChange}
                 readOnly={readOnly}
                 textToHighlight={textToHighlight}
                 textToFilter={textToFilter}
                 markedItemIds={markedItemIds}
-                treeItemActionRender={treeItemActionRender}
-                onTreeItemClick={onTreeItemClick}
                 selectedTreeItemIds={selectedTreeItemIds}
+                onExpandedElementChange={onExpandedElementChange}
+                onTreeItemClick={onTreeItemClick}
+                onDragStart={onDragStart}
+                treeItemActionRender={treeItemActionRender}
               />
             </li>
           );
