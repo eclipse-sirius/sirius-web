d6bf216b7cd3d3d59183710b57783d035bab9451
Florent Latombe <florent.latombe@obeo.fr>
2025-07-08 16:29:40 +0200

    [4886] Make shareable URL include the query text from the Query view
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4886
    Signed-off-by: Florent Latombe <florent.latombe@obeo.fr>

--- packages/core/frontend/sirius-components-core/src/workbench/WorkbenchPart.tsx
+++ packages/core/frontend/sirius-components-core/src/workbench/WorkbenchPart.tsx
@@ -49,7 +49,13 @@ const useSiteStyles = makeStyles()((theme) => ({
   },
 }));
 
-export const WorkbenchPart = ({ editingContextId, readOnly, side, contribution }: WorkbenchPartProps) => {
+export const WorkbenchPart = ({
+  editingContextId,
+  readOnly,
+  side,
+  contribution,
+  initialConfiguration,
+}: WorkbenchPartProps) => {
   const { classes } = useSiteStyles();
 
   const { title, icon, component: Component } = contribution;
@@ -60,7 +66,12 @@ export const WorkbenchPart = ({ editingContextId, readOnly, side, contribution }
         <Typography className={classes.viewHeaderTitle}>{title}</Typography>
       </div>
       <div className={classes.viewContent} data-testid={`view-${title}`}>
-        <Component editingContextId={editingContextId} readOnly={readOnly} />
+        <Component
+          editingContextId={editingContextId}
+          readOnly={readOnly}
+          initialConfiguration={initialConfiguration}
+          ref={contribution.ref}
+        />
       </div>
     </div>
   );
--- packages/validation/frontend/sirius-components-validation/src/Validation.types.ts
+++ packages/validation/frontend/sirius-components-validation/src/Validation.types.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo and others.
+ * Copyright (c) 2024, 2025 Obeo and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,13 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+import { WorkbenchViewConfiguration, WorkbenchViewHandle } from '@eclipse-sirius/sirius-components-core';
 import { GQLValidation } from './useValidationViewSubscription.types';
 export interface ValidationRepresentationState {
   validationPayload: GQLValidation | null;
 }
+export interface ValidationViewHandle extends WorkbenchViewHandle {
+  getWorkbenchViewConfiguration: () => ValidationViewConfiguration | null;
+}
+
+export interface ValidationViewConfiguration extends WorkbenchViewConfiguration {}
