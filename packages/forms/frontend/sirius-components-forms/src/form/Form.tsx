b8b32b49c5ff657ae765967f5624c19958493f76
Florent Latombe <florent.latombe@obeo.fr>
2025-07-11 15:36:14 +0200

    [4886] WIP - Make shareable URL include Details active page ID
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4886
    Signed-off-by: Florent Latombe <florent.latombe@obeo.fr>

--- packages/forms/frontend/sirius-components-forms/src/form/Form.tsx
+++ packages/forms/frontend/sirius-components-forms/src/form/Form.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2024 Obeo.
+ * Copyright (c) 2019, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,15 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+import { WorkbenchViewHandle } from '@eclipse-sirius/sirius-components-core';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
+import React, { ForwardedRef, forwardRef, useEffect, useImperativeHandle, useState } from 'react';
 import { makeStyles } from 'tss-react/mui';
-import React, { useEffect, useState } from 'react';
 import { Page } from '../pages/Page';
 import { ToolbarAction } from '../toolbaraction/ToolbarAction';
 import { FormProps, FormState } from './Form.types';
+import { GQLPage } from './FormEventFragments.types';
 
 const useFormStyles = makeStyles()((theme) => ({
   form: {
@@ -71,81 +73,111 @@ const a11yProps = (id: string) => {
   };
 };
 
-export const Form = ({ editingContextId, form, readOnly }: FormProps) => {
-  const { classes } = useFormStyles();
-  const { id, pages } = form;
+export const Form = forwardRef<WorkbenchViewHandle | null, FormProps>(
+  (
+    { editingContextId, form, readOnly, initiallySelectedPageId }: FormProps,
+    refWorkbenchViewHandle: ForwardedRef<WorkbenchViewHandle | null>
+  ) => {
+    const { classes } = useFormStyles();
+    const { id, pages } = form;
 
-  const [state, setState] = useState<FormState>({ selectedPage: pages[0], pages });
+    const initiallySelectedPage: GQLPage = initiallySelectedPageId
+      ? pages.find((page) => page.id === initiallySelectedPageId) ?? pages[0]
+      : pages[0];
 
-  useEffect(() => {
-    setState(() => {
-      const selectedPage = pages.find((page) => page.id === state.selectedPage?.id);
-      if (selectedPage) {
-        return { selectedPage, pages };
-      }
-      return { selectedPage: pages[0], pages };
-    });
-  }, [pages, state.selectedPage.id]);
+    const [state, setState] = useState<FormState>({ selectedPage: initiallySelectedPage, pages });
 
-  const onChangeTab = (_: React.ChangeEvent<{}>, value: string) => {
-    const selectedPage = pages.find((page) => page.id === value);
-    setState((prevState) => {
-      return { ...prevState, selectedPage };
-    });
-  };
+    useImperativeHandle(
+      refWorkbenchViewHandle,
+      () => {
+        return {
+          getWorkbenchViewConfiguration: () => {
+            return {
+              id: undefined,
+              isActive: undefined,
+              selectedPageId: state.selectedPage.id,
+            };
+          },
+        };
+      },
+      [state.selectedPage]
+    );
 
-  let selectedPageToolbar: JSX.Element | null = null;
-  if (state.selectedPage?.toolbarActions?.length > 0) {
-    selectedPageToolbar = (
-      <div className={classes.toolbar}>
-        {state.selectedPage.toolbarActions.map((toolbarAction) => (
-          <div className={classes.toolbarAction} key={toolbarAction.id}>
-            <ToolbarAction editingContextId={editingContextId} formId={id} readOnly={readOnly} widget={toolbarAction} />
-          </div>
-        ))}
+    useEffect(() => {
+      setState(() => {
+        const selectedPage = pages.find((page) => page.id === state.selectedPage?.id);
+        if (selectedPage) {
+          return { selectedPage, pages };
+        }
+        return { selectedPage: pages[0], pages };
+      });
+    }, [pages, state.selectedPage.id]);
+
+    const onChangeTab = (_: React.ChangeEvent<{}>, value: string) => {
+      const selectedPage = pages.find((page) => page.id === value);
+      setState((prevState) => {
+        return { ...prevState, selectedPage };
+      });
+    };
+
+    let selectedPageToolbar: JSX.Element | null = null;
+    if (state.selectedPage?.toolbarActions?.length > 0) {
+      selectedPageToolbar = (
+        <div className={classes.toolbar}>
+          {state.selectedPage.toolbarActions.map((toolbarAction) => (
+            <div className={classes.toolbarAction} key={toolbarAction.id}>
+              <ToolbarAction
+                editingContextId={editingContextId}
+                formId={id}
+                readOnly={readOnly}
+                widget={toolbarAction}
+              />
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    let page: JSX.Element | null = null;
+    if (state.selectedPage) {
+      page = <Page editingContextId={editingContextId} formId={id} page={state.selectedPage} readOnly={readOnly} />;
+    }
+    const maxWidth: number = state.pages.length > 1 ? 100 : 390; // 390 is the maxWidth to fit to the default Details view
+    const variant: 'scrollable' | 'standard' = state.pages.length > 1 ? 'scrollable' : 'standard';
+    return (
+      <div data-testid="form" className={classes.form}>
+        <div className={classes.pagesListAndToolbar}>
+          <Tabs
+            classes={{ root: classes.tabsRoot }}
+            value={state.selectedPage.id}
+            onChange={onChangeTab}
+            variant={variant}
+            scrollButtons
+            textColor="primary"
+            indicatorColor="primary">
+            {state.pages.map((page) => {
+              return (
+                <Tab
+                  {...a11yProps(page.id)}
+                  classes={{ root: classes.tabRoot }}
+                  style={{ minWidth: 1, maxWidth: maxWidth }} // Set minWidth to one to force tab width to fit the page label length
+                  value={page.id}
+                  title={page.label}
+                  label={
+                    <div className={classes.tabLabel}>
+                      <div className={classes.tabLabelText}>{page.label}</div>
+                    </div>
+                  }
+                  key={page.id}
+                  data-testid={`page-tab-${page.label}`}
+                />
+              );
+            })}
+          </Tabs>
+          {selectedPageToolbar}
+        </div>
+        {page}
       </div>
     );
   }
-
-  let page: JSX.Element | null = null;
-  if (state.selectedPage) {
-    page = <Page editingContextId={editingContextId} formId={id} page={state.selectedPage} readOnly={readOnly} />;
-  }
-  const maxWidth: number = state.pages.length > 1 ? 100 : 390; // 390 is the maxWidth to fit to the default Details view
-  const variant: 'scrollable' | 'standard' = state.pages.length > 1 ? 'scrollable' : 'standard';
-  return (
-    <div data-testid="form" className={classes.form}>
-      <div className={classes.pagesListAndToolbar}>
-        <Tabs
-          classes={{ root: classes.tabsRoot }}
-          value={state.selectedPage.id}
-          onChange={onChangeTab}
-          variant={variant}
-          scrollButtons
-          textColor="primary"
-          indicatorColor="primary">
-          {state.pages.map((page) => {
-            return (
-              <Tab
-                {...a11yProps(page.id)}
-                classes={{ root: classes.tabRoot }}
-                style={{ minWidth: 1, maxWidth: maxWidth }} // Set minWidth to one to force tab width to fit the page label length
-                value={page.id}
-                title={page.label}
-                label={
-                  <div className={classes.tabLabel}>
-                    <div className={classes.tabLabelText}>{page.label}</div>
-                  </div>
-                }
-                key={page.id}
-                data-testid={`page-tab-${page.label}`}
-              />
-            );
-          })}
-        </Tabs>
-        {selectedPageToolbar}
-      </div>
-      {page}
-    </div>
-  );
-};
+);
