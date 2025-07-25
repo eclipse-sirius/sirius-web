a2182c70fcfb6f2f8ea8df18c0f33153b07f547e
Guillaume Coutable <guillaume.coutable@obeo.fr>
2025-07-25 12:13:03 +0200

    [4217] Fix project rename with invalid project name
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4217
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- integration-tests/cypress/pages/Project.ts
+++ integration-tests/cypress/pages/Project.ts
@@ -11,6 +11,8 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 
+import { RenameProjectDialog } from './RenameProject';
+
 export class Project {
   public visit(projectId: string): Cypress.Chainable<Cypress.AUTWindow> {
     return cy.visit(`/projects/${projectId}/edit`, {
@@ -31,9 +33,18 @@ export class Project {
   }
 
   public getProjectNavigationBar(projectName: string): ProjectNavigationBar {
+    this.isLoaded();
     cy.getByTestId(`navbar-${projectName}`).should('be.visible');
     cy.getByTestId('navigation-bar').findByTestId('more').click();
     return new ProjectNavigationBar();
+  }
+
+  private isLoaded(): void {
+    cy.get('@consoleDebug').should('be.calledWith', 'query getProjectAndRepresentation: response received');
+    cy.get('@consoleDebug').should('be.calledWith', 'query getOnboardData: response received');
+    cy.get('@consoleDebug').should('be.calledWith', 'query getRepresentationMetadata: response received');
+    cy.get('@consoleDebug').should('be.calledWith', 'query getAllTreeFilters: response received');
+    cy.get('@consoleDebug').should('be.calledWith', 'query getAllExplorerDescriptions: response received');
   }
 }
 
@@ -44,6 +55,11 @@ class ProjectNavigationBar {
 
   public getRenameButton(): Cypress.Chainable<JQuery<HTMLElement>> {
     return cy.getByTestId('rename');
+  }
+
+  public getRenameDialog(): RenameProjectDialog {
+    this.getRenameButton().should('exist').click();
+    return new RenameProjectDialog();
   }
 
   public getDeleteButton(): Cypress.Chainable<JQuery<HTMLElement>> {
