b570335cfebdfec62f45ef3fad66a325452ba9d1
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2025-08-08 15:43:49 +0200

    [test] Remove unstable tests relying on wait()
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

--- integration-tests/cypress/e2e/project/diagrams/border-node.cy.ts
+++ integration-tests/cypress/e2e/project/diagrams/border-node.cy.ts
@@ -12,9 +12,9 @@
  *******************************************************************************/
 
 import { Studio } from '../../../usecases/Studio';
-import { Explorer } from '../../../workbench/Explorer';
 import { Details } from '../../../workbench/Details';
 import { Diagram } from '../../../workbench/Diagram';
+import { Explorer } from '../../../workbench/Explorer';
 
 describe('Diagram - border node', () => {
   const domainName: string = 'diagramBorderNode';
@@ -36,42 +36,9 @@ describe('Diagram - border node', () => {
         explorer.createObject('Root', 'entity1s-Entity1');
         details.getTextField('Name').type('parent{enter}');
         new Explorer().createRepresentation('Root', diagramDescriptionName, diagramTitle);
-        new Diagram().centerViewport();
       });
 
       afterEach(() => cy.deleteProject(instanceProjectId));
-
-      it('Then during parent resize, the left border node position is updated', () => {
-        const diagram = new Diagram();
-        diagram.selectNode(diagramTitle, 'parent');
-        let initialTransformX: number;
-
-        // Get the initial transform value
-        diagram
-          .getNodes(diagramTitle, 'border')
-          .invoke('attr', 'style')
-          .then((style) => {
-            // Extract the initial x translation value
-            const transformMatch = style?.match(/translate\(([^,]+),/);
-            initialTransformX = transformMatch ? parseInt(transformMatch[1] ?? '', 10) : 0;
-          });
-
-        // Perform the resize action
-        diagram.resizeNodeFeedback('bottom.right', { x: -25, y: 0 });
-
-        // Check the new transform value
-        diagram
-          .getNodes(diagramTitle, 'border')
-          .invoke('attr', 'style')
-          .then((style) => {
-            // Extract the new x translation value
-            const newTransformMatch = style?.match(/translate\(([^,]+),/);
-            const newTransformX = newTransformMatch ? parseInt(newTransformMatch[1] ?? '', 10) : 0;
-
-            // Verify that the translation has changed by 25px
-            expect(newTransformX).to.eq(initialTransformX - 25);
-          });
-      });
 
       it('Then when selected, a border node should not be resizable', () => {
         const diagram = new Diagram();
