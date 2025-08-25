4f04bc80011b3c99840d1d474a8811c030e8b76b
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-08-25 10:26:39 +0200

    [5057] Add support for customizing the behavior of the Duplicate object operation
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5057
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/views/explorer/services/api/IObjectDuplicator.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.application.views.explorer.services.api;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.components.core.api.IEditingContext;
+import org.eclipse.sirius.components.representations.IStatus;
+import org.eclipse.sirius.components.representations.Success;
+
+/**
+ * Used to provide customized behavior for object duplication.
+ *
+ * @author pcdavid
+ */
+public interface IObjectDuplicator {
+    /**
+     * The key to use to advertise the duplicate object created in {@link Success#getParameters()} on success.
+     */
+    String NEW_OBJECT = "newObject";
+
+    boolean canHandle(IEditingContext editingContext, EObject objectToDuplicate, EObject containerEObject, String containmentFeature, DuplicationSettings settings);
+
+    IStatus duplicateObject(IEditingContext editingContext, EObject objectToDuplicate, EObject containerEObject, String containmentFeature, DuplicationSettings settings);
+}
