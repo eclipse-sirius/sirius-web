23d2cca8cc0b85777c48360b134ee13858ffc856
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-08-29 10:10:14 +0200

    [4214] Remove the synchronizedWithSelection mode from the Explorer
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/4214
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/toolbar/TreeToolBar.tsx
+++ packages/trees/frontend/sirius-components-trees/src/toolbar/TreeToolBar.tsx
@@ -11,8 +11,6 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 
-import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
-import IconButton from '@mui/material/IconButton';
 import React from 'react';
 import { makeStyles } from 'tss-react/mui';
 import { TreeFiltersMenu } from '../views/TreeFiltersMenu';
@@ -36,8 +34,6 @@ const useTreeToolbarStyles = makeStyles()((theme) => ({
 
 export const TreeToolBar = ({
   editingContextId,
-  onSynchronizedClick,
-  synchronized,
   treeFilters,
   onTreeFilterMenuItemClick,
   treeToolBarContributionComponents,
@@ -46,9 +42,6 @@ export const TreeToolBar = ({
 }: TreeToolBarProps) => {
   const { classes } = useTreeToolbarStyles();
 
-  const preferenceButtonSynchronizeTitle = synchronized
-    ? 'Disable synchronization with representation'
-    : 'Enable synchronization with representation';
   return (
     <>
       <div className={classes.toolbar}>
@@ -65,15 +58,6 @@ export const TreeToolBar = ({
           <TreeFiltersMenu filters={treeFilters} onTreeFilterMenuItemClick={onTreeFilterMenuItemClick} />
         ) : null}
         {children}
-        <IconButton
-          color="inherit"
-          size="small"
-          aria-label={preferenceButtonSynchronizeTitle}
-          title={preferenceButtonSynchronizeTitle}
-          onClick={onSynchronizedClick}
-          data-testid="tree-synchronize">
-          <SwapHorizIcon color={synchronized ? 'inherit' : 'disabled'} />
-        </IconButton>
       </div>
     </>
   );
