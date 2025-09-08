81faea6a11e27798a445edc83169e51711f2353a
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2025-09-08 16:03:44 +0200

    [5458] Add support for oblique edge type
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5458
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/EdgeTypes.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/EdgeTypes.ts
@@ -16,15 +16,18 @@ import { FC } from 'react';
 import { MultiLabelEdgeData } from './MultiLabelEdge.types';
 import { SmartStepEdgeWrapper } from './SmartStepEdgeWrapper';
 import { SmoothStepEdgeWrapper } from './SmoothStepEdgeWrapper';
+import { ObliqueEdgeWrapper } from './ObliqueEdgeWrapper';
 
 export const edgeTypes: EdgeComponentsMap = {
   Manhattan: SmoothStepEdgeWrapper,
   SmartManhattan: SmartStepEdgeWrapper,
+  Oblique: ObliqueEdgeWrapper,
 };
 
 export interface EdgeDataMap {
   Manhattan: MultiLabelEdgeData;
   SmartManhattan: MultiLabelEdgeData;
+  Oblique: MultiLabelEdgeData;
 }
 
 export type EdgeComponentsMap = {
