a7b42fb867edcce6031ed92775c9ee87151d8d7e
Denis Nikiforov <denis.nikif@gmail.com>
2025-09-11 20:02:19 +0300

    [5483] Select a target object on diagram pane click
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5483
    Signed-off-by: Denis Nikiforov <denis.nikif@gmail.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/DiagramRenderer.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/DiagramRenderer.tsx
@@ -259,7 +259,7 @@ export const DiagramRenderer = memo(({ diagramRefreshedEventPayload }: DiagramRe
   }, [edges.map((edge) => edge.id + edge.selected).join(), readOnly]);
 
   const { onShiftSelection, setShiftSelection } = useShiftSelection();
-  useDiagramSelection(onShiftSelection);
+  useDiagramSelection(diagramRefreshedEventPayload.diagram.targetObjectId, onShiftSelection);
   const { transformBorderNodeChanges } = useBorderChange();
   const { transformUndraggableListNodeChanges, applyMoveChange } = useMoveChange();
   const { transformResizeListNodeChanges } = useResizeChange();
@@ -323,6 +323,15 @@ export const DiagramRenderer = memo(({ diagramRefreshedEventPayload }: DiagramRe
     },
     [onEdgesChange]
   );
+
+  const handlePaneClick = useCallback(() => {
+    setSelection({
+      entries: [
+        { id: diagramRefreshedEventPayload.diagram.targetObjectId },
+        { id: diagramRefreshedEventPayload.diagram.id },
+      ],
+    });
+  }, [diagramRefreshedEventPayload.diagram.id, diagramRefreshedEventPayload.diagram.targetObjectId, setSelection]);
 
   const handlePaneContextMenu = useCallback(
     (event: MouseEvent | React.MouseEvent<Element, MouseEvent>) => {
@@ -443,6 +452,7 @@ export const DiagramRenderer = memo(({ diagramRefreshedEventPayload }: DiagramRe
     onReconnectEnd: onReconnectEdgeEnd,
     connectionRadius: 0,
     onEdgesChange: handleEdgesChange,
+    onPaneClick: handlePaneClick,
     onPaneContextMenu: handlePaneContextMenu,
     onEdgeContextMenu: handleEdgeContextMenu,
     onNodeContextMenu: handleNodeContextMenu,
