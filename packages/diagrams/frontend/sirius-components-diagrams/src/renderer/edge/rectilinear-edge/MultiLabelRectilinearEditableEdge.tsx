81e68ad60c7abb27557ea5e9a7c32a72eac25880
Denis Nikiforov <denis.nikif@gmail.com>
2025-09-14 17:16:46 +0300

    [5485] Edge bending points are above line segments
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5485
    Signed-off-by: Denis Nikiforov <denis.nikif@gmail.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/rectilinear-edge/MultiLabelRectilinearEditableEdge.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/rectilinear-edge/MultiLabelRectilinearEditableEdge.tsx
@@ -184,6 +184,20 @@ export const MultiLabelRectilinearEditableEdge = memo(
         />
         {selected ? <EdgeCreationHandle edgeId={id} edgePath={edgePath}></EdgeCreationHandle> : null}
         {selected &&
+          middleBendingPoints &&
+          middleBendingPoints.map((point, index) => (
+            <TemporaryMovingLine
+              key={index}
+              x={point.x}
+              y={point.y}
+              direction={point.direction}
+              segmentLength={point.segmentLength}
+              index={index}
+              onDrag={onTemporaryLineDrag}
+              onDragStop={onTemporaryLineDragStop}
+            />
+          ))}
+        {selected &&
           localBendingPoints &&
           localBendingPoints.map((point, index) => {
             const reorderBendPoint = [...localBendingPoints].sort((a, b) => a.pathOrder - b.pathOrder);
@@ -202,20 +216,6 @@ export const MultiLabelRectilinearEditableEdge = memo(
               />
             );
           })}
-        {selected &&
-          middleBendingPoints &&
-          middleBendingPoints.map((point, index) => (
-            <TemporaryMovingLine
-              key={index}
-              x={point.x}
-              y={point.y}
-              direction={point.direction}
-              segmentLength={point.segmentLength}
-              index={index}
-              onDrag={onTemporaryLineDrag}
-              onDragStop={onTemporaryLineDragStop}
-            />
-          ))}
         {data ? (
           <DraggableEdgeLabels
             id={id}
