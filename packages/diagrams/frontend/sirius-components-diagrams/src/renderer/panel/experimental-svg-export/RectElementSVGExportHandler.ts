8e31b2c3eb6ad6a1a8c4e62a840f9d8a0a449761
Guillaume Coutable <guillaume.coutable@obeo.fr>
2025-09-17 10:45:16 +0200

    [enh] Prevent compartment separator to be rendered above everything
    Instead of rendering independent border at the very end, render them right after their "container" rect.
    Thus, we have to slightly adjust the position of borders when they are representing a separator of compartment in a list layout to prevent the next children to overlap half of the border.
    
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/panel/experimental-svg-export/RectElementSVGExportHandler.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/panel/experimental-svg-export/RectElementSVGExportHandler.ts
@@ -19,7 +19,7 @@ import { IElementSVGExportHandler, svgNamespace } from './SVGExportEngine.types'
  */
 export class RectElementSVGExportHandler implements IElementSVGExportHandler {
   canHandle(element: Element): boolean {
-    return element.getAttribute('data-svg') === 'rect';
+    return element.getAttribute('data-svg')?.includes('rect') === true;
   }
 
   handle(element: Element, parentSvgElement: SVGElement, svgDocument: XMLDocument): SVGElement[] {
@@ -28,11 +28,18 @@ export class RectElementSVGExportHandler implements IElementSVGExportHandler {
       const rect: SVGRectElement = svgDocument.createElementNS(svgNamespace, 'rect');
       const bounds = element.getBoundingClientRect();
       const style = window.getComputedStyle(element);
+      const isCompartment: boolean = element.getAttribute('data-svg')?.includes('compartment') === true;
 
+      // Epsilon are used to correct the position and the size of the rectangle due to the difference between HTML and SVG border rendering.
+      // While in HTML border are drawn inside the element box, in SVG, they are drawn half inside and half outside of the element box.
+      // Thus we have to move the rectangle and reduce its size by the sum of the half border widths.
+      let horizontalEpsilon: number = 0;
+      let verticalEpsilon: number = 0;
       const borders: SVGElement[] = [];
       if (style.borderWidth.includes('0px')) {
         // The rectangle is not complete, thus we have to handle each side with an svg path
         if (style.borderTopWidth !== '0px') {
+          verticalEpsilon = verticalEpsilon + parseFloat(style.borderTopWidth) / 2;
           const topBorder: SVGPathElement = svgDocument.createElementNS(svgNamespace, 'path');
           topBorder.setAttribute('d', `M${bounds.left} ${bounds.top} h ${bounds.width}`);
           topBorder.setAttribute('fill', 'none');
@@ -42,8 +49,15 @@ export class RectElementSVGExportHandler implements IElementSVGExportHandler {
           borders.push(topBorder);
         }
         if (style.borderBottomWidth !== '0px') {
+          verticalEpsilon = verticalEpsilon + parseFloat(style.borderBottomWidth) / 2;
           const bottomBorder: SVGPathElement = svgDocument.createElementNS(svgNamespace, 'path');
-          bottomBorder.setAttribute('d', `M${bounds.left} ${bounds.bottom} h ${bounds.width}`);
+          if (isCompartment) {
+            // In case of a rectangle representing a compartment in a vertical list, we slightly move the border up to prevent the next sibling to overlap half of the border
+            // (the next sibling is unable to know if it has a previous sibling and thus, it is not able to correct its position)
+            bottomBorder.setAttribute('d', `M${bounds.left} ${bounds.bottom - verticalEpsilon} h ${bounds.width}`);
+          } else {
+            bottomBorder.setAttribute('d', `M${bounds.left} ${bounds.bottom} h ${bounds.width}`);
+          }
           bottomBorder.setAttribute('fill', 'none');
           bottomBorder.setAttribute('stroke', style.borderBottomColor);
           bottomBorder.setAttribute('stroke-width', style.borderBottomWidth);
@@ -51,6 +65,7 @@ export class RectElementSVGExportHandler implements IElementSVGExportHandler {
           borders.push(bottomBorder);
         }
         if (style.borderLeftWidth !== '0px') {
+          horizontalEpsilon = horizontalEpsilon + parseFloat(style.borderLeftWidth) / 2;
           const leftBorder: SVGPathElement = svgDocument.createElementNS(svgNamespace, 'path');
           leftBorder.setAttribute('d', `M${bounds.left} ${bounds.top} v ${bounds.height}`);
           leftBorder.setAttribute('fill', 'none');
@@ -60,8 +75,15 @@ export class RectElementSVGExportHandler implements IElementSVGExportHandler {
           borders.push(leftBorder);
         }
         if (style.borderRightWidth !== '0px') {
+          horizontalEpsilon = horizontalEpsilon + parseFloat(style.borderRightWidth) / 2;
           const rightBorder: SVGPathElement = svgDocument.createElementNS(svgNamespace, 'path');
-          rightBorder.setAttribute('d', `M${bounds.right} ${bounds.top} v ${bounds.height}`);
+          if (isCompartment) {
+            // In case of a rectangle representing a compartment in a horizontal list, we slightly move the border to the left to prevent the next sibling to overlap half of the border
+            // (the next sibling is unable to know if it has a previous sibling and thus, it is not able to correct its position)
+            rightBorder.setAttribute('d', `M${bounds.right - horizontalEpsilon} ${bounds.top} v ${bounds.height}`);
+          } else {
+            rightBorder.setAttribute('d', `M${bounds.right} ${bounds.top} v ${bounds.height}`);
+          }
           rightBorder.setAttribute('fill', 'none');
           rightBorder.setAttribute('stroke', style.borderRightColor);
           rightBorder.setAttribute('stroke-width', style.borderRightWidth);
@@ -72,12 +94,18 @@ export class RectElementSVGExportHandler implements IElementSVGExportHandler {
         rect.setAttribute('stroke', style.borderColor);
         rect.setAttribute('stroke-width', style.borderWidth);
         this.setBorderStyle(rect, style.borderStyle);
+        horizontalEpsilon = horizontalEpsilon + parseFloat(style.borderWidth);
+        verticalEpsilon = verticalEpsilon + parseFloat(style.borderWidth);
       }
 
-      rect.setAttribute('width', String(bounds.width));
-      rect.setAttribute('height', String(bounds.height));
-      rect.setAttribute('x', String(bounds.left));
-      rect.setAttribute('y', String(bounds.top));
+      rect.setAttribute('width', String(bounds.width - horizontalEpsilon));
+      if (isCompartment) {
+        rect.setAttribute('height', String(bounds.height - verticalEpsilon * 2));
+      } else {
+        rect.setAttribute('height', String(bounds.height - verticalEpsilon));
+      }
+      rect.setAttribute('x', String(bounds.left + horizontalEpsilon / 2));
+      rect.setAttribute('y', String(bounds.top + verticalEpsilon / 2));
       rect.setAttribute('rx', String(style.borderRadius));
       rect.setAttribute('fill', style.backgroundColor);
 
