1ad3287d2ad5da902377d4e1aca80493c31ec888
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2025-09-17 14:55:34 +0200

    [test] Add test for deepest node rendering
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/3029
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

---
+++ integration-tests-playwright/playwright/e2e/child-node-depth.spec.ts
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+import { test, expect } from '@playwright/test';
+import { PlaywrightProject } from '../helpers/PlaywrightProject';
+import { PlaywrightExplorer } from '../helpers/PlaywrightExplorer';
+import { PlaywrightNode } from '../helpers/PlaywrightNode';
+
+test.describe('diagram - self contained node', () => {
+  let projectId;
+  test.beforeEach(async ({ page, request }) => {
+    const project = await new PlaywrightProject(request).createProject('diagram-self-contained');
+    projectId = project.projectId;
+
+    await page.goto(`/projects/${projectId}/edit`);
+    const playwrightExplorer = new PlaywrightExplorer(page);
+    await playwrightExplorer.uploadDocument('diagramSelfContained.xml');
+    await playwrightExplorer.expand('diagramSelfContained.xml');
+    await playwrightExplorer.createRepresentation('Root', 'diagramSelfContained - self contained', 'diagram');
+  });
+
+  test.afterEach(async ({ request }) => {
+    await new PlaywrightProject(request).deleteProject(projectId);
+  });
+
+  test('when depth of 12 child nodes, then the deepest node is render', async ({ page }) => {
+    const playwrightNode = new PlaywrightNode(page, 'deepest');
+    await expect(playwrightNode.nodeLocator).toBeAttached();
+  });
+});
---
+++ packages/sirius-web/backend/sirius-web-e2e-tests/src/main/java/org/eclipse/sirius/web/e2e/tests/diagramselfcontained/DiagramSelfContainedViewProvider.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.sirius.web.e2e.tests.diagramselfcontained;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.components.emf.ResourceMetadataAdapter;
+import org.eclipse.sirius.components.emf.services.IDAdapter;
+import org.eclipse.sirius.components.emf.services.JSONResourceFactory;
+import org.eclipse.sirius.components.view.View;
+import org.eclipse.sirius.components.view.builder.generated.diagram.DiagramBuilders;
+import org.eclipse.sirius.components.view.builder.generated.view.ViewBuilders;
+import org.eclipse.sirius.components.view.builder.providers.IColorProvider;
+import org.eclipse.sirius.components.view.diagram.ArrangeLayoutDirection;
+import org.eclipse.sirius.components.view.diagram.DiagramDescription;
+import org.eclipse.sirius.components.view.diagram.LineStyle;
+import org.eclipse.sirius.components.view.diagram.NodeDescription;
+import org.eclipse.sirius.components.view.diagram.SynchronizationPolicy;
+import org.eclipse.sirius.components.view.diagram.UserResizableDirection;
+import org.eclipse.sirius.emfjson.resource.JsonResource;
+import org.eclipse.sirius.web.e2e.tests.services.SiriusWebE2EColorPaletteBuilderProvider;
+import org.eclipse.sirius.web.e2e.tests.services.SiriusWebE2EColorProvider;
+import org.eclipse.sirius.web.e2e.tests.services.api.IE2EViewProvider;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * The view provider for the border-node.cy.ts test suite.
+ *
+ * @author frouene
+ */
+@Profile("test")
+@Service
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+public class DiagramSelfContainedViewProvider implements IE2EViewProvider {
+
+    @Override
+    public List<View> createViews() {
+        var colorPalette = new SiriusWebE2EColorPaletteBuilderProvider().getColorPaletteBuilder().build();
+        IColorProvider colorProvider = new SiriusWebE2EColorProvider(List.of(colorPalette));
+
+        var fullyDisplayInsideLabelDiagramDescription = this.fullyDisplayInsideLabelDiagramDescription(colorProvider);
+
+        var view = new ViewBuilders().newView()
+                .colorPalettes(colorPalette)
+                .descriptions(
+                        fullyDisplayInsideLabelDiagramDescription
+                )
+                .build();
+
+        view.eAllContents().forEachRemaining(eObject -> {
+            eObject.eAdapters().add(new IDAdapter(UUID.nameUUIDFromBytes(EcoreUtil.getURI(eObject).toString().getBytes())));
+        });
+
+        String resourcePath = UUID.nameUUIDFromBytes("diagramSelfContainedView".getBytes()).toString();
+        JsonResource resource = new JSONResourceFactory().createResourceFromPath(resourcePath);
+        resource.eAdapters().add(new ResourceMetadataAdapter("diagramSelfContainedView"));
+        resource.getContents().add(view);
+
+        return List.of(view);
+    }
+
+    private DiagramDescription fullyDisplayInsideLabelDiagramDescription(IColorProvider colorProvider) {
+        return new DiagramBuilders()
+                .newDiagramDescription()
+                .name(DiagramSelfContainedDomainProvider.DOMAIN_NAME + " - self contained")
+                .domainType(DiagramSelfContainedDomainProvider.DOMAIN_NAME + "::Root")
+                .titleExpression(DiagramSelfContainedDomainProvider.DOMAIN_NAME + " diagram")
+                .autoLayout(false)
+                .arrangeLayoutDirection(ArrangeLayoutDirection.UNDEFINED)
+                .nodeDescriptions(this.getNodeDescription(colorProvider))
+                .build();
+    }
+
+
+    private NodeDescription getNodeDescription(IColorProvider colorProvider) {
+
+        var nodeDescription = new DiagramBuilders()
+                .newNodeDescription()
+                .name("Entity Node 1")
+                .domainType(DiagramSelfContainedDomainProvider.DOMAIN_NAME + "::Entity1")
+                .semanticCandidatesExpression("aql:self.eContents()")
+                .synchronizationPolicy(SynchronizationPolicy.SYNCHRONIZED)
+                .collapsible(true)
+                .userResizable(UserResizableDirection.BOTH)
+                .keepAspectRatio(false)
+                .style(
+                        new DiagramBuilders()
+                                .newRectangularNodeStyleDescription()
+                                .background(colorProvider.getColor(SiriusWebE2EColorPaletteBuilderProvider.COLOR_BLUE))
+                                .borderColor(colorProvider.getColor(SiriusWebE2EColorPaletteBuilderProvider.BORDER_BLUE))
+                                .borderRadius(3)
+                                .borderSize(1)
+                                .borderLineStyle(LineStyle.SOLID)
+                                .childrenLayoutStrategy(new DiagramBuilders().newFreeFormLayoutStrategyDescription().build())
+                                .build()
+                )
+                .build();
+
+        nodeDescription.getReusedChildNodeDescriptions().add(nodeDescription);
+        return nodeDescription;
+    }
+
+}
