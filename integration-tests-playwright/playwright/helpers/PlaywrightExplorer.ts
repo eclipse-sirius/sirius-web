98597f2c3c418c72a83960eda27d11d7bfcd4d66
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-09-18 17:27:14 +0200

    [5527] Add Pin/Unpin support to the Details view
    This also makes it a possible 'Show in' target.
    
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5527
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- integration-tests-playwright/playwright/helpers/PlaywrightExplorer.ts
+++ integration-tests-playwright/playwright/helpers/PlaywrightExplorer.ts
@@ -10,7 +10,7 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-import type { Page, Locator } from '@playwright/test';
+import type { Locator, Page } from '@playwright/test';
 
 export class PlaywrightExplorer {
   readonly page: Page;
@@ -74,5 +74,11 @@ export class PlaywrightExplorer {
 
   async dragTo(treeItemLabel: string, target: Locator) {
     await this.explorerLocator.locator(`[data-treeitemlabel="${treeItemLabel}"]`).dragTo(target);
+  }
+
+  async showIn(treeItemLabel: string, selectionTargetId: string) {
+    await this.explorerLocator.locator(`[data-treeitemlabel="${treeItemLabel}"]`).click();
+    await this.explorerLocator.getByTestId(`${treeItemLabel}-more`).click();
+    await this.page.getByTestId(`push-selection-to-${selectionTargetId}`).click();
   }
 }
