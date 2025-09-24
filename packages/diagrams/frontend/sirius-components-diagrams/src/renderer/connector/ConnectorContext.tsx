b4fa6e220aede1b8babd4d9885b845160ccf31ba
Michael Charfadi <michael.charfadi@obeosoft.com>
2025-09-24 17:58:21 +0200

    [5560] Invalidate the connection if the target node is not candidate
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5560
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/connector/ConnectorContext.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/connector/ConnectorContext.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Obeo.
+ * Copyright (c) 2023, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -42,7 +42,7 @@ export const ConnectorContextProvider = ({ children }: ConnectorContextProviderP
     isNewConnection: false,
   });
 
-  const setConnection = (connection: Connection) => {
+  const setConnection = (connection: Connection | null) => {
     setState((prevState) => ({ ...prevState, connection }));
   };
 
