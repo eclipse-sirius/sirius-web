b753ff2d00589625475b42e162ea44ac1561aef6
Michael Charfadi <michael.charfadi@obeosoft.com>
2025-10-01 14:24:48 +0200

    [5608] Hide empty sections on the palette
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5608
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- integration-tests-playwright/playwright/e2e/palette.spec.ts
+++ integration-tests-playwright/playwright/e2e/palette.spec.ts
@@ -11,8 +11,10 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 import { test, expect } from '@playwright/test';
-import { PlaywrightProject } from '../helpers/PlaywrightProject';
+import { PlaywrightDetails } from '../helpers/PlaywrightDetails';
+import { PlaywrightExplorer } from '../helpers/PlaywrightExplorer';
 import { PlaywrightNode } from '../helpers/PlaywrightNode';
+import { PlaywrightProject } from '../helpers/PlaywrightProject';
 
 test.describe('diagram - palette', () => {
   let projectId;
@@ -27,7 +29,7 @@ test.describe('diagram - palette', () => {
     await new PlaywrightProject(request).deleteProject(projectId);
   });
 
-  test('When align tool is triggered on two elements, then they share same position', async ({ page }) => {
+  test('when align tool is triggered on two elements, then they share same position', async ({ page }) => {
     const playwrightNode = new PlaywrightNode(page, 'DataSource1');
     const playwrightNode2 = new PlaywrightNode(page, 'CompositeProcessor1');
     await playwrightNode.click();
@@ -51,5 +53,44 @@ test.describe('diagram - palette', () => {
     await expect(page.getByTestId('GroupPalette')).toBeAttached();
     await expect(page.getByTestId('GroupPalette').getByTestId('Align left')).not.toBeAttached();
     await expect(page.getByTestId('GroupPalette').getByTestId('Align bottom')).toBeAttached();
+  });
+});
+
+test.describe('diagram - palette tool section', () => {
+  let projectId;
+  test.beforeEach(async ({ page, request }) => {
+    await page.addInitScript(() => {
+      // @ts-expect-error: we use a variable in the DOM to disable `fitView` functionality for Cypress tests.
+      window.document.DEACTIVATE_FIT_VIEW_FOR_CYPRESS_TESTS = true;
+    });
+    const project = await new PlaywrightProject(request).createProject('diagram-palette');
+    projectId = project.projectId;
+
+    await page.goto(`/projects/${projectId}/edit`);
+    const playwrightExplorer = new PlaywrightExplorer(page);
+    await playwrightExplorer.uploadDocument('diagramPalette.xml');
+    await playwrightExplorer.expand('diagramPalette.xml');
+    await playwrightExplorer.createRepresentation('Root', 'diagramPalette - palette', 'diagram');
+  });
+
+  test.afterEach(async ({ request }) => {
+    await new PlaywrightProject(request).deleteProject(projectId);
+  });
+
+  test('when a diagram palette is opened, then only not empty tool sections are displayed', async ({ page }) => {
+    await page.getByTestId('rf__wrapper').click({ button: 'right', position: { x: 250, y: 250 } });
+    await expect(page.getByTestId('Palette')).toBeAttached();
+    await expect(page.getByTestId('toolSection-DiagramToolSection')).toBeAttached();
+    await expect(page.getByTestId('toolSection-EmptyToolSection')).not.toBeAttached();
+  });
+
+  test('when a palette contains an empty tool section, then this tool section is not displayed', async ({ page }) => {
+    const playwrightNode = new PlaywrightNode(page, 'Entity1');
+    await playwrightNode.openPalette();
+    await expect(page.getByTestId('toolSection-NodeToolSection')).toBeAttached();
+    await playwrightNode.closePalette();
+    await new PlaywrightDetails(page).setText('Name', 'Empty');
+    await playwrightNode.openPalette();
+    await expect(page.getByTestId('toolSection-NodeToolSection')).not.toBeAttached();
   });
 });
---
+++ packages/sirius-web/backend/sirius-web-e2e-tests/src/main/java/org/eclipse/sirius/web/e2e/tests/diagrampalette/DiagramPaletteDomainProvider.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.sirius.web.e2e.tests.diagrampalette;
+
+import java.util.List;
+
+import org.eclipse.sirius.components.core.api.IEditingContext;
+import org.eclipse.sirius.components.domain.Attribute;
+import org.eclipse.sirius.components.domain.DataType;
+import org.eclipse.sirius.components.domain.Domain;
+import org.eclipse.sirius.components.domain.DomainFactory;
+import org.eclipse.sirius.components.domain.Entity;
+import org.eclipse.sirius.components.domain.Relation;
+import org.eclipse.sirius.web.application.studio.services.api.IDomainProvider;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Used to contribute domains for diagram palette tests.
+ *
+ * @author frouene
+ */
+@Profile("test")
+@Service
+public class DiagramPaletteDomainProvider implements IDomainProvider {
+
+    public static final String DOMAIN_NAME = "diagramPalette";
+
+    @Override
+    public List<Domain> getDomains(IEditingContext editingContext) {
+        var domain = DomainFactory.eINSTANCE.createDomain();
+        domain.setName(DOMAIN_NAME);
+
+        Entity root = DomainFactory.eINSTANCE.createEntity();
+        root.setName("Root");
+        domain.getTypes().add(root);
+
+        Entity entity1 = DomainFactory.eINSTANCE.createEntity();
+        entity1.setName("Entity1");
+        domain.getTypes().add(entity1);
+
+        Relation entity1s = DomainFactory.eINSTANCE.createRelation();
+        entity1s.setName("entity1s");
+        entity1s.setContainment(true);
+        entity1s.setOptional(true);
+        entity1s.setMany(true);
+        entity1s.setTargetType(entity1);
+        root.getRelations().add(entity1s);
+
+        this.addAttribute(entity1, "name", DataType.STRING);
+
+        return List.of(domain);
+    }
+
+    private void addAttribute(Entity entity, String name, DataType type) {
+        Attribute attr = DomainFactory.eINSTANCE.createAttribute();
+        attr.setName(name);
+        attr.setOptional(true);
+        attr.setType(type);
+        entity.getAttributes().add(attr);
+    }
+}
