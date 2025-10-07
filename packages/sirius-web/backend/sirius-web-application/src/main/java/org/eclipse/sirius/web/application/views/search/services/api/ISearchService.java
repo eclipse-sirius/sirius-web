f87ba5f383f5b7e93661aa56e685fa5b302d31f5
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-10-07 15:47:52 +0200

    [5667] Add basic search view
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5667
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/views/search/services/api/ISearchService.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.application.views.search.services.api;
+
+import java.util.List;
+
+import org.eclipse.sirius.components.core.api.IEditingContext;
+import org.eclipse.sirius.web.application.views.search.dto.SearchQuery;
+
+/**
+ * Service use to search for elements inside an editing context based on a user-supplied query.
+ *
+ * @author pcdavid
+ */
+public interface ISearchService {
+    List<Object> search(IEditingContext editingContext, SearchQuery query);
+}
---
+++ packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/search/SearchView.tsx
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+import { WorkbenchViewComponentProps, WorkbenchViewHandle } from '@eclipse-sirius/sirius-components-core';
+import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
+import { makeStyles } from 'tss-react/mui';
+import { SearchQueryInput } from './SearchQueryInput';
+import { SearchResults } from './SearchResults';
+import { SearchViewState } from './SearchView.types';
+import { useSearch } from './useSearch';
+import { GQLSearchPayload, GQLSearchSuccessPayload } from './useSearch.types';
+import { useSearchViewHandle } from './useSearchViewHandle';
+
+const useSearchViewStyles = makeStyles()((theme) => ({
+  view: {
+    display: 'grid',
+    gridTemplateRows: 'min-content min-content 1fr', // input, separator, results
+    gridTemplateColumns: '1fr',
+    overflow: 'auto',
+  },
+  separator: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+const isSearchSuccessPayload = (payload: GQLSearchPayload): payload is GQLSearchSuccessPayload =>
+  payload && payload.__typename === 'SearchSuccessPayload';
+
+export const SearchView = forwardRef<WorkbenchViewHandle, WorkbenchViewComponentProps>(
+  ({ editingContextId, id }: WorkbenchViewComponentProps, ref: ForwardedRef<WorkbenchViewHandle>) => {
+    const { classes } = useSearchViewStyles();
+
+    useSearchViewHandle(id, ref);
+
+    const [state, setState] = useState<SearchViewState>({
+      query: null,
+      result: null,
+      resultsReceivedTimestamp: null,
+    });
+
+    const { launchSearch, loading, data } = useSearch();
+    useEffect(() => {
+      if (loading) {
+        setState((prevState) => ({ ...prevState, result: null, resultsReceivedTimestamp: null }));
+      } else if (isSearchSuccessPayload(data?.viewer?.editingContext?.search)) {
+        const success: GQLSearchSuccessPayload = data.viewer.editingContext.search;
+        setState((prevState) => ({ ...prevState, result: success.result, resultsReceivedTimestamp: Date.now() }));
+      }
+    }, [loading, data]);
+
+    return (
+      <div className={classes.view} data-representation-kind="search-view">
+        <SearchQueryInput
+          onLaunchSearch={(newQuery) => {
+            setState((prevState) => ({ ...prevState, query: newQuery }));
+            launchSearch(editingContextId, newQuery);
+          }}
+        />
+        <div className={classes.separator} />
+        <SearchResults
+          loading={loading}
+          query={state.query}
+          result={state.result}
+          timestamp={state.resultsReceivedTimestamp}
+        />
+      </div>
+    );
+  }
+);
