512a37f6644e44c2f6fe9fd1c0d4575452221464
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2025-10-10 16:31:28 +0200

    [5658] Make labels non-draggable in read-only mode
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5658
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/DraggableEdgeLabels.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/edge/DraggableEdgeLabels.tsx
@@ -14,8 +14,10 @@ import { Label } from '../Label';
 import { EdgeLabelRenderer, useViewport, Position } from '@xyflow/react';
 import Draggable, { DraggableData } from 'react-draggable';
 import { DraggableEdgeLabelsProps } from './DraggableEdgeLabels.types';
-import { useRef, useMemo } from 'react';
+import { useRef, useMemo, useContext } from 'react';
 import { useLabelMove } from '../move/useLabelMove';
+import { DiagramContext } from '../../contexts/DiagramContext';
+import { DiagramContextValue } from '../../contexts/DiagramContext.types';
 
 const getTranslateFromHandlePositon = (position: Position) => {
   switch (position) {
@@ -45,6 +47,7 @@ export const DraggableEdgeLabels = ({
   const { beginLabel, endLabel, label, faded } = data || {};
   const { zoom } = useViewport();
   const { onEdgeLabelMoveStop } = useLabelMove();
+  const { readOnly } = useContext<DiagramContextValue>(DiagramContext);
   const beginLabelNodeRef = useRef<HTMLInputElement | null>(null);
   const centerLabelNodeRef = useRef<HTMLInputElement | null>(null);
   const endLabelNodeRef = useRef<HTMLInputElement | null>(null);
@@ -63,7 +66,8 @@ export const DraggableEdgeLabels = ({
           positionOffset={{ x: sourceX, y: sourceY }}
           onStop={(_e, eventData) => onStop(_e, eventData, 'begin')}
           scale={zoom}
-          nodeRef={beginLabelNodeRef}>
+          nodeRef={beginLabelNodeRef}
+          disabled={readOnly}>
           <div style={{ position: 'absolute', zIndex: 1001 }} ref={beginLabelNodeRef}>
             <div style={{ transform: sourceLabelTranslation, padding: 5 }}>
               <Label diagramElementId={id} label={beginLabel} faded={!!faded} highlighted={selected} />
@@ -77,7 +81,8 @@ export const DraggableEdgeLabels = ({
           positionOffset={{ x: edgeCenter.x, y: edgeCenter.y }}
           onStop={(_e, eventData) => onStop(_e, eventData, 'center')}
           scale={zoom}
-          nodeRef={centerLabelNodeRef}>
+          nodeRef={centerLabelNodeRef}
+          disabled={readOnly}>
           <div style={{ position: 'absolute', zIndex: 1001 }} ref={centerLabelNodeRef}>
             <div style={{ padding: 5 }}>
               <Label diagramElementId={id} label={label} faded={!!faded} highlighted={selected} />
@@ -91,7 +96,8 @@ export const DraggableEdgeLabels = ({
           positionOffset={{ x: targetX, y: targetY }}
           onStop={(_e, eventData) => onStop(_e, eventData, 'end')}
           scale={zoom}
-          nodeRef={endLabelNodeRef}>
+          nodeRef={endLabelNodeRef}
+          disabled={readOnly}>
           <div style={{ position: 'absolute', zIndex: 1001 }} ref={endLabelNodeRef}>
             <div style={{ transform: targetLabelTranslation, padding: 5 }}>
               <Label diagramElementId={id} label={endLabel} faded={!!faded} highlighted={selected} />
