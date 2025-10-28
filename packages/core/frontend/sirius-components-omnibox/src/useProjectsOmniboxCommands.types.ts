c2d9f91a11ddb2b8daaa98cb601cf466be3a9bfa
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2025-10-28 15:22:03 +0100

    [5731] Add the command palette on the projects page
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5731
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

---
+++ packages/core/frontend/sirius-components-omnibox/src/useProjectsOmniboxCommands.types.ts
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+import { LazyQueryExecFunction } from '@apollo/client';
+
+export interface UseProjectsOmniboxCommandsValue {
+  getProjectsOmniboxCommands: LazyQueryExecFunction<
+    GQLGetProjectsOmniboxCommandsQueryData,
+    GQLGetProjectsOmniboxCommandsQueryVariables
+  >;
+  loading: boolean;
+  data: GQLGetProjectsOmniboxCommandsQueryData | null;
+}
+
+export interface GQLGetProjectsOmniboxCommandsQueryVariables {
+  query: string;
+}
+
+export interface GQLGetProjectsOmniboxCommandsQueryData {
+  viewer: GQLViewer;
+}
+
+export interface GQLViewer {
+  projectsOmniboxCommands: GQLViewerOmniboxCommandsConnection;
+}
+
+export interface GQLViewerOmniboxCommandsConnection {
+  edges: GQLViewerOmniboxCommandsEdge[];
+  pageInfo: GQLPageInfo;
+}
+
+export interface GQLViewerOmniboxCommandsEdge {
+  node: GQLOmniboxCommand;
+}
+
+export interface GQLOmniboxCommand {
+  id: string;
+  label: string;
+  iconURLs: string[];
+  description: string;
+  __typename: string;
+}
+
+export interface GQLPageInfo {
+  count: number;
+}
