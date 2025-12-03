76ee25049ec43d2bef471b65d57da453fc6fdf9f
Michael Charfadi <michael.charfadi@obeosoft.com>
2025-12-03 09:21:33 +0100

    [5912] Fix edge handle refresh
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5912
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/handles/useRefreshConnectionHandles.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/handles/useRefreshConnectionHandles.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Obeo.
+ * Copyright (c) 2023, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,10 @@ export const useRefreshConnectionHandles = (id: string, connectionHandles: Conne
   const firstUpdate = useRef<boolean>(true);
 
   const connectionHandlesIdentity = connectionHandles
-    .map((handle) => `${handle.id}#${handle.index}#${handle.position}#${handle.nodeId}`)
+    .map(
+      (handle) =>
+        `${handle.id}#${handle.index}#${handle.position}#${handle.nodeId}#${handle.isVirtualHandle}#${handle.isHidden}`
+    )
     .join(', ');
 
   useEffect(() => {
