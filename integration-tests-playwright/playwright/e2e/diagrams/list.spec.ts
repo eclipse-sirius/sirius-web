5efa341b7276682cb9f98202a1dc407974768586
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2025-12-17 10:28:55 +0100

    [5969] Adjust child node positions dynamically when resizing a parent node with a header
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/5969
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- integration-tests-playwright/playwright/e2e/diagrams/list.spec.ts
+++ integration-tests-playwright/playwright/e2e/diagrams/list.spec.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import { expect, test } from '@playwright/test';
 import { PlaywrightExplorer } from '../../helpers/PlaywrightExplorer';
 import { PlaywrightNode } from '../../helpers/PlaywrightNode';
 import { PlaywrightProject } from '../../helpers/PlaywrightProject';
+import { PlaywrightDetails } from '../../helpers/PlaywrightDetails';
 
 test.describe('diagram - list', () => {
   let projectId;
@@ -60,14 +61,14 @@ test.describe('diagram - list', () => {
     const parentNodeSizeBefore = await parentNode.getReactFlowSize();
 
     await childNode.nodeLocator.click({ position: { x: 25, y: 25 } });
-    const childNodeSizeBefore = await childNode.getReactFlowSize(0, false);
+    const childNodeSizeBefore = await childNode.getReactFlowSize(null, false);
 
     await parentNode.click();
     await parentNode.resize({ height: 0, width: 50 });
 
     const parentNodeSizeAfter = await parentNode.getReactFlowSize();
     await childNode.nodeLocator.click({ position: { x: 25, y: 25 } });
-    const childNodeSizeAfter = await childNode.getReactFlowSize(0, false);
+    const childNodeSizeAfter = await childNode.getReactFlowSize(null, false);
 
     const borderWidth: number = 2;
     expect(childNodeSizeBefore.width).toBe(parentNodeSizeBefore.width - borderWidth);
@@ -86,5 +87,67 @@ test.describe('diagram - list', () => {
 
     expect(parentNodeSizeAfter.width).toBe(parentNodeSizeBefore.width);
     expect(parentNodeSizeAfter.height).toBe(parentNodeSizeBefore.height);
+  });
+});
+
+test.describe('diagram - list', () => {
+  let projectId;
+  test.beforeEach(async ({ page, request }) => {
+    await page.addInitScript(() => {
+      // @ts-expect-error: we use a variable in the DOM to disable `fitView` functionality for Cypress tests.
+      window.document.DEACTIVATE_FIT_VIEW_FOR_CYPRESS_TESTS = true;
+    });
+    const project = await new PlaywrightProject(request).createProject('diagram-growable-list', 'blank-project');
+    projectId = project.projectId;
+
+    await page.goto(`/projects/${projectId}/edit`);
+    const playwrightExplorer = new PlaywrightExplorer(page);
+    await playwrightExplorer.uploadDocument('diagramGrowableList.xml');
+    await playwrightExplorer.expand('diagramGrowableList.xml');
+    await playwrightExplorer.expand('Root');
+    await playwrightExplorer.select('Growable');
+    const detailsView = new PlaywrightDetails(page);
+    await detailsView.setText('Name', 'Test with a very large name that can be wrap');
+
+    await playwrightExplorer.createRepresentation(
+      'Root',
+      'diagramGrowableList - multiple growable list nodes',
+      'diagram'
+    );
+  });
+
+  test.afterEach(async ({ request }) => {
+    await new PlaywrightProject(request).deleteProject(projectId);
+  });
+
+  test('when a list node is resized and the header height changed, then child are moved to the new position', async ({
+    page,
+  }) => {
+    const parentNode = new PlaywrightNode(page, 'Test with a very large name that can be wrap', 'List');
+    const firstChildNode = new PlaywrightNode(page, 'List 1', 'List');
+    await parentNode.click();
+    const firstChildXYPositionBefore = await firstChildNode.getDOMXYPosition();
+
+    const resizeAnchor = page.locator(`.react-flow__resize-control.top.right`);
+
+    const box = (await resizeAnchor.boundingBox())!;
+    await resizeAnchor.hover();
+    await page.mouse.down();
+    await page.mouse.move(box.x + 100, box.y, { steps: 8 });
+
+    await page.waitForFunction(
+      ({ previousY }) => {
+        const child = document.querySelector(`[data-testid="List - List 1"]`);
+        if (!child) {
+          return false;
+        }
+        const childBoundingBox = child.getBoundingClientRect();
+        return childBoundingBox.y < previousY - 10;
+      },
+      {
+        previousY: firstChildXYPositionBefore.y,
+      },
+      { timeout: 2000 }
+    );
   });
 });
