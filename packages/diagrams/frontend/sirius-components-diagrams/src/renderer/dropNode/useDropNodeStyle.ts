10b601b257751d3643fdcc012c6ecc677f2168f0
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-01-08 16:17:48 +0100

    [6027] Improve visual feedback for diagram DnD
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6027
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/dropNode/useDropNodeStyle.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/dropNode/useDropNodeStyle.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 Obeo.
+ * Copyright (c) 2023, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -17,23 +17,39 @@ import { DiagramContext } from '../../contexts/DiagramContext';
 import { DiagramContextValue } from '../../contexts/DiagramContext.types';
 import { useDropNodeStyleValue } from './useDropNodeStyle.types';
 
+/**
+ * Computes the CSS style customization to apply to a given node during a DnD operation.
+ *
+ * @param isDropNodeTarget whether the node to style is currently targeted by the drop
+ *                         (i.e. the mouse is on top of it, and releasing the mousing
+ *                         button would trigger the drop on this node)
+ * @param isDropNodeCandidate whether the node to style is a compatible drop candidate
+ *                            for the dragged element(s)
+ * @param isDragging whether the node is (one of) the node(s) being moved/dragged.
+ * @returns CSS properties to apply to the node to give feedback to the user.
+ */
 export const useDropNodeStyle = (
   isDropNodeTarget: boolean,
-  isDropNodeCandidate,
+  isDropNodeCandidate: boolean,
   isDragging: boolean
 ): useDropNodeStyleValue => {
   const { readOnly } = useContext<DiagramContextValue>(DiagramContext);
-
   const theme = useTheme();
-  const style: React.CSSProperties = {};
+  const dropPossible = `rgb(from ${theme.palette.success.main} r g b / 0.5)`;
+  const dropValid = theme.palette.success.main;
+  const dropInvalid = `rgb(from ${theme.palette.error.main} r g b / 0.5)`;
 
-  if (!readOnly) {
-    if (!isDragging && isDropNodeCandidate) {
-      style.boxShadow = `0px 0px 2px 2px ${theme.palette.success.main}`;
-      style.transition = `box-shadow 0.3s ease-in-out`;
+  const style: React.CSSProperties = {};
+  if (!readOnly && !isDragging) {
+    let highlightColor: string | null = null;
+    if (isDropNodeCandidate) {
+      highlightColor = isDropNodeTarget ? dropValid : dropPossible;
+    } else if (isDropNodeTarget) {
+      highlightColor = dropInvalid;
     }
-    if (isDropNodeTarget) {
-      style.boxShadow = `0px 0px 2px 2px ${theme.palette.primary.main}`;
+    if (highlightColor) {
+      style.boxShadow = `0px 0px 2px 2px ${highlightColor}`;
+      style.transition = `box-shadow 0.3s ease-in-out`;
     }
   }
 
