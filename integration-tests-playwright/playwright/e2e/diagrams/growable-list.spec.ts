d9447ec5b936e076f57d54c917079fe667e5348c
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2026-01-12 18:56:27 +0100

    [6045] Prevent misplacement of a subnode after hiding another list subnode
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6045
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- integration-tests-playwright/playwright/e2e/diagrams/growable-list.spec.ts
+++ integration-tests-playwright/playwright/e2e/diagrams/growable-list.spec.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -29,11 +29,7 @@ test.describe('diagram - growable list', () => {
     const playwrightExplorer = new PlaywrightExplorer(page);
     await playwrightExplorer.uploadDocument('diagramGrowableList.xml');
     await playwrightExplorer.expand('diagramGrowableList.xml');
-    await playwrightExplorer.createRepresentation(
-      'Root',
-      'diagramGrowableList - multiple growable list nodes',
-      'diagram'
-    );
+    await playwrightExplorer.createRepresentation('Root', 'diagramList - multiple growable list nodes', 'diagram');
   });
 
   test.afterEach(async ({ request }) => {
@@ -75,5 +71,58 @@ test.describe('diagram - growable list', () => {
     const growableListNodeSizeAfterHide = await growableListNode.getReactFlowSize();
     expect(growableListNodeSizeAfterHide.height).toBe(400);
     expect(growableListNodeSizeAfterHide.width).toBe(152);
+  });
+});
+test.describe('diagram - growable list', () => {
+  let projectId;
+  test.beforeEach(async ({ page, request }) => {
+    await page.addInitScript(() => {
+      // @ts-expect-error: we use a variable in the DOM to disable `fitView` functionality for Cypress tests.
+      window.document.DEACTIVATE_FIT_VIEW_FOR_CYPRESS_TESTS = true;
+    });
+    const project = await new PlaywrightProject(request).createProject('diagram-subnode-list', 'blank-project');
+    projectId = project.projectId;
+
+    await page.goto(`/projects/${projectId}/edit`);
+    const playwrightExplorer = new PlaywrightExplorer(page);
+    await playwrightExplorer.uploadDocument('diagramSubNodeList.xml');
+    await playwrightExplorer.expand('diagramSubNodeList.xml');
+    await playwrightExplorer.createRepresentation('Root', 'diagramList - list with subnode', 'diagram');
+  });
+
+  test.afterEach(async ({ request }) => {
+    await new PlaywrightProject(request).deleteProject(projectId);
+  });
+
+  test('when a sub node is hide, then next sub nodes are correctly placed', async ({ page }) => {
+    const listNode = new PlaywrightNode(page, 'ListWithSubNodes', 'List');
+
+    await listNode.click();
+
+    // Hide Node Panel Info to avoid overlap in diagram
+    const panel = await page.locator('.react-flow__panel.bottom.left');
+    await panel.evaluate((node) => {
+      node.style.visibility = 'hidden';
+    });
+
+    //issue tested only appear after resize
+    await listNode.resize({ height: 20, width: 50 });
+
+    const comp3Node = new PlaywrightNode(page, 'ListWithSubNodes', 'FreeForm', 2);
+
+    await comp3Node.openPalette();
+    await page.getByTestId('tool-Hide').click();
+    await expect(page.locator(`[data-testid="FreeForm - ListWithSubNodes"]`)).toHaveCount(3);
+
+    const comp1Node = new PlaywrightNode(page, 'ListWithSubNodes', 'FreeForm', 0);
+    const comp2Node = new PlaywrightNode(page, 'ListWithSubNodes', 'FreeForm', 1);
+    const comp4Node = new PlaywrightNode(page, 'ListWithSubNodes', 'FreeForm', 2);
+
+    const { height: list1NodeHeight } = await listNode.getReactFlowSize('ListWithSubNodes');
+    const { height: comp1NodeHeight } = await comp1Node.getReactFlowSize('Compartment 1');
+    const { height: comp2NodeHeight } = await comp2Node.getReactFlowSize('Compartment 2');
+    const { height: comp4NodeHeight } = await comp4Node.getReactFlowSize('Compartment 4');
+
+    expect(list1NodeHeight).toBeGreaterThanOrEqual(comp1NodeHeight + comp2NodeHeight + comp4NodeHeight);
   });
 });
