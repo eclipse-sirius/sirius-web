f57aa95eed169d960db87db40b7b669d17c8f078
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2026-01-20 16:06:42 +0100

    [6101] Allow more control of the redirect URL in the new project card
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6101
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/views/project-browser/create-projects-area/ProjectTemplatesModal.tsx
+++ packages/sirius-web/frontend/sirius-web-application/src/views/project-browser/create-projects-area/ProjectTemplatesModal.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 Obeo.
+ * Copyright (c) 2023, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -98,7 +98,9 @@ export const ProjectTemplatesModal = ({ onClose }: ProjectTemplatesModalProps) =
   const cards: JSX.Element[] = [];
   if (state.projectTemplates) {
     state.projectTemplates
-      .map((template) => <NewProjectCard key={template.id} template={template} />)
+      .map((template) => (
+        <NewProjectCard key={template.id} url={`/new/project?templateId=${template.id}`} template={template} />
+      ))
       .forEach((card: JSX.Element) => cards.push(card));
   }
 
