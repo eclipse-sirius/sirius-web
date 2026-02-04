469b348df18d64136a7b4809d5b0acc4d6795f4b
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2026-02-04 10:13:25 +0100

    [6146] Add an option to search in libraries in the search view
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6146
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

--- packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/application/controllers/search/SearchIntegrationTests.java
+++ packages/sirius-web/backend/sirius-web/src/test/java/org/eclipse/sirius/web/application/controllers/search/SearchIntegrationTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -59,9 +59,8 @@ public class SearchIntegrationTests extends AbstractIntegrationTests {
     @GivenSiriusWebServer
     @DisplayName("Given a project, when we execute a basic search, then all the matching semantic elements are returned")
     public void givenProjectWhenWeExecuteBasicSearchThenAllMatchingElementsAreReturned() {
-        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "sirius-web", false, false, false, false);
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "sirius-web", false, false, false, false, false);
         assertThat(matches).containsExactlyInAnyOrder(
-                "sirius-web-tests-data",
                 "sirius-web-domain",
                 "sirius-web-application",
                 "sirius-web-infrastructure",
@@ -75,10 +74,10 @@ public class SearchIntegrationTests extends AbstractIntegrationTests {
     @DisplayName("Given a project, when we execute a whole word search, then only the matching semantic elements are returned")
     public void givenProjectWhenWeExecuteWholeWordSearchThenOnlyMatchingElementsAreReturned() {
         // "Command" appear multiple times as part of "Command1" and "Command2"
-        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "Command", false, /* mathcWholeWord */ false, false, false);
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "Command", false, /* mathcWholeWord */ false, false, false, false);
         assertThat(matches).containsExactly("Command1 over HTTP", "Command2 over null", "Command1", "Command2");
         // but never as a distinct whole word.
-        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "Command", false, /* mathcWholeWord */ true, false, false);
+        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "Command", false, /* mathcWholeWord */ true, false, false, false);
         assertThat(matches).isEmpty();
     }
 
@@ -86,10 +85,10 @@ public class SearchIntegrationTests extends AbstractIntegrationTests {
     @GivenSiriusWebServer
     @DisplayName("Given a project, when we execute a case insensitive search, then all the matching semantic elements are returned")
     public void givenProjectWhenWeExecuteCaseInsensitiveSearchThenAllMatchingElementsAreReturned() {
-        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "project creation", /* matchCase */ false, false, false, false);
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "project creation", /* matchCase */ false, false, false, false, false);
         assertThat(matches).containsExactlyInAnyOrder("Project Creation");
 
-        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "project creation", /* matchCase */ true, false, false, false);
+        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "project creation", /* matchCase */ true, false, false, false, false);
         assertThat(matches).isEmpty();
     }
 
@@ -98,10 +97,10 @@ public class SearchIntegrationTests extends AbstractIntegrationTests {
     @DisplayName("Given a project, when we execute a search including attributes, then all the matching semantic elements are returned")
     public void givenProjectWhenWeExecuteSearchIncludingAttributesThenAllMatchingElementsAreReturned() {
         // There are no matches in object names/labels
-        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "2024-02", false, false, false, /* searchInAttributes */ false);
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "2024-02", false, false, false, /* searchInAttributes */ false, false);
         assertThat(matches).isEmpty();
         // but there are in several object attributes
-        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "2024-02", false, false, false, /* searchInAttributes */ true);
+        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "2024-02", false, false, false, /* searchInAttributes */ true, false);
         // Note: the matches are in these object's attributes, but we return only the objects themselves
         assertThat(matches).containsExactlyInAnyOrder("2024.3.0", "2024.5.0");
     }
@@ -110,24 +109,47 @@ public class SearchIntegrationTests extends AbstractIntegrationTests {
     @GivenSiriusWebServer
     @DisplayName("Given a project, when we execute a search with regular expression, then all the matching semantic elements are returned")
     public void givenProjectWhenWeExecuteSearchWithRegexpThenAllMatchingElementsAreReturned() {
-        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "^sirius-web-([dt]|ap{2}).*[an]$", false, false, /* useRegularExpressions */ false, false);
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "^sirius-web-([dt]|ap{2}).*[an]$", false, false, /* useRegularExpressions */ false, false, false);
         assertThat(matches).isEmpty();
-        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "^sirius-web-([dt]|ap{2}).*[an]$", false, false, /* useRegularExpressions */ true, false);
+        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "^sirius-web-([dt]|ap{2}).*[an]$", false, false, /* useRegularExpressions */ true, false, false);
         assertThat(matches).containsExactlyInAnyOrder(
-                "sirius-web-tests-data",
                 "sirius-web-domain",
                 "sirius-web-application"
         );
     }
 
-    private List<String> search(UUID editingContextId, String text, boolean matchCase, boolean matchWholeWord, boolean useRegularExpressions, boolean searchInAttributes) {
+    @Test
+    @GivenSiriusWebServer
+    @DisplayName("Given a project, when we execute a search including libraries, then all the matching semantic elements are returned")
+    public void givenProjectWhenWeExecuteSearchIncludingLibrariesThenAllMatchingElementsAreReturned() {
+        List<String> matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "sirius-web", false, false, false, false, /* searchInLibraries */ false);
+        assertThat(matches).containsExactlyInAnyOrder(
+                "sirius-web-domain",
+                "sirius-web-application",
+                "sirius-web-infrastructure",
+                "sirius-web-starter",
+                "sirius-web"
+        );
+        matches = this.search(PapayaIdentifiers.PAPAYA_EDITING_CONTEXT_ID, "sirius-web", false, false, false, false, /* searchInLibraries */ true);
+        assertThat(matches).containsExactlyInAnyOrder(
+                "sirius-web-tests-data",
+                "sirius-web-domain",
+                "sirius-web-application",
+                "sirius-web-infrastructure",
+                "sirius-web-starter",
+                "sirius-web"
+        );
+    }
+
+    private List<String> search(UUID editingContextId, String text, boolean matchCase, boolean matchWholeWord, boolean useRegularExpressions, boolean searchInAttributes, boolean searchInLibraries) {
         // The SearchQuery object must be passed as a plain Map here
         var queryMap = Map.of(
                 "text", text,
                 "matchCase", matchCase,
                 "matchWholeWord", matchWholeWord,
                 "useRegularExpression", useRegularExpressions,
-                "searchInAttributes", searchInAttributes
+                "searchInAttributes", searchInAttributes,
+                "searchInLibraries", searchInLibraries
         );
         Map<String, Object> variables = Map.of(
                 "editingContextId", editingContextId.toString(),
