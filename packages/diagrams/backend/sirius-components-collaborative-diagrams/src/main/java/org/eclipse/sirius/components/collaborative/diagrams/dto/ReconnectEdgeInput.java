9133cb539588d5da356cc4277771fb32d1cc231f
Florian ROUËNÉ <florian.rouene@obeosoft.com>
2026-02-17 14:33:34 +0100

    [6110] Preserve custom handle positions during edge reconnection
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6110
    Signed-off-by: Florian ROUËNÉ <florian.rouene@obeosoft.com>

--- packages/diagrams/backend/sirius-components-collaborative-diagrams/src/main/java/org/eclipse/sirius/components/collaborative/diagrams/dto/ReconnectEdgeInput.java
+++ packages/diagrams/backend/sirius-components-collaborative-diagrams/src/main/java/org/eclipse/sirius/components/collaborative/diagrams/dto/ReconnectEdgeInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2024 Obeo.
+ * Copyright (c) 2022, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -28,5 +28,8 @@ public record ReconnectEdgeInput(
         String representationId,
         String edgeId,
         String newEdgeEndId,
-        ReconnectEdgeKind reconnectEdgeKind) implements IDiagramInput {
+        ReconnectEdgeKind reconnectEdgeKind,
+        double targetPositionX,
+        double targetPositionY) implements IDiagramInput {
+
 }
