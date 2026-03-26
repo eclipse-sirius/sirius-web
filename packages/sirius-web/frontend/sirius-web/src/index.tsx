a2e2583f42fb8ed0970c72c69a9e101074533f47
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-03-26 09:32:30 +0100

    [releng] Use the latest version of Roboto
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/frontend/sirius-web/src/index.tsx
+++ packages/sirius-web/frontend/sirius-web/src/index.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2025 Obeo.
+ * Copyright (c) 2019, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -27,7 +27,10 @@ import { EllipseNode } from './nodes/EllipseNode';
 import { EllipseNodeConverter } from './nodes/EllipseNodeConverter';
 import { EllipseNodeLayoutHandler } from './nodes/EllipseNodeLayoutHandler';
 
-import './fonts.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './portals.css';
 import './ReactFlow.css';
 import './reset.css';
