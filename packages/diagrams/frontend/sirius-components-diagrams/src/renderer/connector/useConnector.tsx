ff38c510787e41ce1e7d5bd0aad55d68a100bc58
Michael Charfadi <michael.charfadi@obeosoft.com>
2026-03-26 10:12:19 +0100

    [6338] Move the execution of connector tools in a dedicated hook
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6338
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/connector/useConnector.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/connector/useConnector.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 Obeo.
+ * Copyright (c) 2023, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -113,8 +113,6 @@ export const useConnector = (): UseConnectorValue => {
     []
   );
 
-  const onConnectorContextualMenuClose = () => resetConnection();
-
   const onConnectionStartElementClick = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (event.button === 0) {
       setIsNewConnection(true);
@@ -198,7 +196,7 @@ export const useConnector = (): UseConnectorValue => {
     onConnect,
     onConnectStart,
     onConnectEnd,
-    onConnectorContextualMenuClose,
+    onConnectorContextualMenuClose: resetConnection,
     onConnectionStartElementClick,
     addTempConnectionLine,
     removeTempConnectionLine,
