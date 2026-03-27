8f6b30c95e833d42bf5645ec986f83081f301661
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2026-03-27 13:36:04 +0100

    [6346] Ensure forms are read only when opened on a library
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6346
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/capability/services/FormCapabilitiesService.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/capability/services/FormCapabilitiesService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -40,7 +40,7 @@ public class FormCapabilitiesService implements IFormCapabilitiesService {
 
     @Override
     public FormCapabilitiesDTO getFormCapabilities(String editingContextId, String formId) {
-        boolean canEdit = true;
+        boolean canEdit = false;
         var optionalProjectId = this.projectEditingContextService.getProjectId(editingContextId);
         if (optionalProjectId.isPresent()) {
             canEdit = this.capabilityEvaluator.hasCapability(SiriusWebCapabilities.PROJECT, optionalProjectId.get(), SiriusWebCapabilities.Project.EDIT);
