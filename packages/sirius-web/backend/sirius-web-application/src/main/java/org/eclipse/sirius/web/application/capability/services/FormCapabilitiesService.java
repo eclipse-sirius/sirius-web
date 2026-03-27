3b46ffe4df89ee6d76b7c70778e2344ab2e0d904
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2026-03-27 13:36:04 +0100

    [6346] Ensure forms are read only when opened on a library
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6346
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

--- packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/capability/services/FormCapabilitiesService.java
+++ packages/sirius-web/backend/sirius-web-application/src/main/java/org/eclipse/sirius/web/application/capability/services/FormCapabilitiesService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -13,12 +13,19 @@
 package org.eclipse.sirius.web.application.capability.services;
 
 import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.eclipse.sirius.components.collaborative.forms.dto.FormCapabilitiesDTO;
 import org.eclipse.sirius.components.collaborative.forms.services.api.IFormCapabilitiesService;
+import org.eclipse.sirius.web.application.UUIDParser;
 import org.eclipse.sirius.web.application.capability.SiriusWebCapabilities;
 import org.eclipse.sirius.web.application.capability.services.api.ICapabilityEvaluator;
 import org.eclipse.sirius.web.application.project.services.api.IProjectEditingContextService;
+import org.eclipse.sirius.web.domain.boundedcontexts.library.Library;
+import org.eclipse.sirius.web.domain.boundedcontexts.library.services.api.ILibrarySearchService;
+import org.eclipse.sirius.web.domain.boundedcontexts.semanticdata.SemanticData;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
 /**
@@ -31,10 +38,13 @@ public class FormCapabilitiesService implements IFormCapabilitiesService {
 
     private final IProjectEditingContextService projectEditingContextService;
 
+    private final ILibrarySearchService librarySearchService;
+
     private final ICapabilityEvaluator capabilityEvaluator;
 
-    public FormCapabilitiesService(IProjectEditingContextService projectEditingContextService, ICapabilityEvaluator capabilityEvaluator) {
+    public FormCapabilitiesService(IProjectEditingContextService projectEditingContextService, ILibrarySearchService librarySearchService, ICapabilityEvaluator capabilityEvaluator) {
         this.projectEditingContextService = Objects.requireNonNull(projectEditingContextService);
+        this.librarySearchService = Objects.requireNonNull(librarySearchService);
         this.capabilityEvaluator = Objects.requireNonNull(capabilityEvaluator);
     }
 
@@ -44,6 +54,14 @@ public class FormCapabilitiesService implements IFormCapabilitiesService {
         var optionalProjectId = this.projectEditingContextService.getProjectId(editingContextId);
         if (optionalProjectId.isPresent()) {
             canEdit = this.capabilityEvaluator.hasCapability(SiriusWebCapabilities.PROJECT, optionalProjectId.get(), SiriusWebCapabilities.Project.EDIT);
+        } else {
+            // Forms in libraries should never be editable (since libraries are read-only).
+            Optional<Library> optionalLibrary = new UUIDParser().parse(editingContextId)
+                    .map(AggregateReference::<SemanticData, UUID>to)
+                    .flatMap(this.librarySearchService::findBySemanticData);
+            if (optionalLibrary.isPresent()) {
+                canEdit = false;
+            }
         }
         return new FormCapabilitiesDTO(canEdit);
     }
