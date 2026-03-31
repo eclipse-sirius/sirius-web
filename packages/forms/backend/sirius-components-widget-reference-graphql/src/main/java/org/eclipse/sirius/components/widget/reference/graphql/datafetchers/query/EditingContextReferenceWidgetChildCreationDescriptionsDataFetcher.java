926fb53bb9ce52a8e3c4800cfcab1d387545d80d
Théo Giraudet <theo.giraudet@obeo.fr>
2026-03-31 15:59:32 +0200

    [6363] Add new architecture tests for data fetchers
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6363
    Signed-off-by: Théo Giraudet <theo.giraudet@obeo.fr>

---
+++ packages/forms/backend/sirius-components-widget-reference-graphql/src/main/java/org/eclipse/sirius/components/widget/reference/graphql/datafetchers/query/EditingContextReferenceWidgetChildCreationDescriptionsDataFetcher.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.components.widget.reference.graphql.datafetchers.query;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.sirius.components.annotations.spring.graphql.QueryDataFetcher;
+import org.eclipse.sirius.components.collaborative.dto.EditingContextChildObjectCreationDescriptionsPayload;
+import org.eclipse.sirius.components.collaborative.widget.reference.dto.ReferenceWidgetChildCreationDescriptionsInput;
+import org.eclipse.sirius.components.core.api.ChildCreationDescription;
+import org.eclipse.sirius.components.graphql.api.IDataFetcherWithFieldCoordinates;
+import org.eclipse.sirius.components.graphql.api.IEditingContextDispatcher;
+
+import graphql.schema.DataFetchingEnvironment;
+
+/**
+ * The data fetcher used to retrieve referenceWidgetChildCreationDescriptions.
+ *
+ * @author frouene
+ */
+@QueryDataFetcher(type = "EditingContext", field = "referenceWidgetChildCreationDescriptions")
+public class EditingContextReferenceWidgetChildCreationDescriptionsDataFetcher implements IDataFetcherWithFieldCoordinates<CompletableFuture<List<ChildCreationDescription>>> {
+
+    private static final String CONTAINER_ID_ARGUMENT = "containerId";
+    private static final String REFERENCE_KIND_ARGUMENT = "referenceKind";
+    private static final String WIDGET_DESCRIPTION_ID_ARGUMENT = "descriptionId";
+
+    private final IEditingContextDispatcher editingContextDispatcher;
+
+    public EditingContextReferenceWidgetChildCreationDescriptionsDataFetcher(IEditingContextDispatcher editingContextDispatcher) {
+        this.editingContextDispatcher = Objects.requireNonNull(editingContextDispatcher);
+    }
+
+    @Override
+    public CompletableFuture<List<ChildCreationDescription>> get(DataFetchingEnvironment environment) throws Exception {
+        String editingContextId = environment.getSource();
+        String containerId = environment.getArgument(CONTAINER_ID_ARGUMENT);
+        String referenceKind = environment.getArgument(REFERENCE_KIND_ARGUMENT);
+        String descriptionId = environment.getArgument(WIDGET_DESCRIPTION_ID_ARGUMENT);
+
+        ReferenceWidgetChildCreationDescriptionsInput input = new ReferenceWidgetChildCreationDescriptionsInput(UUID.randomUUID(), editingContextId, containerId,
+                referenceKind, descriptionId);
+
+        return this.editingContextDispatcher.dispatchQuery(input.editingContextId(), input)
+                .filter(EditingContextChildObjectCreationDescriptionsPayload.class::isInstance)
+                .map(EditingContextChildObjectCreationDescriptionsPayload.class::cast)
+                .map(EditingContextChildObjectCreationDescriptionsPayload::childCreationDescriptions)
+                .toFuture();
+    }
+
+}
---
+++ packages/tests/backend/sirius-components-graphql-tests/src/main/java/org/eclipse/sirius/components/graphql/tests/AbstractGraphQLCodingRulesTests.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.sirius.components.graphql.tests;
+
+import com.tngtech.archunit.core.domain.JavaAnnotation;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import java.text.MessageFormat;
+import java.util.Optional;
+import org.eclipse.sirius.components.annotations.spring.graphql.MutationDataFetcher;
+import org.eclipse.sirius.components.annotations.spring.graphql.QueryDataFetcher;
+import org.eclipse.sirius.components.annotations.spring.graphql.SubscriptionDataFetcher;
+import org.eclipse.sirius.components.graphql.api.IDataFetcherWithFieldCoordinates;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+
+/**
+ * Superclass of all GraphQL coding rules test cases.
+ *
+ * @author tgiraudet
+ */
+public abstract class AbstractGraphQLCodingRulesTests {
+
+    protected abstract String getProjectRootPackage();
+
+    protected abstract JavaClasses getClasses();
+
+    @Test
+    public void dataFetcherWithFieldCoordinatesShouldHaveDataFetcherAnnotation() {
+        ArchRule rule = ArchRuleDefinition.classes()
+            .that()
+            .resideInAPackage(this.getProjectRootPackage())
+            .and()
+            .areAssignableTo(IDataFetcherWithFieldCoordinates.class)
+            .should(this.haveDataFetcherAnnotation());
+
+        rule.check(this.getClasses());
+    }
+
+    @Test
+    public void dataFetcherShouldRespectNamingConventions() {
+        ArchRule rule = ArchRuleDefinition.classes()
+            .that()
+            .resideInAPackage(this.getProjectRootPackage())
+            .and()
+            .areAssignableTo(IDataFetcherWithFieldCoordinates.class)
+            .should(this.nameMatchesTypeFieldDataFetcher());
+
+        rule.check(this.getClasses());
+    }
+
+    private ArchCondition<JavaClass> haveDataFetcherAnnotation() {
+        return new ArchCondition<>("have a data fetcher annotation") {
+            @Override
+            public void check(JavaClass javaClass, ConditionEvents events) {
+                var optionalDataFetcherAnnotation = AbstractGraphQLCodingRulesTests.this.getDataFetcherAnnotation(javaClass);
+                var pattern = "The data fetcher class {0} should have a data fetcher annotation among: QueryDataFetcher, MutationDataFetcher, SubscriptionDataFetcher";
+                String message = MessageFormat.format(pattern, javaClass.getSimpleName());
+                events.add(new SimpleConditionEvent(javaClass, optionalDataFetcherAnnotation.isPresent(), message));
+            }
+        };
+    }
+
+    private ArchCondition<JavaClass> nameMatchesTypeFieldDataFetcher() {
+        return new ArchCondition<>("name matches TypeFieldDataFetcher") {
+            @Override
+            public void check(JavaClass javaClass, ConditionEvents events) {
+                var optionalDataFetcherAnnotation = AbstractGraphQLCodingRulesTests.this.getDataFetcherAnnotation(javaClass);
+                boolean hasCorrectName = true;
+                var expectedName = javaClass.getSimpleName();
+
+                if (optionalDataFetcherAnnotation.isPresent()) {
+                    var dataFetcherAnnotation = optionalDataFetcherAnnotation.get();
+                    var type = dataFetcherAnnotation.get("type").orElse("").toString();
+                    var field = dataFetcherAnnotation.get("field").orElse("").toString();
+
+                    expectedName = MessageFormat.format("{0}{1}DataFetcher", type, StringUtils.capitalize(field));
+                    hasCorrectName = javaClass.getSimpleName().equals(expectedName);
+                }
+                var pattern = "The data fetcher class {0} should have a name matching ''<Type><Field>DataFetcher'': expecting ''{1}''";
+                String message = MessageFormat.format(pattern, javaClass.getSimpleName(), expectedName);
+                events.add(new SimpleConditionEvent(javaClass, hasCorrectName, message));
+            }
+        };
+    }
+
+    private Optional<JavaAnnotation<JavaClass>> getDataFetcherAnnotation(JavaClass javaClass) {
+        return javaClass.tryGetAnnotationOfType(QueryDataFetcher.class.getTypeName())
+            .or(() -> javaClass.tryGetAnnotationOfType(MutationDataFetcher.class.getTypeName()))
+            .or(() -> javaClass.tryGetAnnotationOfType(SubscriptionDataFetcher.class.getTypeName()));
+    }
+
+}
