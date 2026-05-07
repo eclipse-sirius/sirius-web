8ae6220d9c57184d728063bcd54ef81fd7aa1ef0
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-05-07 17:37:04 +0200

    [6492] Only execute the Explorer's Duplicate object action when invoked from the Explorer
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6492
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/explorer/ExplorerView.tsx
+++ packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/explorer/ExplorerView.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2025 Obeo.
+ * Copyright (c) 2019, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -355,6 +355,7 @@ export const ExplorerView = forwardRef<WorkbenchViewHandle, WorkbenchViewCompone
           {treeDescriptionSelector}
         </TreeToolBar>
         <DuplicateObjectKeyboardShortcut
+          target={treeElement?.current}
           editingContextId={editingContextId}
           readOnly={readOnly}
           selectedTreeItem={state.singleTreeItemSelected}
