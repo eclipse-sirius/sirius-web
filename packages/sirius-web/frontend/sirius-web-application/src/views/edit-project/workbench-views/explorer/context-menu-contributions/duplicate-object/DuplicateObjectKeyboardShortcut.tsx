90f07f28726dbe187dcc057ae11548e9a20ecf2c
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-05-07 17:37:04 +0200

    [6492] Only execute the Explorer's Duplicate object action when invoked from the Explorer
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6492
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/explorer/context-menu-contributions/duplicate-object/DuplicateObjectKeyboardShortcut.tsx
+++ packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/workbench-views/explorer/context-menu-contributions/duplicate-object/DuplicateObjectKeyboardShortcut.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -21,6 +21,7 @@ export const DuplicateObjectKeyboardShortcut = ({
   selectedTreeItem,
   selectTreeItems,
   children,
+  target,
 }: KeyboardShortcutProps) => {
   const [open, setOpen] = useState<boolean>(false);
   const { setSelection } = useSelection();
@@ -39,11 +40,13 @@ export const DuplicateObjectKeyboardShortcut = ({
   };
 
   useEffect(() => {
-    if (!readOnly) {
-      window.addEventListener('keydown', duplicateObjectKeyPressHandler);
+    if (!readOnly && target) {
+      target.addEventListener('keydown', duplicateObjectKeyPressHandler);
     }
-    return () => window.removeEventListener('keydown', duplicateObjectKeyPressHandler);
-  }, [selectedTreeItem, readOnly]);
+    return () => {
+      target?.removeEventListener('keydown', duplicateObjectKeyPressHandler);
+    };
+  }, [selectedTreeItem, readOnly, target]);
 
   const onObjectCreated = (selection: Selection) => {
     setSelection(selection);
