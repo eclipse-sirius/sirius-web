9274e16d91678ae8a4690dfcee5f7bcaa4a07d30
OrlannC <orlann.cailleau@obeosoft.com>
2026-05-19 10:38:51 +0200

    [6241] Add storybook for sirius components testing - Mandatory files for storybook
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6241
    Signed-off-by: Orlann Cailleau <orlann.cailleau@obeosoft.com>

---
+++ packages/storybook/frontend/storybook/.storybook/main.js
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Obeo - initial API and implementation
+ *******************************************************************************/
+import { createRequire } from "module";
+import { dirname, join, resolve } from "path";
+import { fileURLToPath } from "url";
+import { mergeConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(import.meta.url);
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-vitest"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-docs"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  typescript: {
+    reactDocgen: "react-docgen-native",
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      server: {
+        fs: {
+          allow: [resolve(__dirname, "../../../")],
+        },
+      },
+      optimizeDeps: {
+        include: ["@eclipse-sirius/sirius-web-application"],
+      },
+      define: {
+        "process.env": {},
+      },
+    });
+  },
+};
+
+export default config;
