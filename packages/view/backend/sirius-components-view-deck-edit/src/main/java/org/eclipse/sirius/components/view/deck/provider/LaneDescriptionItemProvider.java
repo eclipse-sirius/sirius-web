741ccb2a6d17f8fa3f36756bb785aea1bba81496
Laurent Fasani <laurent.fasani@obeo.fr>
2026-07-22 10:09:17 +0200

    [6726] Extracted the manual code from the generated for the gantt and deck metamodels
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6726
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- packages/view/backend/sirius-components-view-deck-edit/src/main/java/org/eclipse/sirius/components/view/deck/provider/LaneDescriptionItemProvider.java
+++ packages/view/backend/sirius-components-view-deck-edit/src/main/java/org/eclipse/sirius/components/view/deck/provider/LaneDescriptionItemProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Obeo.
+ * Copyright (c) 2023, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -130,7 +130,7 @@ public class LaneDescriptionItemProvider extends DeckElementDescriptionItemProvi
     /**
      * This returns LaneDescription.svg. <!-- begin-user-doc --> <!-- end-user-doc -->
      *
-     * @generated NOT
+     * @generated
      */
     @Override
     public Object getImage(Object object) {
@@ -189,7 +189,7 @@ public class LaneDescriptionItemProvider extends DeckElementDescriptionItemProvi
      * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created
      * under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
      *
-     * @generated NOT
+     * @generated
      */
     @Override
     protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
@@ -208,7 +208,7 @@ public class LaneDescriptionItemProvider extends DeckElementDescriptionItemProvi
      * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}. <!-- begin-user-doc -->
      * <!-- end-user-doc -->
      *
-     * @generated NOT
+     * @generated
      */
     @Override
     public String getCreateChildText(Object owner, Object feature, Object child, Collection<?> selection) {
