d4bfae95bc76d592bcc955a997b8681501f77818
Michael Charfadi <michael.charfadi@obeosoft.com>
2026-08-14 08:23:02 +0200

    [releng] Add sirius-components-palette maven project
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

---
+++ packages/palette/backend/sirius-components-palette/src/test/java/org/eclipse/sirius/components/collaborative/palette/architecture/SpringCodingRulesTests.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.components.collaborative.palette.architecture;
+
+import org.eclipse.sirius.components.spring.tests.architecture.AbstractSpringCodingRulesTests;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+
+/**
+ * Spring coding rules tests.
+ *
+ * @author mcharfadi
+ */
+public class SpringCodingRulesTests extends AbstractSpringCodingRulesTests {
+
+    @Override
+    protected String getProjectRootPackage() {
+        return ArchitectureConstants.SIRIUS_COMPONENTS_PALETTE_ROOT_PACKAGE;
+    }
+
+    @Override
+    protected JavaClasses getClasses() {
+        return ArchitectureConstants.CLASSES;
+    }
+
+}
