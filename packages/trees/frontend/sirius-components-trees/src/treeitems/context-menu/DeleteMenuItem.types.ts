03ef2c5da92210f07c3fb3e666ab85dfb08112cf
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2026-08-20 17:07:35 +0200

    [6194] Stop using ErrorPayload#message in tree mutations
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6194
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- packages/trees/frontend/sirius-components-trees/src/treeitems/context-menu/DeleteMenuItem.types.ts
+++ packages/trees/frontend/sirius-components-trees/src/treeitems/context-menu/DeleteMenuItem.types.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,7 @@
  *     Obeo - initial API and implementation
  *******************************************************************************/
 
+import { GQLMessage } from '@eclipse-sirius/sirius-components-core';
 import { GQLTreeItem } from '../../views/TreeView.types';
 
 export interface DeleteMenuItemProps {
@@ -45,5 +46,5 @@ export interface GQLSuccessPayload extends GQLDeleteTreeItemPayload {
 }
 
 export interface GQLErrorPayload extends GQLDeleteTreeItemPayload {
-  message: string;
+  messages: GQLMessage[];
 }
