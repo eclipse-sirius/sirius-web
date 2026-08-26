0161029bc5a38d010a5ed404086295d61ff2b4be
Théo Giraudet <theo.giraudet@obeo.fr>
2026-08-26 14:44:21 +0200

    [cleanup] Restrict default diagram tools to studio DSLs
    Signed-off-by: Théo Giraudet <theo.giraudet@obeo.fr>

--- packages/starters/backend/sirius-components-flow-starter/src/main/java/org/eclipse/sirius/components/flow/starter/view/descriptions/PowerOutputDescriptionProvider.java
+++ packages/starters/backend/sirius-components-flow-starter/src/main/java/org/eclipse/sirius/components/flow/starter/view/descriptions/PowerOutputDescriptionProvider.java
@@ -28,7 +28,6 @@ import org.eclipse.sirius.components.view.diagram.NodeDescription;
 import org.eclipse.sirius.components.view.diagram.NodePalette;
 import org.eclipse.sirius.components.view.diagram.SynchronizationPolicy;
 import org.eclipse.sirius.components.view.diagram.UserResizableDirection;
-import org.eclipse.sirius.components.view.diagram.provider.DefaultToolsFactory;
 
 /**
  * Used to create the powerOutput node description.
@@ -79,7 +78,7 @@ public class PowerOutputDescriptionProvider implements INodeDescriptionProvider 
         return this.diagramBuilderHelper.newNodePalette()
                 .edgeTools(this.createEdgeToolPowerLink(cache))
                 .toolSections(
-                        new DefaultToolsFactory().createDefaultHideRevealNodeToolSection()
+                        this.flowViewBuilder.createHideRevealNodeToolSection()
                 )
                 .deleteTool(this.flowViewBuilder.createDeleteTool())
                 .build();
