ea1ab69750ae34776d73a9bbd4a826ba4c03eada
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2026-09-03 10:15:47 +0200

    [6194] Remove the last usages of ErrorPayload#message in the frontend
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6194
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- integration-tests-cypress/cypress/support/serverCommands.js
+++ integration-tests-cypress/cypress/support/serverCommands.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2025 Obeo.
+ * Copyright (c) 2021, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -72,7 +72,10 @@ Cypress.Commands.add('deleteNodeFromDiagram', (editingContextId, representationI
         }
       }
       ... on ErrorPayload {
-        message
+        messages {
+          body
+          level
+        }
       }
     }
    }
