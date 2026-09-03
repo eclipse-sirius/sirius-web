872d42f41954b7618875ad1291234bda99d3f0f0
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2026-09-03 10:15:47 +0200

    [6194] Remove the last usages of ErrorPayload#message in the frontend
    Bug: https://github.com/eclipse-sirius/sirius-web/issues/6194
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/UndoRedo.types.ts
+++ packages/sirius-web/frontend/sirius-web-application/src/views/edit-project/UndoRedo.types.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,8 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
+import { GQLMessage } from '@eclipse-sirius/sirius-components-core';
+
 export interface GQLUndoVariables {
   input: GQLUndoInput;
 }
@@ -44,4 +46,12 @@ export interface GQLUndoRedoItemPayload {
 
 export interface GQLSuccessPayload {
   __typename: 'SuccessPayload';
+  id: string;
+  messages: GQLMessage[];
+}
+
+export interface GQLErrorPayload {
+  __typename: 'ErrorPayload';
+  id: string;
+  messages: GQLMessage[];
 }
