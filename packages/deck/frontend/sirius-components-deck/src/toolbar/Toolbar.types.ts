64993c90e0bde922fada9b092c4cd02534fdd543
Michaël Charfadi <michael.charfadi@obeosoft.com>
2026-09-08 10:55:16 +0200

    [enh] Initialize React refs with null
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>
    (cherry picked from commit 3c714192ac6d47bb34997c5198c14290ee1d3fc7)

--- packages/deck/frontend/sirius-components-deck/src/toolbar/Toolbar.types.ts
+++ packages/deck/frontend/sirius-components-deck/src/toolbar/Toolbar.types.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,7 @@ export interface ToolbarProps {
   onZoomOut: () => void;
   onFitToScreen: () => void;
   onResetZoom: () => void;
-  fullscreenNode: React.RefObject<HTMLDivElement>;
+  fullscreenNode: React.RefObject<HTMLDivElement | null>;
 }
 
 export interface ToolbarState {
--- packages/diagrams/frontend/sirius-components-diagrams/src/renderer/hooks/useGetUpdatedModalPosition.tsx
+++ packages/diagrams/frontend/sirius-components-diagrams/src/renderer/hooks/useGetUpdatedModalPosition.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -25,7 +25,7 @@ const xyFlowDomBoundsSelector = (state: ReactFlowState): DOMRect | undefined => 
 export const useGetUpdatedModalPosition = (): UseGetUpdatedModalPositionValue => {
   const xyFlowDomBounds = useStore(xyFlowDomBoundsSelector, equalityFunction);
 
-  const getUpdatedModalPosition = (initialPosition: XYPosition, modalRef: React.RefObject<HTMLDivElement>) => {
+  const getUpdatedModalPosition = (initialPosition: XYPosition, modalRef: React.RefObject<HTMLDivElement | null>) => {
     let position: XYPosition = { ...initialPosition };
     if (modalRef.current && xyFlowDomBounds) {
       const modalBounds: DOMRect = modalRef.current.getBoundingClientRect();
@@ -42,7 +42,7 @@ export const useGetUpdatedModalPosition = (): UseGetUpdatedModalPositionValue =>
     return position;
   };
 
-  const getUpdatedBounds = (modalRef: React.RefObject<HTMLDivElement>) => {
+  const getUpdatedBounds = (modalRef: React.RefObject<HTMLDivElement | null>) => {
     let draggableBounds = {
       left: 0,
       top: 0,
