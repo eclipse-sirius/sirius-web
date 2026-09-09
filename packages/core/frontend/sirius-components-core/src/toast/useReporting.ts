488fdb9eefd353f2a608685d197b7660f73e2ee1
Michael Charfadi <michael.charfadi@obeosoft.com>
2026-09-09 14:26:16 +0200

    [releng] Bump to React19
    Signed-off-by: Michaël Charfadi <michael.charfadi@obeosoft.com>

--- packages/core/frontend/sirius-components-core/src/toast/useReporting.ts
+++ packages/core/frontend/sirius-components-core/src/toast/useReporting.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,7 @@ export const useReporting = <T>(
         addErrorMessage(error.message);
       }
       if (data) {
-        const payload: GQLErrorPayload | GQLSuccessPayload = extractPayload(data);
+        const payload: GQLErrorPayload | GQLSuccessPayload = extractPayload(data as T);
         if (isSuccessPayload(payload) || isErrorPayload(payload)) {
           const { messages } = payload;
           if (messages) {
